a1761d44c612ee06421d9e769bd31565fdbdeb95
Ferdinando Ametrano <nando@ametrano.net>
2001-10-25 10:17:42 +0000

    updating for quep

---
+++ QuantLib-site/webquepupdate.bat
@@ -0,0 +1,13 @@
+
+if not %1.==. goto NOTNANDO
+set USERNAME=nando
+goto DONE
+
+:NOTNANDO
+set USERNAME=%1
+:DONE
+
+
+scp -p -C -r quep/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/styles
+
+pause
--- QuantLib-site/webstylesupdate.bat
+++ QuantLib-site/webstylesupdate.bat
@@ -8,8 +8,6 @@ set USERNAME=%1
 :DONE
 
 
-scp -p -C -r *.html LICENSE.TXT %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs
-scp -p -C -r images/*.jpg %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/images
 scp -p -C -r styles/*.css %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/styles
 
 pause
