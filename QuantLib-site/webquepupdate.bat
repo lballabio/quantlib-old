81b1a4f1468b268e4335bc474ae50b6e8348ad44
Luigi Ballabio <luigi.ballabio@gmail.com>
2001-11-09 16:54:36 +0000

    *** empty log message ***

--- QuantLib-site/webquepupdate.bat
+++ QuantLib-site/webquepupdate.bat
@@ -8,7 +8,7 @@ set USERNAME=%1
 :DONE
 
 
-rem scp -p -C -r quep/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep
-scp -p -C -r quep/quep3/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep3
+scp -p -C -r quep/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep
+rem scp -p -C -r quep/quep3/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep3
 
 pause
