a5f661629da73511cfc2b49354553acadfe6e519
Ferdinando Ametrano <nando@ametrano.net>
2002-01-10 09:27:44 +0000

    tidyfied

--- QuantLib-site/webquepupdate.bat
+++ QuantLib-site/webquepupdate.bat
@@ -8,8 +8,8 @@ set USERNAME=%1
 :DONE
 
 
-scp -p -C -r quep.html %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs
-scp -p -C -r quep/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep
-rem scp -p -C -r quep/quep3/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep3
+scp -C -r quep.html %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs
+scp -C -r quep/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep
+rem scp -C -r quep/quep3/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep3
 
 pause
