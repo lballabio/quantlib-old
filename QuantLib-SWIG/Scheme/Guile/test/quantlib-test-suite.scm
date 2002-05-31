ddefd24eaf4d4faae76ee744aa403495172ef709
Luigi Ballabio <luigi.ballabio@gmail.com>
2002-05-31 15:16:55 +0000

    Compacting MzScheme and Guile

---
+++ QuantLib-SWIG/Scheme/Guile/test/quantlib-test-suite.scm
@@ -0,0 +1,83 @@
+; Copyright (C) 2000, 2001, 2002 RiskMap srl
+;
+; This file is part of QuantLib, a free-software/open-source library
+; for financial quantitative analysts and developers - http://quantlib.org/
+;
+; QuantLib is free software developed by the QuantLib Group; you can
+; redistribute it and/or modify it under the terms of the QuantLib License;
+; either version 1.0, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; QuantLib License for more details.
+;
+; You should have received a copy of the QuantLib License along with this
+; program; if not, please email ferdinando@ametrano.net
+;
+; The QuantLib License is also available at http://quantlib.org/license.html
+; The members of the QuantLib Group are listed in the QuantLib License
+;
+; $Id$
+
+(use-modules (QuantLib))
+(load "unittest.scm")
+
+(let ((test-dir "../../Scheme/test"))
+  (if (file-exists? test-dir)
+      (chdir test-dir)))
+
+(load "date.scm")
+(load "daycounters.scm")
+(load "distributions.scm")
+(load "europeanoption.scm")
+(load "instruments.scm")
+(load "marketelements.scm")
+(load "operators.scm")
+(load "riskstatistics.scm")
+(load "segmentintegral.scm")
+(load "solvers1d.scm")
+(load "statistics.scm")
+(load "termstructures.scm")
+
+(let ((suite (make-suite)))
+  (suite-add-test suite Date-test
+                  "Testing dates")
+  (suite-add-test suite Day-counter-test
+                  "Testing act/act day counters")
+  (suite-add-test suite Distribution-test
+                  "Testing distributions")
+  (suite-add-test suite European-option-test-1
+                  "Testing European option greeks")
+  (suite-add-test suite European-option-test-2
+                  "Testing European option implied volatility")
+  (suite-add-test suite Instrument-test
+                  "Testing observability of stocks")
+  (suite-add-test suite Market-element-observability-test
+                  "Testing observability of market elements")
+  (suite-add-test suite Market-element-handle-observability-test
+                  "Testing observability of market element handles")
+  (suite-add-test suite Operator-test
+                  "Testing differential operators")
+  (suite-add-test suite Risk-statistics-test
+                  "Testing risk statistics")
+  (suite-add-test suite Segment-integral-test
+                  "Testing segment integral")
+  (suite-add-test suite Solver-1D-test
+                  "Testing 1D solvers")
+  (suite-add-test suite Statistics-test
+                  "Testing statistics")
+  (suite-add-test suite Term-structure-test-1
+                  "Testing consistency of implied term structure")
+  (suite-add-test suite Term-structure-test-2
+                  "Testing observability of implied term structure")
+  (suite-add-test suite Term-structure-test-3
+                  "Testing consistency of forward-spreaded term structure")
+  (suite-add-test suite Term-structure-test-4
+                  "Testing observability of forward-spreaded term structure")
+  (suite-add-test suite Term-structure-test-5
+                  "Testing consistency of zero-spreaded term structure")
+  (suite-add-test suite Term-structure-test-6
+                  "Testing observability of zero-spreaded term structure")
+  (suite-run suite))
+
---
+++ QuantLib-SWIG/Scheme/test/marketelements.scm
@@ -0,0 +1,105 @@
+; Copyright (C) 2000, 2001, 2002 RiskMap srl
+;
+; This file is part of QuantLib, a free-software/open-source library
+; for financial quantitative analysts and developers - http://quantlib.org/
+;
+; QuantLib is free software developed by the QuantLib Group; you can
+; redistribute it and/or modify it under the terms of the QuantLib License;
+; either version 1.0, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; QuantLib License for more details.
+;
+; You should have received a copy of the QuantLib License along with this
+; program; if not, please email ferdinando@ametrano.net
+;
+; The QuantLib License is also available at http://quantlib.org/license.html
+; The members of the QuantLib Group are listed in the QuantLib License
+;
+; $Id$
+
+(define (Market-element-observability-test)
+  (let ((flag #f))
+    (deleting-let ((me (new-SimpleMarketElement 0.0)
+                       delete-MarketElement)
+                   (obs (new-Observer (lambda () (set! flag #t)))
+                        delete-Observer))
+      (deleting-let ((temp (MarketElement->Observable me)
+                           delete-Observable))
+        (Observer-register-with obs temp))
+      (SimpleMarketElement-value-set! me 3.14)
+      (if (not flag)
+          (error "Observer was not notified of market element change")))))
+
+(define (Derived-market-element-test)
+  (for-each 
+   (lambda (f)
+     (deleting-let* ((me (new-SimpleMarketElement 17.0)
+                         delete-MarketElement)
+                     (h (new-MarketElementHandle me)
+                        delete-MarketElementHandle)
+                     (derived-me (new-DerivedMarketElement h f)
+                                 delete-MarketElement))
+       (let ((derived-result (MarketElement-value derived-me))
+             (function-result (f (MarketElement-value me))))
+         (if (> (abs (- derived-result function-result)) 1.0e-10)
+             (let ((error-msg
+                    (string-append
+                     (format "derived market element yields ~a~n"
+                             derived-result)
+                     (format "function result is ~a~n"
+                             function-result))))
+               (error error-msg))))))
+   (list (lambda (x) (+ x 10))
+         (lambda (x) (* x 10))
+         (lambda (x) (- x 10)))))
+
+(define (Composite-market-element-test)
+  (deleting-let* ((me1 (new-SimpleMarketElement 12.0) delete-MarketElement)
+                  (me2 (new-SimpleMarketElement 13.0) delete-MarketElement)
+                  (h1 (new-MarketElementHandle me1)
+                      delete-MarketElementHandle)
+                  (h2 (new-MarketElementHandle me2)
+                      delete-MarketElementHandle))
+    (for-each 
+     (lambda (f)
+       (deleting-let ((composite-me (new-CompositeMarketElement h1 h2 f)
+                                    delete-MarketElement))
+         (let ((composite-result (MarketElement-value composite-me))
+               (function-result (f (MarketElement-value me1)
+                                   (MarketElement-value me2))))
+           (if (> (abs (- composite-result function-result)) 1.0e-10)
+               (let ((error-msg
+                      (string-append
+                       (format "composite market element yields ~a~n"
+                               composite-result)
+                       (format "function result is ~a~n"
+                               function-result))))
+                 (error error-msg))))))
+     (list (lambda (x y) (+ x y))
+           (lambda (x y) (* x y))
+           (lambda (x y) (- x y))))))
+
+(define (Market-element-handle-observability-test)
+  (let ((flag #f))
+    (deleting-let* ((me1 (new-SimpleMarketElement 0.0)
+                         delete-MarketElement)
+                    (me2 (new-SimpleMarketElement 0.0)
+                         delete-MarketElement)
+                    (h (new-MarketElementHandle me1)
+                       delete-MarketElementHandle)
+                    (obs (new-Observer (lambda () (set! flag #t)))
+                         delete-Observer))
+      (deleting-let ((temp (MarketElementHandle->Observable h)
+                           delete-Observable))
+        (Observer-register-with obs temp))
+      (SimpleMarketElement-value-set! me1 3.14)
+      (if (not flag)
+          (error "Observer was not notified of market element change"))
+      (set! flag #f)
+      (MarketElementHandle-link-to! h me2)
+      (if (not flag)
+          (error "Observer was not notified of market element change")))))
+
