903cfe646e6b1fb5c7481f88cdc71dad096325d8
Ferdinando Ametrano <nando@ametrano.net>
2004-04-05 14:45:56 +0000

    updated

--- QuantLib-site/webimagesupdate.bat
+++ QuantLib-site/webimagesupdate.bat
@@ -8,6 +8,7 @@ set USERNAME=%1
 :DONE
 
 
+scp -C -r images/*.gif %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/images
 scp -C -r images/*.jpg %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/images
 scp -C -r images/*.png %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/images
 
