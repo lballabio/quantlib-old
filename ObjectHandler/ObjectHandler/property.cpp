55b67cd7271108e1cab3f95c62c4538c12baabdb
Eric Ehlers <eric.ehlers@nazcatech.be>
2004-10-31 20:45:20 +0000

    Duffy's Propery class for Object

---
+++ ObjectHandler/ObjectHandler/property.cpp
@@ -0,0 +1,27 @@
+#include <ObjectHandler/property.hpp>
+
+template <class Name, class Type>
+	Property < Name, Type>::Property(const Name& name, const Type& t)
+{ // Constructor
+
+	nam = name;
+	con = t;
+}
+
+template <class Name, class Type>
+	Type Property<Name, Type>::operator() () const
+{	// Getting the current value of the property
+	return con;
+}
+
+template <class Name, class Type>
+void Property<Name, Type>::operator() (const Type& t)
+{	// Setting the value (contents) to a given value
+	con = t;
+}
+
+template <class Name, class Type>
+Name Property<Name, Type>::name() const
+{
+	return nam;
+}
