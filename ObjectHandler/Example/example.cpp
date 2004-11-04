fec1a4b8cac4425e9da04ae21214b9ab34819984
Eric Ehlers <eric.ehlers@nazcatech.be>
2004-11-04 20:17:16 +0000

    example ObjectHandler client

--- ObjectHandler/Example/example.cpp
+++ ObjectHandler/Example/example.cpp
@@ -1,6 +1,23 @@
+/*
+ Copyright (C) 2004 Eric Ehlers
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
 #include <iostream>
 #include <exception>
 #include <interface.hpp>
+#include <ObjectHandler/autolink.hpp>
 
 using namespace std;
 
@@ -49,6 +66,7 @@ int main() {
 
 		QL_LOGMESSAGE("bye");
 		cout << endl << "bye" << endl;
+		return 0;
 	} catch (const exception &e) {
 		cout << "Error: " << e.what() << endl;
 		QL_LOGMESSAGE("Error: " + string(e.what()));
