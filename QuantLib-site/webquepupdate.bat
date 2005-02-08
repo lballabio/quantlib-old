acaf4a1345934b6fa4695af6db825f636e8ade18
Ferdinando Ametrano <nando@ametrano.net>
2005-02-08 19:22:27 +0000

    fixed broken links and moved old folders

--- QuantLib-site/webquepupdate.bat
+++ QuantLib-site/webquepupdate.bat
@@ -8,8 +8,15 @@ set USERNAME=%1
 :DONE
 
 
-scp -C -r quep.shtml %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs
-scp -C -r quep/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep
-rem scp -C -r quep/quep3/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep3
+C:\cygwin\bin\scp -C -r quep.shtml %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs
+C:\cygwin\bin\scp -C -r quep/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep
+C:\cygwin\bin\scp -C -r quep/quep001/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep001
+C:\cygwin\bin\scp -C -r quep/quep002/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep002
+C:\cygwin\bin\scp -C -r quep/quep002/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep003
+C:\cygwin\bin\scp -C -r quep/quep005/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep005
+C:\cygwin\bin\scp -C -r quep/quep006/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep006
+C:\cygwin\bin\scp -C -r quep/quep007/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep007
+C:\cygwin\bin\scp -C -r quep/quep011/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep011
+C:\cygwin\bin\scp -C -r quep/quep012/*.* %USERNAME%@shell.sourceforge.net:/home/groups/q/qu/quantlib/htdocs/quep/quep012
 
 pause
