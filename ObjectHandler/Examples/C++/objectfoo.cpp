41534a77b9e8905474c69c44207b466a255970e5
Eric Ehlers <eric.ehlers@nazcatech.be>
2005-08-18 10:20:15 +0000

    delete test code

--- ObjectHandler/Examples/C++/objectfoo.cpp
+++ ObjectHandler/Examples/C++/objectfoo.cpp
@@ -26,12 +26,6 @@ ObjectFoo::ObjectFoo(
         const int &i) {
     foo_        = boost::shared_ptr<Foo>(new Foo(s, i));
     // populate base class Property vector
-    //any_ptr anyString(new boost::any(foo_->s()));
-    //any_ptr anyInt(new boost::any(foo_->i()));
-    //ObjectProperty propString(PROPERTY_STR, anyString);
-    //ObjectProperty propInt(PROPERTY_INT, anyInt);
-    //properties_.push_back(propString);
-    //properties_.push_back(propInt);    
 	createProperty(PROPERTY_STR, foo_->s());
 	createProperty(PROPERTY_INT, foo_->i());
 }
@@ -40,8 +34,6 @@ ObjectFoo::ObjectFoo(
 void ObjectFoo::update(const string &s, const int &i) {
     foo_->update(s, i);
     // update Property vector
-    //*properties_[IDX_STR]() = s;
-    //*properties_[IDX_INT]() = i;    
 	updateProperty(IDX_STR, s);
 	updateProperty(IDX_INT, i);
 }
