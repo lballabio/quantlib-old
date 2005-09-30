98d95680a00beda175f00e97f735e7992ebbb339
Joseph Wang <drjoe@users.sf.net>
2005-09-30 23:28:54 +0000

    Some fixes

--- QuantLib-SWIG/Perl/examples/european-option.pl
+++ QuantLib-SWIG/Perl/examples/european-option.pl
@@ -2,15 +2,17 @@
 
 use QuantLib;
 package QuantLibc;
+use strict;
 
 my($todaysDate) = new_Date(15, 5, 1998);
+Settings_setEvaluationDate(Settings_instance(), $todaysDate);
 my($settlementDate) = new_Date(17, 5, 1998);
 my($riskFreeRate) = new_FlatForward($settlementDate,
 				    0.05,
 				    new_Actual365Fixed);
 
 my($exercise) = new_EuropeanExercise(new_Date(17,5,1999));
-my($payoff) = new_PlainVanillaPayoff(Option_Call, 8.0);
+my($payoff) = new_PlainVanillaPayoff(QuantLibc::Option_Put, 8.0);
 
 my($underlying) = new_SimpleQuote(7.0);
 my($volatility) = new_BlackConstantVol($todaysDate, 
@@ -18,3 +20,18 @@ my($volatility) = new_BlackConstantVol($todaysDate,
 				       new_Actual365Fixed());
 my($dividendYield) = new_FlatForward($settlementDate,
 				     0.05, new_Actual365Fixed());
+
+my($process) = 
+    new_BlackScholesProcess(new_QuoteHandle($underlying),
+			    new_YieldTermStructureHandle($dividendYield),
+			    new_YieldTermStructureHandle($riskFreeRate),
+			    new_BlackVolTermStructureHandle($volatility));
+
+my($option) =
+    new_VanillaOption($process, $payoff, $exercise);
+
+$option->setPricingEngine(new_AnalyticEuropeanEngine());
+print "analytic", $option->NPV(), "\n";
+
+$option->setPricingEngine(new_IntegralEngine());
+print "integral", $option->NPV(), "\n";
