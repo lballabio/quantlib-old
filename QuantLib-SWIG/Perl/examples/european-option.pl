86054cfd3202f044dc27dd466a61d9b03afb1328
Joseph Wang <drjoe@users.sf.net>
2005-09-30 23:44:36 +0000

    add constants

--- QuantLib-SWIG/Perl/examples/european-option.pl
+++ QuantLib-SWIG/Perl/examples/european-option.pl
@@ -4,15 +4,15 @@ use QuantLib;
 package QuantLibc;
 use strict;
 
-my($todaysDate) = new_Date(15, 5, 1998);
+my($todaysDate) = new_Date(15, $QuantLibc::May, 1998);
 Settings_setEvaluationDate(Settings_instance(), $todaysDate);
-my($settlementDate) = new_Date(17, 5, 1998);
+my($settlementDate) = new_Date(17, $QuantLibc::May, 1998);
 my($riskFreeRate) = new_FlatForward($settlementDate,
 				    0.05,
 				    new_Actual365Fixed);
 
-my($exercise) = new_EuropeanExercise(new_Date(17,5,1999));
-my($payoff) = new_PlainVanillaPayoff(QuantLibc::Option_Put, 8.0);
+my($exercise) = new_EuropeanExercise(new_Date(17,$QuantLibc::May, 1999));
+my($payoff) = new_PlainVanillaPayoff($QuantLibc::Option_Put, 8.0);
 
 my($underlying) = new_SimpleQuote(7.0);
 my($volatility) = new_BlackConstantVol($todaysDate, 
@@ -31,7 +31,7 @@ my($option) =
     new_VanillaOption($process, $payoff, $exercise);
 
 $option->setPricingEngine(new_AnalyticEuropeanEngine());
-print "analytic", $option->NPV(), "\n";
+print "analytic ", $option->NPV(), "\n";
 
 $option->setPricingEngine(new_IntegralEngine());
-print "integral", $option->NPV(), "\n";
+print "integral ", $option->NPV(), "\n";
