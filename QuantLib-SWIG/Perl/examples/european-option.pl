8aca6aaa664f2183fa3e4d2e7395e7efd79fbc8e
Luigi Ballabio <luigi.ballabio@gmail.com>
2005-10-03 08:00:55 +0000

    *** empty log message ***

--- QuantLib-SWIG/Perl/examples/european-option.pl
+++ QuantLib-SWIG/Perl/examples/european-option.pl
@@ -1,37 +1,38 @@
 #!/usr/bin/perl
 
 use QuantLib;
-package QuantLibc;
 use strict;
 
-my($todaysDate) = new_Date(15, $QuantLibc::May, 1998);
-Settings_setEvaluationDate(Settings_instance(), $todaysDate);
-my($settlementDate) = new_Date(17, $QuantLibc::May, 1998);
-my($riskFreeRate) = new_FlatForward($settlementDate,
-				    0.05,
-				    new_Actual365Fixed);
+my($todaysDate) = new QuantLib::Date(15, $QuantLib::May, 1998);
+QuantLib::Settings::instance()->setEvaluationDate($todaysDate);
+my($settlementDate) = new QuantLib::Date(17, $QuantLib::May, 1998);
+my($riskFreeRate) = new QuantLib::FlatForward($settlementDate,
+                                              0.05,
+                                              new QuantLib::Actual365Fixed);
 
-my($exercise) = new_EuropeanExercise(new_Date(17,$QuantLibc::May, 1999));
-my($payoff) = new_PlainVanillaPayoff($QuantLibc::Option_Put, 8.0);
+my($exercise) = new QuantLib::EuropeanExercise(
+                                 new QuantLib::Date(17,$QuantLib::May, 1999));
+my($payoff) = new QuantLib::PlainVanillaPayoff($QuantLib::Option::Put, 8.0);
 
-my($underlying) = new_SimpleQuote(7.0);
-my($volatility) = new_BlackConstantVol($todaysDate, 
-				       0.10,
-				       new_Actual365Fixed());
-my($dividendYield) = new_FlatForward($settlementDate,
-				     0.05, new_Actual365Fixed());
+my($underlying) = new QuantLib::SimpleQuote(7.0);
+my($volatility) = new QuantLib::BlackConstantVol($todaysDate,
+                                                 0.10,
+                                                 new QuantLib::Actual365Fixed);
+my($dividendYield) = new QuantLib::FlatForward($settlementDate,
+                                               0.05,
+                                               new QuantLib::Actual365Fixed);
 
-my($process) = 
-    new_BlackScholesProcess(new_QuoteHandle($underlying),
-			    new_YieldTermStructureHandle($dividendYield),
-			    new_YieldTermStructureHandle($riskFreeRate),
-			    new_BlackVolTermStructureHandle($volatility));
+my($process) = new QuantLib::BlackScholesProcess(
+                      new QuantLib::QuoteHandle($underlying),
+                      new QuantLib::YieldTermStructureHandle($dividendYield),
+                      new QuantLib::YieldTermStructureHandle($riskFreeRate),
+                      new QuantLib::BlackVolTermStructureHandle($volatility));
 
-my($option) =
-    new_VanillaOption($process, $payoff, $exercise);
+my($option) = new QuantLib::VanillaOption($process, $payoff, $exercise);
 
-$option->setPricingEngine(new_AnalyticEuropeanEngine());
-print "analytic ", $option->NPV(), "\n";
+$option->setPricingEngine(new QuantLib::AnalyticEuropeanEngine);
+print "analytic: ", $option->NPV(), "\n";
 
-$option->setPricingEngine(new_IntegralEngine());
-print "integral ", $option->NPV(), "\n";
+$option->setPricingEngine(new QuantLib::IntegralEngine);
+print "integral: ", $option->NPV(), "\n";
+
