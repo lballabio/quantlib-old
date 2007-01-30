1c70b1b0c098476309e6c1a3c6e62e20f0511716
Eric Ehlers <eric.ehlers@nazcatech.be>
2007-01-30 10:10:37 +0000

    replace updateLuncherXlls.bat with updateLauncherXlls.py

---
+++ Launcher/updateLauncherXlls.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import glob
+
+SOURCE_TARGET_DIRS = (
+    ( "X:/Offices1/CabotoXL/01 Production/xll", "Addins/01 Production" ),
+    ( "X:/Offices1/CabotoXL/02 Pre-Production/xll", "Addins/02 Pre-Production" ),
+    ( "X:/Offices1/CabotoXL/03 Testing/xll", "Addins/03 Testing" ),
+)
+
+#SOURCE_TARGET_DIRS = (
+#    ( "C:/erik/projects/trunk/QuantLibXL/xll", "Addins/01 Production" ),
+#    ( "C:/erik/projects/trunk/QuantLibXL/xll", "Addins/02 Pre-Production" ),
+#    ( "C:/erik/projects/trunk/QuantLibXL/xll", "Addins/03 Testing" ),
+#)
+
+DELETE_PATTERN = "QuantLibXL-*.xll"
+COPY_PATTERN = "QuantLibXL-vc80-mt-s-*.xll"
+#COPY_PATTERN = "QuantLibXL-*.xll"
+
+def deleteGlob(path):
+    for file in glob.glob(path):
+        print "deleting file\n" + file
+        os.unlink(file)
+
+def copyGlob(sourcePattern, targetPath):
+    for sourceFile in glob.glob(sourcePattern):
+        print "copy from =\n" + sourceFile
+        print "copy to =\n" + targetPath
+        shutil.copy(sourceFile, targetPath)
+
+for (sourceDir, targetDir) in SOURCE_TARGET_DIRS:
+    targetDir = os.getcwd() + "/" + targetDir
+    deleteGlob(targetDir + "/" + DELETE_PATTERN)
+    copyGlob(sourceDir + "/" + COPY_PATTERN, targetDir)
+
+raw_input('copy complete. press any key to exit.')
+
