e1e935461483f01f2aa2c1a82097830c830d09c4
Joseph Wang <drjoe@users.sf.net>
2007-02-01 04:54:40 +0000

    add simple hello world program

---
+++ QuantLib-SWIG/Java/examples/Hello.java
@@ -0,0 +1,17 @@
+import org.quantlib.*;
+
+public class Hello { 
+
+	static { 
+		System.loadLibrary("quantlib_javawrap"); 
+	} 
+
+	public static void main(String[] args) { 
+		QuantLib ql = new QuantLib(); 
+		double nd = QuantLib.nullDouble(); 
+		
+		System.out.println("ql nullDouble is "+nd); 
+		System.out.println("and sunday is "+Weekday.Sunday.toString()); 
+	} 
+}
+
