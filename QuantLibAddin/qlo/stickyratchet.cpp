7eb9c2542f30800a949d427fcd8483262ac6ba54
Marco Bianchetti <marcobianchetti@users.sf.net>
2007-02-07 15:39:35 +0000

    exposed doubleStickyRatchets payoffs to excel

---
+++ QuantLibAddin/qlo/stickyratchet.cpp
@@ -0,0 +1,179 @@
+
+/*
+ Copyright (C) 2007 Marco Bianchetti
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#if defined(HAVE_CONFIG_H)
+    #include <qlo/config.hpp>
+#endif
+
+#include <qlo/stickyratchet.hpp>
+#include <qlo/typefactory.hpp>
+
+namespace QuantLibAddin {
+
+    DoubleStickyRatchetPayoff::DoubleStickyRatchetPayoff(
+            const QuantLib::Real type1,
+            const QuantLib::Real type2,
+            const QuantLib::Real gearing1,
+            const QuantLib::Real gearing2,
+            const QuantLib::Real gearing3,
+            const QuantLib::Spread spread1,
+            const QuantLib::Spread spread2,
+            const QuantLib::Spread spread3,
+            const QuantLib::Real initialValue1,
+            const QuantLib::Real initialValue2,
+            const QuantLib::Real accrualFactor) {
+        libraryObject_ = boost::shared_ptr<QuantLib::DoubleStickyRatchetPayoff>(
+        new QuantLib::DoubleStickyRatchetPayoff(
+            type1,
+            type2,
+            gearing1,
+            gearing2,
+            gearing3,
+            spread1,
+            spread2,
+            spread3,
+            initialValue1,
+            initialValue2,
+            accrualFactor));    
+        }
+
+    RatchetPayoff::RatchetPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Real initialValue,
+              const QuantLib::Real accrualFactor) {
+            libraryObject_ = boost::shared_ptr<QuantLib::RatchetPayoff>(
+            new QuantLib::RatchetPayoff(
+              gearing1,
+              gearing2,
+              spread1,
+              spread2,
+              initialValue,
+              accrualFactor));
+            }
+
+    StickyPayoff::StickyPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Real initialValue,
+              const QuantLib::Real accrualFactor) {
+            libraryObject_ = boost::shared_ptr<QuantLib::StickyPayoff>(
+            new QuantLib::StickyPayoff(
+              gearing1,
+              gearing2,
+              spread1,
+              spread2,
+              initialValue,
+              accrualFactor));
+            }
+
+    RatchetMaxPayoff::RatchetMaxPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor) {
+            libraryObject_ = boost::shared_ptr<QuantLib::RatchetMaxPayoff>(
+            new QuantLib::RatchetMaxPayoff(
+              gearing1,
+              gearing2,
+              gearing3,
+              spread1,
+              spread2,
+              spread3,
+              initialValue1,
+              initialValue2,
+              accrualFactor));
+            }
+
+    RatchetMinPayoff::RatchetMinPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor) {
+            libraryObject_ = boost::shared_ptr<QuantLib::RatchetMinPayoff>(
+            new QuantLib::RatchetMinPayoff(
+              gearing1,
+              gearing2,
+              gearing3,
+              spread1,
+              spread2,
+              spread3,
+              initialValue1,
+              initialValue2,
+              accrualFactor));
+            }
+
+    StickyMaxPayoff::StickyMaxPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor) {
+            libraryObject_ = boost::shared_ptr<QuantLib::StickyMaxPayoff>(
+            new QuantLib::StickyMaxPayoff(
+              gearing1,
+              gearing2,
+              gearing3,
+              spread1,
+              spread2,
+              spread3,
+              initialValue1,
+              initialValue2,
+              accrualFactor));
+            }
+
+    StickyMinPayoff::StickyMinPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor) {
+            libraryObject_ = boost::shared_ptr<QuantLib::StickyMinPayoff>(
+            new QuantLib::StickyMinPayoff(
+              gearing1,
+              gearing2,
+              gearing3,
+              spread1,
+              spread2,
+              spread3,
+              initialValue1,
+              initialValue2,
+              accrualFactor));
+            }
+}
---
+++ QuantLibAddin/qlo/stickyratchet.hpp
@@ -0,0 +1,124 @@
+
+/*
+ Copyright (C) 2007 Marco Bianchetti
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#ifndef qla_stickyratchet_hpp
+#define qla_stickyratchet_hpp
+
+#include <oh/objhandler.hpp>
+#include <ql/Instruments/stickyratchet.hpp>
+#include <qlo/payoffs.hpp>
+
+namespace QuantLibAddin {
+
+    class DoubleStickyRatchetPayoff : public Payoff {
+      public:
+          DoubleStickyRatchetPayoff() {}
+          DoubleStickyRatchetPayoff(
+              const QuantLib::Real type1,
+              const QuantLib::Real type2,
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor);
+    };
+
+    class RatchetPayoff : public DoubleStickyRatchetPayoff {
+      public:
+          RatchetPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Real initialValue,
+              const QuantLib::Real accrualFactor);
+    };
+
+    class StickyPayoff : public DoubleStickyRatchetPayoff {
+      public:
+          StickyPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Real initialValue,
+              const QuantLib::Real accrualFactor);
+    };
+
+    class RatchetMaxPayoff : public DoubleStickyRatchetPayoff {
+      public:
+          RatchetMaxPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor);
+    };
+
+    class RatchetMinPayoff : public DoubleStickyRatchetPayoff {
+      public:
+          RatchetMinPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor);
+    };
+
+    class StickyMaxPayoff : public DoubleStickyRatchetPayoff {
+      public:
+          StickyMaxPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor);
+    };
+
+    class StickyMinPayoff : public DoubleStickyRatchetPayoff {
+      public:
+          StickyMinPayoff(
+              const QuantLib::Real gearing1,
+              const QuantLib::Real gearing2,
+              const QuantLib::Real gearing3,
+              const QuantLib::Spread spread1,
+              const QuantLib::Spread spread2,
+              const QuantLib::Spread spread3,
+              const QuantLib::Real initialValue1,
+              const QuantLib::Real initialValue2,
+              const QuantLib::Real accrualFactor);
+    };
+
+}
+
+#endif
