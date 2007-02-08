0399a967eb90bfd065e4d5cbc56e32ba59c51fd7
Ferdinando Ametrano <nando@ametrano.net>
2007-02-08 15:50:00 +0000

    *** empty log message ***

--- QuantLibAddin/qlo/optimization.cpp
+++ QuantLibAddin/qlo/optimization.cpp
@@ -1,6 +1,6 @@
 
 /*
- Copyright (C) 2006 Ferdinando Ametrano
+ Copyright (C) 2006, 2007 Ferdinando Ametrano
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
