b9a28d96e173c32b118cef92d3ca4776d4f7eacf
Eric Ehlers <eric.ehlers@nazcatech.be>
2007-02-27 22:11:01 +0000

    update OH version number

--- QuantLibAddin/Addins/C/auto_link.h
+++ QuantLibAddin/Addins/C/auto_link.h
@@ -57,7 +57,7 @@
 #  endif
 #endif
 
-#define OH_LIB_NAME "ObjectHandler-" LIB_TOOLSET LIB_THREAD_OPT LIB_RT_OPT "-0_2_0.lib"
+#define OH_LIB_NAME "ObjectHandler-" LIB_TOOLSET LIB_THREAD_OPT LIB_RT_OPT "-0_9_0.lib"
 #define QL_LIB_NAME "QuantLib-" LIB_TOOLSET LIB_THREAD_OPT LIB_RT_OPT "-0_9_0.lib"
 
 #pragma message("Linking to lib file: " OH_LIB_NAME)
