a25d4e491a7c808ffa912945c1c4afd299126f07
Eric Ehlers <eric.ehlers@nazcatech.be>
2007-03-18 13:10:46 +0000

    linux catching up

---
+++ QuantLibAddin/qlo/qladdin.hpp
@@ -0,0 +1,81 @@
+
+/*
+ Copyright (C) 2007 Eric Ehlers
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#ifndef qla_qladdin_hpp
+
+#include <qlo/analysis.hpp>
+#include <qlo/asianoption.hpp>
+#include <qlo/assetswap.hpp>
+#include <qlo/barrieroption.hpp>
+#include <qlo/baseinstruments.hpp>
+#include <qlo/bonds.hpp>
+#include <qlo/capfloor.hpp>
+#include <qlo/capletvolstructure.hpp>
+#include <qlo/cliquetoption.hpp>
+#include <qlo/cmsmarket.hpp>
+#include <qlo/config.hpp>
+#include <qlo/couponvectors.hpp>
+#include <qlo/date.hpp>
+#include <qlo/dividendvanillaoption.hpp>
+#include <qlo/enumclassctors.hpp>
+#include <qlo/europeanoption.hpp>
+#include <qlo/exercise.hpp>
+#include <qlo/forwardrateagreement.hpp>
+#include <qlo/forwardvanillaoption.hpp>
+#include <qlo/getcovariance.hpp>
+#include <qlo/handle.hpp>
+#include <qlo/index.hpp>
+#include <qlo/interpolation2D.hpp>
+#include <qlo/interpolation.hpp>
+#include <qlo/marketmodels.hpp>
+#include <qlo/mathf.hpp>
+#include <qlo/optimization.hpp>
+#include <qlo/options.hpp>
+#include <qlo/payoffs.hpp>
+#include <qlo/pricingengines.hpp>
+#include <qlo/processes.hpp>
+#include <qlo/qladdindefines.hpp>
+#include <qlo/quantoforwardvanillaoption.hpp>
+#include <qlo/quantovanillaoption.hpp>
+#include <qlo/quotes.hpp>
+#include <qlo/randomsequencegenerator.hpp>
+#include <qlo/ratehelpers.hpp>
+#include <qlo/schedule.hpp>
+#include <qlo/sequencestatistics.hpp>
+#include <qlo/settings.hpp>
+#include <qlo/shortratemodels.hpp>
+#include <qlo/simplecashflow.hpp>
+#include <qlo/smilesection.hpp>
+#include <qlo/statistics.hpp>
+#include <qlo/stickyratchet.hpp>
+#include <qlo/swap.hpp>
+#include <qlo/swaption.hpp>
+#include <qlo/swaptionvolstructure.hpp>
+#include <qlo/symmetricschurdecomposition.hpp>
+#include <qlo/termstructures.hpp>
+#include <qlo/typefactory.hpp>
+#include <qlo/typeregistry.hpp>
+#include <qlo/utilities.hpp>
+#include <qlo/vanillaoption.hpp>
+#include <qlo/vanillaswap.hpp>
+#include <qlo/vcconfig.hpp>
+#include <qlo/volatilities.hpp>
+
+#include <qlo/ValueObjects/vo_all.hpp>
+
+#endif
+
