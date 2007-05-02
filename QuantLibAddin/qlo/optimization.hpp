21697282c6e4f92fbd966bc4a71c2b051e96d41e
Ferdinando Ametrano <nando@ametrano.net>
2007-05-02 10:17:20 +0000

    (re-)introduced qlSecondsToString in the application layer library

--- QuantLibAddin/qlo/optimization.hpp
+++ QuantLibAddin/qlo/optimization.hpp
@@ -24,6 +24,17 @@
 
 namespace QuantLibAddin {
 
+    inline std::string qlSecondsToString(QuantLib::Real elapsed) {
+        QuantLib::Integer seconds = static_cast<QuantLib::Integer>(elapsed);
+        QuantLib::Integer hours = seconds/3600;
+        seconds -= hours * 3600;
+        QuantLib::Integer minutes = seconds/60;
+        seconds -= minutes * 60;
+        std::ostringstream out;
+        out << hours << ":" << minutes << ":" << seconds;
+        return out.str();
+    }
+
     class EndCriteria :
         public ObjectHandler::LibraryObject<QuantLib::EndCriteria> {
       public:
