17151dbd252635b88b204f1452365a81d737274d
Marco Bianchetti <marcobianchetti@users.sf.net>
2007-06-12 10:40:06 +0000

    Alpha forms wrapped and exported (in their own file) VC7 catching up

---
+++ QuantLibAddin/qlo/alphaform.cpp
@@ -0,0 +1,43 @@
+
+/*
+ Copyright (C) 2007 Marco Bianchetti
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#if defined(HAVE_CONFIG_H)     // Dynamically created by configure
+    #include <qlo/config.hpp>
+#endif
+#include <qlo/alphaform.hpp>
+
+namespace QuantLibAddin {
+
+    AlphaFormInverseLinear::AlphaFormInverseLinear(
+        std::vector<QuantLib::Time>& times, 
+        QuantLib::Real alpha) {
+            libraryObject_ = 
+                boost::shared_ptr<QuantLib::AlphaFormInverseLinear>(new
+                    QuantLib::AlphaFormInverseLinear(times, alpha));
+        }
+
+
+    AlphaFormLinearHyperbolic::AlphaFormLinearHyperbolic(
+        std::vector<QuantLib::Time>& times, 
+        QuantLib::Real alpha) {
+            libraryObject_ = 
+                boost::shared_ptr<QuantLib::AlphaFormLinearHyperbolic>(new
+                    QuantLib::AlphaFormLinearHyperbolic(times, alpha));
+    }
+
+}
---
+++ QuantLibAddin/qlo/alphaform.hpp
@@ -0,0 +1,46 @@
+
+/*
+ Copyright (C) 2007 Marco Bianchetti
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#ifndef qla_alpha_form_hpp
+#define qla_alpha_form_hpp
+
+#include <oh/objecthandler.hpp>
+
+#include <ql/models/marketmodels/models/alphaform.hpp>
+#include <ql/models/marketmodels/models/alphaformconcrete.hpp>
+
+
+namespace QuantLibAddin {
+
+    class AlphaForm : public ObjectHandler::LibraryObject<QuantLib::AlphaForm> {};
+
+    class AlphaFormInverseLinear : public AlphaForm {
+      public:
+        AlphaFormInverseLinear(std::vector<QuantLib::Time>& times, 
+                               QuantLib::Real alpha);
+    };
+
+    class AlphaFormLinearHyperbolic : public AlphaForm {
+      public:
+        AlphaFormLinearHyperbolic(std::vector<QuantLib::Time>& times, 
+                                  QuantLib::Real alpha) ;
+    };
+
+}
+
+#endif
