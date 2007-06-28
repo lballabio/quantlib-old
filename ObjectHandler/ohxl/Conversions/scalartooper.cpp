5607a6cf364485272cf64c048912ffec590305d9
Eric Ehlers <eric.ehlers@nazcatech.be>
2007-06-28 20:13:08 +0000

    Cleaner disambiguation of template arguments

--- ObjectHandler/ohxl/Conversions/scalartooper.cpp
+++ ObjectHandler/ohxl/Conversions/scalartooper.cpp
@@ -77,9 +77,14 @@ namespace ObjectHandler {
         scalarToOper(boost::any_cast<T>(value), xAny);
     }
 
-    template <class T1, class T2>
-    void wrapScalarToOper(const boost::any &value, OPER &xAny) {
-        scalarToOper(static_cast<T2>(boost::any_cast<T1>(value)), xAny);
+    template <>
+    void wrapScalarToOper<unsigned int>(const boost::any &value, OPER &xAny) {
+        wrapScalarToOper<long>(value, xAny);
+    }
+
+    template <>
+    void wrapScalarToOper<int>(const boost::any &value, OPER &xAny) {
+        wrapScalarToOper<long>(value, xAny);
     }
 
     template <class T>
@@ -100,9 +105,9 @@ namespace ObjectHandler {
             OPER *xTemp = boost::any_cast<OPER*>(value);
             Excel(xlCoerce, &xAny, 1, xTemp);
         } else if (value.type() == typeid(unsigned int)) {
-            wrapScalarToOper<unsigned int, long>(value, xAny);
+            wrapScalarToOper<unsigned int>(value, xAny);
         } else if (value.type() == typeid(int)) {
-            wrapScalarToOper<int, long>(value, xAny);
+            wrapScalarToOper<int>(value, xAny);
         } else if (value.type() == typeid(long)) {
             wrapScalarToOper<long>(value, xAny);
         } else if (value.type() == typeid(double)) {
