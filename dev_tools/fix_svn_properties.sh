98b9b0772be8a5de6cd86bdeec6e9619cab7d0f0
Eric Ehlers <eric.ehlers@nazcatech.be>
2007-06-28 14:20:32 +0000

    update fix_svn_properties.sh to set svn:eol-style to native for gensrc "stub.*" files and re-run the script for gensrc, QuantLibAddin and QuantLibXL

--- dev_tools/fix_svn_properties.sh
+++ dev_tools/fix_svn_properties.sh
@@ -4,6 +4,7 @@ find . -name '*.[hc]pp' -o -name '*.[hc]' \
     -o -name '*.html' -o -name '*.css' -o -name '*.docs' -o -name '*.doxy' \
     -o -name '*.sh' -o -name '*.bat' -o -name '*.el' -o -name '*.py' \
     -o -name '*.txt' -o -name '*.TXT' -o -name '*.tex' -o -name '*.xml' \
+    -o -name 'stub.*' \
  | xargs -n 1 svn propset svn:eol-style native
 # these should have UNIX eol, even when extracted from a .zip
 find . -name '*.a[cm]' -o -name '*.m4' -o -name '*.in' \
