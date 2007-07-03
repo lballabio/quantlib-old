fd2dc8252219ad4c65015c787674bc6aba265d90
Eric Ehlers <eric.ehlers@nazcatech.be>
2007-07-03 21:29:44 +0000

    Add support for multiple XLLs per environment

--- Launcher/updateLauncherXlls.py
+++ Launcher/updateLauncherXlls.py
@@ -1,36 +1,56 @@
+import sys
 import os
+import re
 import shutil
 import glob
 
-SOURCE_TARGET_DIRS = (
-    ( "C:/Projects/QuantLibSVN/DevCycle/Rev10747/QuantLibXL/xll", "Addins/01 Production" ),
-    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11190/QuantLibXL/xll", "Addins/02 Pre-Production" ),
-    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11705/QuantLibXL/xll", "Addins/03 Testing" ),
+SOURCE_TARGET_FILE_LIST = (
+    ( "C:/Projects/QuantLibSVN/DevCycle/Rev10747/QuantLibXL/xll", "Addins/01 Production", "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
+    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11190/QuantLibXL/xll", "Addins/02 Pre-Production", "QuantLibXL-vc80-mt-s-0_9_0.xll" ),
+    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11705/ObjectHandler/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11705/QuantLibXL/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
 )
 
-#SOURCE_TARGET_DIRS = (
-#    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev10747/QuantLibXL/xll", "Addins/01 Production" ),
-#    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11190/QuantLibXL/xll", "Addins/02 Pre-Production" ),
-#    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11705/QuantLibXL/xll", "Addins/03 Testing" ),
+#SOURCE_TARGET_FILE_LIST = (
+#    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev10747/QuantLibXL/xll", "Addins/01 Production", "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
+#    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11190/QuantLibXL/xll", "Addins/02 Pre-Production", "QuantLibXL-vc80-mt-s-0_9_0.xll" ),
+#    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11705/ObjectHandler/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+#    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11705/QuantLibXL/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
 #)
 
-DELETE_PATTERN = "*.xll"
-COPY_PATTERN = "*.xll"
+#SOURCE_TARGET_FILE_LIST = (
+#    ( "C:/erik/projects/trunk/QuantLibXL/xll", "Addins/01 Production", "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
+#    ( "C:/erik/projects/trunk/QuantLibXL/xll", "Addins/02 Pre-Production", "QuantLibXL-vc80-mt-s-0_9_0.xll" ),
+#    ( "C:/erik/projects/trunk/ObjectHandler/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+#    ( "C:/erik/projects/trunk/QuantLibXL/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+#)
+
+COPY_MESSAGE = 'copy from:\n%s\ncopy to:\n%s\n'
 
 def deleteGlob(path):
     for file in glob.glob(path):
         print "deleting file\n" + file
         os.unlink(file)
 
-def copyGlob(sourcePattern, targetPath):
-    for sourceFile in glob.glob(sourcePattern):
-        print "copy from =\n" + sourceFile
-        print "copy to =\n" + targetPath
-        shutil.copy(sourceFile, targetPath)
+cwd = re.sub(r'\\', '/', os.getcwd())
 
-for (sourceDir, targetDir) in SOURCE_TARGET_DIRS:
-    targetDir = os.getcwd() + "/" + targetDir
-    deleteGlob(targetDir + "/" + DELETE_PATTERN)
-    copyGlob(sourceDir + "/" + COPY_PATTERN, targetDir)
+print "\nDeleting old copies of XLLs...\n"
 
-raw_input('copy complete. press any key to exit.')
+for sourceDir, targetDir, xllName in SOURCE_TARGET_FILE_LIST:
+    deleteGlob(cwd + "/" + targetDir + "/*.xll")
+
+print "\nDone.\n"
+
+print "Copying XLLs...\n"
+
+for (sourceDir, targetDir, xllName) in SOURCE_TARGET_FILE_LIST:
+    sourceFile = sourceDir + "/" + xllName
+    if not os.path.exists(sourceFile):
+        raw_input("Error - nonexistent file:\n" + sourceFile)
+        sys.exit()
+    targetFile = cwd + "/" + targetDir + "/" + xllName
+    print COPY_MESSAGE % (sourceFile, targetFile)
+    shutil.copy(sourceFile, targetFile)
+
+raw_input('Done, press any key to exit.\n')
+
