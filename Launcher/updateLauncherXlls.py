5b5e161b887f1474accc4cbb1912aa1e842f942e
Ferdinando Ametrano <nando@ametrano.net>
2007-07-11 17:51:29 +0000

    binaries to be packeaged in the Luncher should always be taken from the network distribution, not from the local hard drive

--- Launcher/updateLauncherXlls.py
+++ Launcher/updateLauncherXlls.py
@@ -4,21 +4,25 @@ import re
 import shutil
 import glob
 
-#SOURCE_TARGET_FILE_LIST = (
-#    ( "C:/Projects/QuantLibSVN/DevCycle/Rev10747/QuantLibXL/xll", "Addins/01 Production", "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
-#    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11190/QuantLibXL/xll", "Addins/02 Pre-Production", "QuantLibXL-vc80-mt-s-0_9_0.xll" ),
-#    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11705/ObjectHandler/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
-#    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11705/QuantLibXL/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
-#    ( "C:/Projects/QuantLibSVN/DevCycle/Rev11705/SensitivityAnalysis/saohxll/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
-#)
-
 SOURCE_TARGET_FILE_LIST = (
-    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev10747/QuantLibXL/xll", "Addins/01 Production", "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
-    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11190/QuantLibXL/xll", "Addins/02 Pre-Production", "QuantLibXL-vc80-mt-s-0_9_0.xll" ),
-    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11705/ObjectHandler/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
-    ( "C:/katiuscia/Projects/QuantLibSVN/DevCycle/Rev11705/QuantLibXL/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
-    ( "C:/katiuscia/projects/QuantlibSVN/DevCycle/Rev11705/SensitivityAnalysis/saohxll/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev10747/xll", "Addins/01 Production",     "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
+
+    ( "X:/Apps/Appsscript/CabotoXL/Rev11190/xll", "Addins/02 Pre-Production", "QuantLibXL-vc80-mt-s-0_9_0.xll" ),
+
+    ( "X:/Apps/Appsscript/CabotoXL/Rev11705/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev11705/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev11705/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
 )
+
+#SOURCE_TARGET_FILE_LIST = (
+#    ( "X:/Apps/Appsscript/CabotoXL/Rev10747/xll", "Addins/01 Production", "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
+
+#    ( "X:/Apps/Appsscript/CabotoXL/Rev11190/xll", "Addins/02 Pre-Production", "QuantLibXL-vc80-mt-s-0_9_0.xll" ),
+
+#    ( "X:/Apps/Appsscript/CabotoXL/Rev11705/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+#    ( "X:/Apps/Appsscript/CabotoXL/Rev11705/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+#    ( "X:/Apps/Appsscript/CabotoXL/Rev11705/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
+#)
 
 #SOURCE_TARGET_FILE_LIST = (
 #    ( "C:/erik/projects/trunk/QuantLibXL/xll", "Addins/01 Production", "QuantLibXL-vc80-mt-s-0_8_0.xll" ),
