e34efbccd396ffd95453e309ba6ad9e10bf902d4
Ferdinando Ametrano <nando@ametrano.net>
2007-07-25 08:40:40 +0000

    more default properties added (Luigi would you please check them and run on the trunk? thx). It can help to have the following in your SVN config file: enable-auto-props = yes
    ### Section for configuring automatic properties.
    [auto-props]
    *.1 = svn:eol-style=LF
    *.R = svn:eol-style=native
    *.ac = svn:eol-style=LF
    *.am = svn:eol-style=LF
    *.bat = svn:eol-style=native
    *.c = svn:eol-style=native
    *.cmake = svn:eol-style=native
    *.cmd = svn:eol-style=CRLF
    *.cpp = svn:eol-style=native
    *.cs = svn:eol-style=native
    *.csproj = svn:eol-style=CRLF
    *.css = svn:eol-style=native
    *.dev = svn:eol-style=CRLF
    *.docs = svn:eol-style=native
    *.doxy = svn:eol-style=native
    *.dsp = svn:eol-style=CRLF
    *.dsw = svn:eol-style=CRLF
    *.dtd = svn:eol-style=native
    *.el = svn:eol-style=native
    *.gif = svn:mime-type=image/gif
    *.h = svn:eol-style=native
    *.hpp = svn:eol-style=native
    *.htm = svn:eol-style=native
    *.html = svn:eol-style=native
    *.i = svn:eol-style=native
    *.ico = svn:mime-type=image/x-icon;svn:needs-lock
    *.in = svn:eol-style=LF
    *.ipp = svn:eol-style=native
    *.jam = svn:eol-style=native
    *.java = svn:eol-style=native
    *.jpg = svn:mime-type=image/jpeg;svn:needs-lock
    *.m4 = svn:eol-style=LF
    *.nsi = svn:eol-style=native
    *.pdf = svn:mime-type=application/pdf
    *.pl = svn:eol-style=native
    *.png = svn:mime-type=image/png;svn:needs-lock
    *.py = svn:eol-style=native
    *.qbk = svn:eol-style=native
    *.rb = svn:eol-style=native
    *.rst = svn:eol-style=native
    *.scm = svn:eol-style=native
    *.sh = svn:eol-style=native;svn:executable
    *.sln = svn:eol-style=CRLF
    *.ss = svn:eol-style=native
    *.tex = svn:eol-style=native
    *.tpp = svn:eol-style=native
    *.txt = svn:eol-style=native
    *.vcproj = svn:eol-style=CRLF
    *.xhtml = svn:eol-style=native
    *.xla = svn:mime-type=application/vnd.ms-excel;svn:needs-lock
    *.xll = svn:mime-type=application/vnd.ms-excel;svn:needs-lock
    *.xls = svn:mime-type=application/vnd.ms-excel;svn:needs-lock
    *.xml = svn:eol-style=native
    Makefile = svn:eol-style=native
    README = svn:eol-style=native
    configure = svn:eol-style=native
    stub.* = svn:eol-style=native

--- dev_tools/fix_svn_properties.sh
+++ dev_tools/fix_svn_properties.sh
@@ -1,31 +1,50 @@
 #!/bin/sh
 # text files
 find . -name '*.[hc]pp' -o -name '*.[hc]' \
-    -o -name '*.html' -o -name '*.css' -o -name '*.docs' -o -name '*.doxy' \
-    -o -name '*.el' \
-    -o -iname '*.txt' -o -name '*.tex' \
-    -o -name '*.xml' -o -name 'stub.*' \
+    -o -name '*.xhtml' -o -name '*.html' -o -name '*.htm' -o -name '*.css' \
+    -o -name '*.docs' -o -name '*.doxy' -o -name '*.qbk' \
+    -o -name '*.el' -o -name '*.bat' -o -name '*.sh' -o -name '*.cmake' \
+    -o -name '*.ipp' -o -name '*.jam' -o -name '*.tpp' \
+    -o -iname '*.txt' -o -name '*.tex' -o -name '*.dtd' \
+    -o -name '*.xml' -o -name 'stub.*' -o -name '*.rst' \
     -o -name '*.i' -o -name '*.py' -o -name '*.rb' -o -name '*.R' \
     -o -iname '*.pl' -o -name '*.scm' -o -name '*.ss' \
-    -o -name '*.cs' -o -name '*.java' \
+    -o -name '*.cs' -o -name '*.java' -o -name '*.nsi' \
+    -o -name '*.tpp' -o -name '*.Makefile' -o -name '*.README' \
+    -o -name '*.configure' \
  | xargs -n 1 svn propset svn:eol-style native
 # these should have UNIX eol, even when extracted from a .zip
 find . -name '*.a[cm]' -o -name '*.m4' -o -name '*.in' \
-    -o -name '*.sh' -o -name '*.1' \
+    -o -name '*.1' \
  | xargs -n 1 svn propset svn:eol-style LF
 # these should have Windows eol, even when extracted from a .tar.gz
-find . -name '*.dev' -o -name '*.nsi' -o -name '*.sln' \
+find . -name '*.dev' -o -name '*.sln' \
     -o -name '*.vcproj' -o -name '*.csproj' \
-    -o -name '*.cmd' -o -name '*.bat' \
+    -o -name '*.cmd' \
     -o -name '*.dsw' -o -name '*.dsp' \
  | xargs -n 1 svn propset svn:eol-style CRLF
 # these should have svn:needs-lock
-find . -name '*.png' -o -name '*.jpg' -o -name '*.xls' \
-    -o -name '*.xla' -o -name '*.xll' \
+find . -name '*.png' -o -name '*.jpg' -o -name '*.ico' \
+    -o -name '*.xls' -o -name '*.xla' -o -name '*.xll' \
  | xargs -n 1 svn propset svn:needs-lock 1
 # these should have svn:mime-type application/vnd.ms-excel
 find . -name '*.xla' -o -name '*.xll' -o -name '*.xls' \
  | xargs -n 1 svn propset svn:mime-type application/vnd.ms-excel
+# these should have svn:mime-type application/pdf
+find . -name '*.pdf' \
+ | xargs -n 1 svn propset svn:mime-type application/pdf
 # these should have svn:executable
 find . -name '*.sh' \
  | xargs -n 1 svn propset svn:executable 1
+# these should have svn:mime-type image/gif
+find . -name '*.gif' \
+ | xargs -n 1 svn propset svn:mime-type image/gif
+# these should have svn:mime-type image/x-icon
+find . -name '*.ico' \
+ | xargs -n 1 svn propset svn:mime-type image/x-icon
+# these should have svn:mime-type image/jpeg
+find . -name '*.jpg' \
+ | xargs -n 1 svn propset svn:mime-type image/jpeg
+# these should have svn:mime-type image/png
+find . -name '*.png' \
+ | xargs -n 1 svn propset svn:mime-type image/png
