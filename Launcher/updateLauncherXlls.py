d608a1d2422f8e1faf76c28594610cb3c0000a25
Eric Ehlers <eric.ehlers@nazcatech.be>
2007-10-05 12:53:38 +0000

    Remove some of the hard coded directories

--- Launcher/updateLauncherXlls.py
+++ Launcher/updateLauncherXlls.py
@@ -1,9 +1,16 @@
 import sys
 import os
-import re
 import shutil
 import glob
 
+# Current working dir
+cwd = os.getcwd().replace('\\', '/')
+# Our subpath from svn trunk - must be updated if this script is moved
+relativePath = 'Launcher'
+# The path to the trunk directory
+trunkPath = cwd[0:len(cwd)-len(relativePath)]
+
+# Use these values to update from production
 SOURCE_TARGET_LIST = (
     ( "X:/Apps/Appsscript/CabotoXL/Rev12288/xll", "Addins/01 Production", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
     ( "X:/Apps/Appsscript/CabotoXL/Rev12288/xll", "Addins/01 Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
@@ -18,26 +25,26 @@ SOURCE_TARGET_LIST = (
     ( "X:/Apps/Appsscript/CabotoXL/Rev12939/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
 )
 
+# Use these values to update from subversion
 #SOURCE_TARGET_LIST = (
-#    ( "C:/erik/projects/trunk/QuantLibXL/xll",                  "Addins/01 Production", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
-#    ( "C:/erik/projects/trunk/ObjectHandler/xll",               "Addins/01 Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
-#    ( "C:/erik/projects/trunk/SensitivityAnalysis/saohxll/xll", "Addins/01 Production", "saohxll-vc80-mt-0_1_9.xll" ),
-#    ( "C:/erik/projects/trunk/QuantLibXL/xll",                  "Addins/02 Pre-Production", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
-#    ( "C:/erik/projects/trunk/ObjectHandler/xll",               "Addins/02 Pre-Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
-#    ( "C:/erik/projects/trunk/SensitivityAnalysis/saohxll/xll", "Addins/02 Pre-Production", "saohxll-vc80-mt-0_1_9.xll" ),
-#    ( "C:/erik/projects/trunk/QuantLibXL/xll",                  "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
-#    ( "C:/erik/projects/trunk/ObjectHandler/xll",               "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
-#    ( "C:/erik/projects/trunk/SensitivityAnalysis/saohxll/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
+#    ( trunkPath + "QuantLibXL/xll",                  "Addins/01 Production", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+#    ( trunkPath + "ObjectHandler/xll",               "Addins/01 Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+#    ( trunkPath + "SensitivityAnalysis/saohxll/xll", "Addins/01 Production", "saohxll-vc80-mt-0_1_9.xll" ),
+#    ( trunkPath + "QuantLibXL/xll",                  "Addins/02 Pre-Production", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+#    ( trunkPath + "ObjectHandler/xll",               "Addins/02 Pre-Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+#    ( trunkPath + "SensitivityAnalysis/saohxll/xll", "Addins/02 Pre-Production", "saohxll-vc80-mt-0_1_9.xll" ),
+#    ( trunkPath + "QuantLibXL/xll",                  "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+#    ( trunkPath + "ObjectHandler/xll",               "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+#    ( trunkPath + "SensitivityAnalysis/saohxll/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
 #)
 
 COPY_MESSAGE = 'copy from:\n%s\ncopy to:\n%s\n'
 
 def deleteGlob(path):
     for file in glob.glob(path):
+        file = file.replace('\\', '/')
         print "deleting file\n" + file
         os.unlink(file)
-
-cwd = re.sub(r'\\', '/', os.getcwd())
 
 print "\nDeleting old copies of XLLs...\n"
 
