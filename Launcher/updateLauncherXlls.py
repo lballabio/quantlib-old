2fb44b28f88db3e7ed246edd4600887409c68807
Ferdinando Ametrano <nando@ametrano.net>
2007-10-25 16:14:30 +0000

    getting ready for next release

--- Launcher/updateLauncherXlls.py
+++ Launcher/updateLauncherXlls.py
@@ -16,13 +16,13 @@ SOURCE_TARGET_LIST = (
     ( "X:/Apps/Appsscript/CabotoXL/Rev12288/xll", "Addins/01 Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
     ( "X:/Apps/Appsscript/CabotoXL/Rev12288/xll", "Addins/01 Production", "saohxll-vc80-mt-0_1_9.xll" ),
 
-    ( "X:/Apps/Appsscript/CabotoXL/Rev12614/xll", "Addins/02 Pre-Production", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
-    ( "X:/Apps/Appsscript/CabotoXL/Rev12614/xll", "Addins/02 Pre-Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
-    ( "X:/Apps/Appsscript/CabotoXL/Rev12614/xll", "Addins/02 Pre-Production", "saohxll-vc80-mt-0_1_9.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev12939/xll", "Addins/02 Pre-Production", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev12939/xll", "Addins/02 Pre-Production", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev12939/xll", "Addins/02 Pre-Production", "saohxll-vc80-mt-0_1_9.xll" ),
 
-    ( "X:/Apps/Appsscript/CabotoXL/Rev12939/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
-    ( "X:/Apps/Appsscript/CabotoXL/Rev12939/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
-    ( "X:/Apps/Appsscript/CabotoXL/Rev12939/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev13059/xll", "Addins/03 Testing", "QuantLibXLDynamic-vc80-mt-0_9_0.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev13059/xll", "Addins/03 Testing", "ObjectHandler-xll-vc80-mt-0_9_0.xll" ),
+    ( "X:/Apps/Appsscript/CabotoXL/Rev13059/xll", "Addins/03 Testing", "saohxll-vc80-mt-0_1_9.xll" ),
 )
 
 # Use these values to update from subversion
