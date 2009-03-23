780abb06ac13aeb360ac2a2e1a694d36fa25385f
Luigi Ballabio <luigi.ballabio@gmail.com>
2009-03-23 16:04:58 +0000

    Upgraded to SWIG 1.3.39

--- QuantLib-SWIG/CSharp/swig.cmd
+++ QuantLib-SWIG/CSharp/swig.cmd
@@ -1,1 +1,1 @@
-swig.exe -csharp -c++ -I..\SWIG -outdir csharp -namespace QuantLib -o cpp\quantlib_wrap.cpp ..\SWIG\quantlib.i
+swig.exe -csharp -c++ -outdir csharp -namespace QuantLib -o cpp\quantlib_wrap.cpp ..\SWIG\quantlib.i
