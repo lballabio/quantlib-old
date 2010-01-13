5b649aa2c1939b8b68777b68b72617bbea0d9107
Ferdinando Ametrano <nando@ametrano.net>
2010-01-13 17:46:14 +0000

    merged up to r16999 of branches/R01000x-branch into trunk, respecting ancestry

--- QuantLibAddin/qlo/qladdindefines.hpp
+++ QuantLibAddin/qlo/qladdindefines.hpp
@@ -28,26 +28,26 @@
 #endif
 
 #include <oh/ohdefines.hpp>
-#if OBJHANDLER_HEX_VERSION < 0x000909f0
+#if OBJHANDLER_HEX_VERSION < 0x010000f0
     #error using an old version of ObjectHandler, please update.
 #endif
 
 #include <ql/version.hpp>
-#if QL_HEX_VERSION < 0x000909f0
+#if QL_HEX_VERSION < 0x010000b2
     #error using an old version of QuantLib, please update.
 #endif
 
 //! version string
 #ifdef _DEBUG
-    #define QLADDIN_VERSION "0.9.9-debug"
+    #define QLADDIN_VERSION "1.0.0-debug"
 #else
-    #define QLADDIN_VERSION "0.9.9"
+    #define QLADDIN_VERSION "1.0.0"
 #endif
 
 //! version hexadecimal number
-#define QLADDIN_HEX_VERSION 0x000909f0
+#define QLADDIN_HEX_VERSION 0x010000f0
 //! version string for output lib name
-#define QLADDIN_LIB_VERSION "0_9_9"
+#define QLADDIN_LIB_VERSION "1_0_0"
 
 #if defined BOOST_MSVC
 #pragma warning(disable : 4996)     // Using a deprecated function
