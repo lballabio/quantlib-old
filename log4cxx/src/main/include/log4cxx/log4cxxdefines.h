61b63b4446353772f37a3a7e3d7bca632c4cb0e9
Luigi Ballabio <luigi.ballabio@gmail.com>
2010-10-13 07:43:59 +0000

    Tagged 1.0.1 release

---
+++ log4cxx/src/main/include/log4cxx/log4cxxdefines.h
@@ -0,0 +1,25 @@
+
+/*
+ Copyright (C) 2006, 2007, 2008 Eric Ehlers
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+// This file is #included by client applications to enforce version
+// number checks.  This package is based on log4cxx version 0.10.0, customized
+// for use by ObjectHandler.  We designate log4cxx hexidecimal version
+// "0x001000f3" for use with ObjectHandler version 1.0.0.
+
+#define LOG4CXX_VERSION 0x001000f3
+
