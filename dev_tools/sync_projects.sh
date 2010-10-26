83519c143ee34ba55984f2caa00570c9f2eba3c6
Luigi Ballabio <luigi.ballabio@gmail.com>
2010-10-26 09:12:14 +0000

    Updated sync_projects script to look into VC10 projects.

--- dev_tools/sync_projects.sh
+++ dev_tools/sync_projects.sh
@@ -2,7 +2,7 @@
 
 # execute this script from the root of an uncompressed QuantLib tarball
 
-# 1) get reference lists of distributed files (done with find; this is
+# get reference lists of distributed files (done with find; this is
 # why this script should be run from an uncompressed tarball created
 # with 'make dist', not from a working copy.)
 
@@ -11,7 +11,7 @@ find ql -name '*.[hc]pp' -or -name '*.[hc]' \
 find test-suite -name '*.[hc]pp' | grep -v 'quantlibbenchmark' \
 | sort > test-suite.ref.files
 
-# 2) extract file names from VC7 projects and clean up so that they
+# extract file names from VC7 projects and clean up so that they
 # have the same format as the reference lists.
 
 grep -o -E 'RelativePath=".*"' QuantLib_vc7.vcproj \
@@ -22,7 +22,7 @@ grep -o -E 'RelativePath=".*">' test-suite/testsuite_vc7.vcproj \
 | awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
 | sed -e 's|^|test-suite/|' | sort > test-suite.vc7.files
 
-# 3) Same for VC8...
+# Same for VC8...
 
 grep -o -E 'RelativePath=".*"' QuantLib_vc8.vcproj \
 | awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
@@ -32,7 +32,7 @@ grep -o -E 'RelativePath=".*"' test-suite/testsuite_vc8.vcproj \
 | awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
 | sed -e 's|^|test-suite/|' | sort > test-suite.vc8.files
 
-# 4) ...VC9...
+# ...VC9...
 
 grep -o -E 'RelativePath=".*"' QuantLib_vc9.vcproj \
 | awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
@@ -42,7 +42,25 @@ grep -o -E 'RelativePath=".*"' test-suite/testsuite_vc9.vcproj \
 | awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
 | sed -e 's|^|test-suite/|' | sort > test-suite.vc9.files
 
-# 5) ...and Dev-C++.
+# ...VC10...
+
+grep -o -E 'Include=".*\.[hc]p*"' QuantLib_vc10.vcxproj \
+| awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
+| sort > ql.vc10.files
+
+grep -o -E 'Include=".*\.[hc]p*"' test-suite/testsuite_vc10.vcxproj \
+| awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
+| sed -e 's|^|test-suite/|' | sort > test-suite.vc10.files
+
+grep -o -E 'Include=".*\.[hc]p*"' QuantLib_vc10.vcxproj.filters \
+| awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
+| sort > ql.vc10.filters
+
+grep -o -E 'Include=".*\.[hc]p*"' test-suite/testsuite_vc10.vcxproj.filters \
+| awk -F'"' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
+| sed -e 's|^|test-suite/|' | sort > test-suite.vc10.filters
+
+# ...and Dev-C++.
 
 grep -o -E 'FileName=.*' QuantLib.dev \
 | grep -v 'QuantLib\.dev' \
@@ -54,7 +72,7 @@ grep -o -E 'FileName=.*' test-suite/testsuite.dev \
 | awk -F'=' '{ print $2 }' | sed -e 's|\\|/|g' | sed -e 's|^./||' \
 | sed -e 's|^|test-suite/|' | sort > test-suite.devcpp.files
 
-# 6) write out differences...
+# write out differences...
 
 echo 'Visual Studio 7:' > sync.report
 diff -b ql.vc7.files ql.ref.files >> sync.report
@@ -74,15 +92,27 @@ diff -b test-suite.vc9.files test-suite.ref.files >> sync.report
 
 echo '' >> sync.report
 echo '' >> sync.report
+echo 'Visual Studio 10:' >> sync.report
+echo 'project:' >> sync.report
+diff -b ql.vc10.files ql.ref.files >> sync.report
+diff -b test-suite.vc10.files test-suite.ref.files >> sync.report
+echo 'filters:' >> sync.report
+diff -b ql.vc10.filters ql.ref.files >> sync.report
+diff -b test-suite.vc10.filters test-suite.ref.files >> sync.report
+
+echo '' >> sync.report
+echo '' >> sync.report
 echo 'Dev-C++:' >> sync.report
 diff -b ql.devcpp.files ql.ref.files >> sync.report
 diff -b test-suite.devcpp.files test-suite.ref.files >> sync.report
 
-# 7) and cleanup
+# ...and cleanup
 rm -f ql.ref.files test-suite.ref.files
 rm -f ql.vc7.files test-suite.vc7.files
 rm -f ql.vc8.files test-suite.vc8.files
 rm -f ql.vc9.files test-suite.vc9.files
+rm -f ql.vc10.files test-suite.vc10.files
+rm -f ql.vc10.filters test-suite.vc10.filters
 rm -f ql.devcpp.files test-suite.devcpp.files
 
 cat sync.report
