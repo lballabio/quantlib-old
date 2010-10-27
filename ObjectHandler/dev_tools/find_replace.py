681dd3ab9df0740c92d1675a7c630d7fbde62cf5
Eric Ehlers <eric.ehlers@nazcatech.be>
2010-10-27 19:11:42 +0000

    - Replace some unnecessary raw strings (r'') with literal strings ('') - Clarify comments

--- ObjectHandler/dev_tools/find_replace.py
+++ ObjectHandler/dev_tools/find_replace.py
@@ -13,10 +13,6 @@
 # Plus optionally
 #        v - verbose
 #
-# At a DOS command prompt this script may be invoked as follows:
-#
-# "C:\Program Files\Python24\python.exe" find_replace.py -s
-#
 # Settings within this script:
 #
 # ROOT_DIR
@@ -45,62 +41,72 @@ import re
 import getopt
 import shutil
 
+# ROOT_DIR - The root folder of the source tree from which you want the
+# find/replace to begin.
+#ROOT_DIR = 'C:/erik/projects/trunk/QuantLibXL'
 ROOT_DIR = '/usr/local/erik/projects/trunk/QuantLibXL'
 
-# callback functions - called from regexes which require multiple passes
+# CALLBACK FUNCTIONS - Called from regexes which require multiple passes
 
-# convert case
+# Convert case
 def toLower(m): return m.group(0).lower()
 
-# replace pre increment/decrement with post increment/decrement
-regex2 = re.compile(r'(\w+?)\+\+')
-repl2 = r'++\1'
-regex3 = re.compile(r'(\w+?)--')
-repl3 = r'--\1'
-def sub1(m):
-    x = regex2.sub(repl2, m.group(2))
-    x = regex3.sub(repl3, x)
+# Replace pre increment/decrement with post increment/decrement
+regex1 = re.compile(r'(\w+?)\+\+')
+regex2 = re.compile(r'(\w+?)--')
+def callback_example(m):
+    x = regex1.sub('++\1', m.group(2))
+    x = regex2.sub('--\1', x)
     return m.group(1) + x + ')'
 
-# SUBSTITUTIONS - a list of regexes to be performed
+# SUBSTITUTIONS - A list of regexes to be performed.
+# Each substitution is in the format
+#   (re.compile('find text'), 'replace text'),
 
 SUBSTITUTIONS = (
 
-#   place the active regexes here,
-#   using the commented examples below
+## Uncomment and modify the examples as required.
 
-#    (re.compile(r'aaa'), r'bbb'),
+##  1) Simple
+##  Straight find/replace.
+#   (re.compile('aaa'), 'bbb'),
 
-#   simple
-#    (re.compile(r'include <Addins/C++'), r'include <Addins/Cpp'),
+##  2) Group
+##  Use parentheses to indicate group(s) in the find text.
+##  Use \x in the replace text to refer to a group, where x = group number.
+##  Replace text must be a raw string r'' instead of normal string ''.
+#   (re.compile('ccc(.*)ccc'), r'ddd\1ddd'),
 
-#   group (\1 requires r'' not '')
-#    (re.compile(r"<Rule (.*)>"), r"<Rule \1><SubRules>"),
+##  3) Newline flag
+##  Use re.S to indicate that . matches newline.
+##  This allows you to perform substitutions that span lines.
+#   (re.compile('eee.*eee', re.S), 'fff'),
 
-#   newline flag re.S
-#    (re.compile(r"details\.\s.*?'(.*?)'", re.S), ''),
+##  4) Multiline flag
+##  Use re.M to anchor ^ and $ to begin/end of lines within buffer.
+#   (re.compile('^ggg.*ggg$', re.M | re.S), 'hhh'),
 
-#   multiline (^$) and newline (.)
-#    (re.compile(r'xxx^xxx.*xxx$xxx', re.M | re.S), 'yyy'),
+##  5) Conversion function
+##  Instead of replacement text, provide name of conversion function.
+#   (re.compile('abcDEFghi'), toLower),
 
-#   call a conversion function
-#    (re.compile(r'^import .*?$', re.M), toLower),
-
-#   frequently used
-    (re.compile(r'1_0_0'), '1_1_0'),
-    (re.compile(r'1\.0\.0'), '1.1.0'),
-    (re.compile(r'0x010000'), '0x010100'),
-    (re.compile(r'R010000f0'), 'R010001f0'),
-#    (re.compile(r'Major=1\nMinor=0\nRelease=0'), 'Major=1\nMinor=1\nRelease=0'),
-    (re.compile(r'0\.10\.0c'), '0.10.0d'),
-
+##  Frequently used
+    (re.compile('1_0_1'), '1_1_0'),
+    (re.compile('1\.0\.1'), '1.1.0'),
+    (re.compile('0x010001'), '0x010100'),
+    (re.compile('R010001f0'), 'R010100f0'),
+#   (re.compile('0\.10\.0c'), '0.10.0d'),
 )
 
 # INCLUDE_FILES
 # Regexes to indicate names of files to be processed by the find/replace.
+# Leave this list empty to process all files in the directory tree
+# except for those excluded by IGNORE_FILES.
 
 INCLUDE_FILES = (
+
 #    re.compile(r'^.+\.[ch]pp$'),
+
 )
 
 # IGNORE_FILES
@@ -108,63 +114,60 @@ INCLUDE_FILES = (
 
 IGNORE_FILES = (
 
-    re.compile(r'^\.'),
-    re.compile(r'^.+\.bmp$'),
-    re.compile(r'^.+\.exe$'),
-    re.compile(r'^.+\.exp$'),
-    re.compile(r'^.+\.ico$'),
-    re.compile(r'^.+\.jpg$'),
-    re.compile(r'^.+\.la$'),
-    re.compile(r'^.+\.lib$'),
-    re.compile(r'^.+\.log$'),
-    re.compile(r'^.+\.ncb$'),
-    re.compile(r'^.+\.o$'),
-    re.compile(r'^.+\.pdf$'),
-    re.compile(r'^.+\.plg$'),
-    re.compile(r'^.+\.png$'),
-    re.compile(r'^.+\.pyc$'),
-    re.compile(r'^.+\.xls$'),
-    re.compile(r'^.+~$'),
+    re.compile('^.+\.bmp$'),
+    re.compile('^.+\.exe$'),
+    re.compile('^.+\.exp$'),
+    re.compile('^.+\.ico$'),
+    re.compile('^.+\.jpg$'),
+    re.compile('^.+\.la$'),
+    re.compile('^.+\.lib$'),
+    re.compile('^.+\.log$'),
+    re.compile('^.+\.ncb$'),
+    re.compile('^.+\.o$'),
+    re.compile('^.+\.pdf$'),
+    re.compile('^.+\.plg$'),
+    re.compile('^.+\.png$'),
+    re.compile('^.+\.pyc$'),
+    re.compile('^.+\.xls$'),
+    re.compile('^.+~$'),
+    re.compile('^\.'),
 
-    re.compile(r'^Announce\.txt$'),
-    re.compile(r'^ChangeLog\.txt$'),
-    re.compile(r'^changes\..+$'),
-    re.compile(r'^config\.status$'),
-    re.compile(r'^configure$'),
-    re.compile(r'^design\.docs$'),
-    re.compile(r'^history\.docs$'),
-    re.compile(r'^libtool$'),
-    re.compile(r'^Makefile$'),
-    re.compile(r'^Makefile\.in$'),
-    re.compile(r'^News\.txt$'),
-    re.compile(r'^NEWS\.txt$'),
-    re.compile(r'^objecthandler\.cpp$'),
-    re.compile(r'^ohfunctions\.cpp$'),
-    re.compile(r'^todonando\.txt$'),
-
+    re.compile('^Announce\.txt$'),
+    re.compile('^ChangeLog\.txt$'),
+    re.compile('^changes\..+$'),
+    re.compile('^config\.status$'),
+    re.compile('^configure$'),
+    re.compile('^design\.docs$'),
+    re.compile('^history\.docs$'),
+    re.compile('^libtool$'),
+    re.compile('^Makefile$'),
+    re.compile('^Makefile\.in$'),
+    re.compile('^NEWS\.txt$'),
+    re.compile('^News\.txt$'),
+    re.compile('^objecthandler\.cpp$'),
+    re.compile('^ohfunctions\.cpp$'),
+    re.compile('^todonando\.txt$'),
 )
 
 # IGNORE_DIRS
 # Regexes to indicate directories to be ignored by the find/replace.
 
 IGNORE_DIRS = (
-    re.compile(r'^\.'),
-    re.compile(r'^autom4te\.cache$'),
-    #re.compile(r'^bak.*$'),
-    re.compile(r'^build$'),
-    re.compile(r'^configure$'),
-    re.compile(r'^dev_tools$'),
-    re.compile(r'^framework$'),
-    re.compile(r'^html$'),
-    re.compile(r'^Launcher$'),
-    re.compile(r'^lib$'),
-    re.compile(r'^log4cxx$'),
-    re.compile(r'^QuantLib-FpML$'),
-    re.compile(r'^QuantLib-site$'),
-    re.compile(r'^QuantLib-SWIG$'),
-    re.compile(r'^SensitivityAnalysis$'),
-    re.compile(r'^\.svn$'),
-    re.compile(r'^Workbooks$'),
+    re.compile('^\.'),
+    re.compile('^\.svn$'),
+    re.compile('^autom4te\.cache$'),
+    re.compile('^build$'),
+    re.compile('^configure$'),
+    re.compile('^dev_tools$'),
+    re.compile('^framework$'),
+    re.compile('^html$'),
+    re.compile('^Launcher$'),
+    re.compile('^lib$'),
+    re.compile('^log4cxx$'),
+    re.compile('^QuantLib$'),
+    re.compile('^QuantLib-site$'),
+    re.compile('^QuantLib-SWIG$'),
+    re.compile('^Workbooks$'),
 )
 
 def prompt_exit(msg='', status=0):
