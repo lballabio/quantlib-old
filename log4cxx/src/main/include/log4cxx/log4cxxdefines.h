12b1f04c30689069c714771c17796bb24f4db34f
Eric Ehlers <eric.ehlers@nazcatech.be>
2010-10-27 19:08:51 +0000

    Increment version number to 1.1.0.

--- log4cxx/src/main/include/log4cxx/log4cxxdefines.h
+++ log4cxx/src/main/include/log4cxx/log4cxxdefines.h
@@ -19,7 +19,7 @@
 // This file is #included by client applications to enforce version
 // number checks.  This package is based on log4cxx version 0.10.0, customized
 // for use by ObjectHandler.  We designate log4cxx hexidecimal version
-// "0x001000f3" for use with ObjectHandler version 1.0.0.
+// "0x001000f3" for use with ObjectHandler version 1.1.0.
 
 #define LOG4CXX_VERSION 0x001000f3
 
