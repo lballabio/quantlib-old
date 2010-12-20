c0ec211f435749ce55827611aa60e5dbc9abdd2b
Luigi Ballabio <luigi.ballabio@gmail.com>
2010-12-20 09:45:41 +0000

    Avoid false positive in inclusion checks.

--- dev_tools/check_all_inclusions.sh
+++ dev_tools/check_all_inclusions.sh
@@ -4,5 +4,5 @@
 
 find ./ql -name *.[ch]pp \
 | grep -v '/qldefines\.hpp$' | grep -v '/auto_link\.hpp$' | grep -v 'config' \
-| xargs -n 1 ../dev_tools/check_inclusions.py
+| grep -v '/mathconstants\.hpp$' | xargs -n 1 ../dev_tools/check_inclusions.py
 
