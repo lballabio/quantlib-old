303b169ba21f054068327715d87a7d4964923337
Klaus Spanderen <klaus@spanderen.de>
2012-03-18 21:40:22 +0000

    changed indent

--- QuantLib-SWIG/Scala/examples/RandomNumbers.scala
+++ QuantLib-SWIG/Scala/examples/RandomNumbers.scala
@@ -21,32 +21,31 @@ package examples;
 import org.quantlib.{Array => QArray, _}
 
 object RandomNumbers {
-	def main(args: Array[String]) : Unit = {
+    def main(args: Array[String]) : Unit = {
 
         try {
             System.loadLibrary("QuantLibJNI")
         } 
-		catch {
-		    case ex: UnsatisfiedLinkError => {
-		  	    println("please check your LD_LIBRARY_PATH variable")
-			    throw ex
+        catch {
+            case ex: UnsatisfiedLinkError => {
+                  println("please check your LD_LIBRARY_PATH variable")
+                throw ex
             }
-		}
+        }
 
-		val rng = new GaussianRandomGenerator(
-									new UniformRandomGenerator(12345))
-		
-		val fmt = "%8d  %10.6f       %10.6f      %10.6f %10.6f\n"
-		printf("  #rngs\t  mean(expected)  error(expected)\tmean\t  error\n");
-		(1 to 10).foreach(i => {
-				val nRngs = 1024*(1 << i)
-				val st = new IncrementalStatistics()
+        val rng = new GaussianRandomGenerator(
+                                    new UniformRandomGenerator(12345))
+        
+        val fmt = "%8d  %10.6f       %10.6f      %10.6f %10.6f\n"
+        printf("  #rngs\t  mean(expected)  error(expected)\tmean\t  error\n");
+        (1 to 10).foreach(i => {
+            val nRngs = 1024*(1 << i)
+            val st = new IncrementalStatistics()
 
-				(0 until nRngs).foreach(_ => st.add(rng.nextValue()))
+            (0 until nRngs).foreach(_ => st.add(rng.nextValue()))
 
-				printf(fmt, nRngs, 0.0, 1/scala.math.sqrt(nRngs),
-					   st.mean(), st.errorEstimate())
-			}
-		)
-	}
+            printf(fmt, nRngs, 0.0, 1/scala.math.sqrt(nRngs),
+                   st.mean(), st.errorEstimate())
+        } )
+    }
 }
