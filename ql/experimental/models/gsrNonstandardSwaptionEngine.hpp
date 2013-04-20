6183ca8c9ebaeaa4e90e6821a3439d970c4407e3
Peter Caspers <pcaspers@vodafone.de>
2013-04-20 15:35:10 +0200

    gsr model implementation

---
+++ ql/experimental/models/gsrNonstandardSwaptionEngine.hpp
@@ -0,0 +1,99 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2013 Peter Caspers
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file gsrNonstandardSwaptionEngine.hpp
+    \brief
+*/
+
+#ifndef quantlib_pricers_gsr_nonstandardswaption_hpp
+#define quantlib_pricers_gsr_nonstandardswaption_hpp
+
+#include <ql/experimental/models/gsr/nonstandardswaption.hpp>
+#include <ql/experimental/models/gsr/gsr.hpp>
+
+#include <ql/pricingengines/genericmodelengine.hpp>
+#include <ql/termstructures/volatility/swaption/swaptionvolstructure.hpp>
+
+
+namespace QuantLib {
+
+    //! Gsr non standard swaption engine
+    /*! \ingroup swaptionengines
+		All fixed coupons with start date greater or equal to the respective option expiry are considered to be part of the exercise into right.
+		All float coupons with start date greater or equal to the respective option expiry are consideres to be part of the exercise into right.
+		\warning Cash settled swaptions are not supported
+
+		TODO at the moment the matching algorithm to find the representative swaption is restricted to the models monocurve setup at the moment. Extend this to multicurve ? Does that have an effect ?
+		\warning the standardSwapBase index should have associated forward and discount curves. These curves are used for setup of the swaption helper.
+		         this means that the market price of the calibration instrument is calculated using these curves. Therefore the model price must be calculated
+	             using the same curves, otherwise the calibration gets incosistent, i.e. the pricing engine used for model calibration has to be capable of using
+				 the same curves as associated to the index. Also the volatility structure passed to construct the calibration helper should use curves that are consistent with the model calibration curve setup.
+    */
+
+    class GsrNonstandardSwaptionEngine
+        : public GenericModelEngine<Gsr,
+                                    NonstandardSwaption::arguments,
+                                    NonstandardSwaption::results > {
+      public:
+
+
+        GsrNonstandardSwaptionEngine(
+                         const boost::shared_ptr<Gsr>& model,
+						 const int integrationPoints=64,
+						 const Real stddevs=7.0,
+						 const bool extrapolatePayoff=true,
+						 const bool flatPayoffExtrapolation=false,
+						 const Handle<YieldTermStructure>& discountCurve=Handle<YieldTermStructure>(),
+						 const Handle<YieldTermStructure>& forwardCurve=Handle<YieldTermStructure>())
+        : GenericModelEngine<Gsr,
+                             NonstandardSwaption::arguments,
+                             NonstandardSwaption::results>(model),
+		  integrationPoints_(integrationPoints) , stddevs_(stddevs), extrapolatePayoff_(extrapolatePayoff), flatPayoffExtrapolation_(flatPayoffExtrapolation),
+		discountYts_(discountCurve), forwardYts_(forwardCurve) {
+		
+			  registerWith(discountYts_);
+			  registerWith(forwardYts_);
+		
+		}
+        void calculate() const;
+
+      
+	private:
+		const int integrationPoints_;
+		const Real stddevs_;
+		const bool extrapolatePayoff_,flatPayoffExtrapolation_;
+		const Handle<YieldTermStructure> discountYts_;
+		const Handle<YieldTermStructure> forwardYts_;
+
+		friend class NonstandardSwaption;
+
+		Disposable<std::vector<boost::shared_ptr<CalibrationHelper>>> calibrationBasket(boost::shared_ptr<SwapIndex> standardSwapBase,
+			boost::shared_ptr<SwaptionVolatilityStructure> swaptionVolatility,
+			const NonstandardSwaption::CalibrationBasketType basketType = NonstandardSwaption::MaturityStrikeByDeltaGamma ) const;
+		
+		Real fixedLegNpv(Size idx, Date& referenceDate, Real y) const;
+		Real floatingLegNpv(Size idx, Date& referenceDate, Real y) const;
+
+    };
+
+}
+
+
+#endif
+
---
+++ ql/experimental/models/gsrSwaptionEngine.cpp
@@ -0,0 +1,147 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2013 Peter Caspers
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#include <ql/experimental/models/gsr/gsrSwaptionEngine.hpp>
+
+namespace QuantLib {
+
+    void GsrSwaptionEngine::calculate() const {
+
+		boost::shared_ptr<Interpolation> spreadF_, spreadD_; // zero rate spreads to market curves
+
+		// build spread curves, which we just represent as interpolated differences between the respective curves zero rates
+		
+		const Size yN = 24; // per year discretization steps for spread interpolation objects
+
+		Real T = model_->forwardMeasureTime();
+
+		Size N=(int)(yN*T+0.5);
+		std::vector<Real> t(N);
+		std::vector<Real> yF(N),yD(N);
+		for(Size i=0;i<N;++i) {
+			t[i]=i*(T/((Real)N-1));
+		}
+
+		for(Size i=0;i<N;++i) yD[i] = discountYts_.empty() ? 0.0 : 
+			                              discountYts_->zeroRate(t[i],QuantLib::Continuous,QuantLib::NoFrequency,true) -
+										  model_->termStructure()->zeroRate(t[i],QuantLib::Continuous,QuantLib::NoFrequency,true);
+
+		for(Size i=0;i<N;++i) yF[i] = forwardYts_.empty() ? 0.0 : 
+			                              forwardYts_->zeroRate(t[i],QuantLib::Continuous,QuantLib::NoFrequency,true) -
+										  model_->termStructure()->zeroRate(t[i],QuantLib::Continuous,QuantLib::NoFrequency,true);
+
+		spreadD_ = boost::shared_ptr<Interpolation>(new CubicInterpolation(t.begin(),t.end(),yD.begin(),CubicInterpolation::Spline,true,CubicInterpolation::Lagrange,0.0,CubicInterpolation::Lagrange,0.0));
+		spreadF_ = boost::shared_ptr<Interpolation>(new CubicInterpolation(t.begin(),t.end(),yF.begin(),CubicInterpolation::Spline,true,CubicInterpolation::Lagrange,0.0,CubicInterpolation::Lagrange,0.0));
+
+		spreadD_->enableExtrapolation();
+		spreadF_->enableExtrapolation();
+
+        QL_REQUIRE(arguments_.settlementType==Settlement::Physical,
+                   "cash-settled swaptions not yet implemented ...");
+
+		Date today = Settings::instance().evaluationDate();
+
+		if(arguments_.exercise->dates().back() <= today) {	// swaption is expired, possibly generated swap is not valued
+			results_.value = 0.0;
+			return;
+		}
+
+		int idx = arguments_.exercise->dates().size()-1;
+		int minIdxAlive = std::upper_bound(arguments_.exercise->dates().begin(), arguments_.exercise->dates().end(), today) - arguments_.exercise->dates().begin();
+
+		VanillaSwap swap = *arguments_.swap;
+		Option::Type type = arguments_.type==VanillaSwap::Payer ? Option::Call : Option::Put;
+		Schedule schedule = swap.fixedSchedule();
+		Schedule floatSchedule = swap.floatingSchedule();
+
+		Array npv0(2*integrationPoints_+1,0.0), npv1(2*integrationPoints_+1,0.0);
+		Array z = model_->yGrid(stddevs_, integrationPoints_);
+		Array p(z.size(),0.0);
+
+		Date expiry1 = Null<Date>(), expiry0;
+		Time expiry1Time = Null<Real>(), expiry0Time;
+
+		do {
+
+			if(idx == minIdxAlive-1)
+				expiry0 = today;
+			else
+				expiry0 = arguments_.exercise->dates()[idx];
+
+			expiry0Time = std::max(model_->termStructure()->timeFromReference(expiry0),0.0);
+
+			Size j1 = std::upper_bound(schedule.dates().begin(), schedule.dates().end(), expiry0 - 1 ) - schedule.dates().begin();
+			Size k1 = std::upper_bound(floatSchedule.dates().begin(), floatSchedule.dates().end(), expiry0 -1 ) - floatSchedule.dates().begin();
+
+			for(Size k=0; k < (expiry0 > today ? npv0.size() : 1); k++) {
+
+				Real price = 0.0;
+				if(expiry1Time != Null<Real>()) {
+					Array yg = model_->yGrid(stddevs_, integrationPoints_, expiry1Time, expiry0Time, expiry0Time > 0 ? z[k] : 0.0);
+					CubicInterpolation payoff0(z.begin(),z.end(),npv1.begin(),CubicInterpolation::Spline,true,CubicInterpolation::Lagrange,0.0,CubicInterpolation::Lagrange,0.0);
+					for(Size i=0;i<yg.size();i++) {
+						p[i] = payoff0(yg[i],true);
+					}
+					CubicInterpolation payoff1(z.begin(),z.end(),p.begin(),CubicInterpolation::Spline,true,CubicInterpolation::Lagrange,0.0,CubicInterpolation::Lagrange,0.0);
+					for(Size i=0;i<z.size()-1;i++) {
+						price += model_->gaussianShiftedPolynomialIntegral( 0.0, payoff1.cCoefficients()[i], payoff1.bCoefficients()[i], payoff1.aCoefficients()[i], p[i], z[i], z[i], z[i+1] );
+					}
+					if(extrapolatePayoff_) {
+							if(flatPayoffExtrapolation_) {
+								price += model_->gaussianShiftedPolynomialIntegral( 0.0, 0.0, 0.0, 0.0, p[z.size()-2], z[z.size()-2], z[z.size()-1], 100.0 );
+								price += model_->gaussianShiftedPolynomialIntegral( 0.0, 0.0, 0.0, 0.0, p[0], z[0], -100.0 , z[0] );
+							}
+							else {
+								if(type == Option::Call) price += model_->gaussianShiftedPolynomialIntegral( 0.0, payoff1.cCoefficients()[z.size()-2], payoff1.bCoefficients()[z.size()-2], payoff1.aCoefficients()[z.size()-2], p[z.size()-2], z[z.size()-2], z[z.size()-1], 100.0 );
+								if(type == Option::Put) price += model_->gaussianShiftedPolynomialIntegral( 0.0, payoff1.cCoefficients()[0], payoff1.bCoefficients()[0], payoff1.aCoefficients()[0], p[0], z[0], -100.0 , z[0] );
+							}
+					}	
+				}
+
+				npv0[k] = price;
+
+				if(expiry0 >today) {
+					Real floatingLegNpv = 0.0;
+					for(Size l=k1;l<arguments_.floatingCoupons.size();l++) {
+						floatingLegNpv += arguments_.nominal * arguments_.floatingAccrualTimes[l] *
+							                (arguments_.floatingSpreads[l]+model_->forwardRate(arguments_.floatingFixingDates[l],arguments_.swap->iborIndex(),expiry0,z[k],spreadF_)) * 
+											model_->zerobond(arguments_.floatingPayDates[l],expiry0,z[k],spreadD_);
+					}
+					Real fixedLegNpv = 0.0;
+					for(Size l=j1;l<arguments_.fixedCoupons.size();l++) {
+						fixedLegNpv += arguments_.fixedCoupons[l] * model_->zerobond(arguments_.fixedPayDates[l],expiry0,z[k],spreadD_);
+					}
+					//std::cout << expiry0Time << ";" << expiry1Time << ";" << k << ";" << npv0[k] << ";" << (floatingLegNpv-fixedLegNpv) / model_->numeraire(expiry0Time,z[k]) << ";" << model_->numeraire(expiry0Time,z[k]) << std::endl;
+					npv0[k] = std::max( npv0[k] * (npv0[k]>0.0 ? model_->numeraire(expiry0Time,z[k],spreadD_) : 0.0) , (type==Option::Call ? 1.0 : -1.0) * ( floatingLegNpv - fixedLegNpv ) ) / model_->numeraire(expiry0Time,z[k],spreadD_);
+				}
+
+			}
+
+			npv1.swap(npv0);
+			expiry1 = expiry0;
+			expiry1Time = expiry0Time;
+
+		} while(--idx >= minIdxAlive-1);
+
+		results_.value = npv1[0] * model_->numeraire(0.0,0.0,spreadD_);
+
+	}
+
+}
+
