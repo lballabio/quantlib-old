67a8d64e640750ce1b5a85546edacaf760c2a179
Peter <peter@peter-ThinkPad-W520.(none)>
2013-05-19 19:32:21 +0200

    enable gsr tests

--- test-suite/quantlibtestsuite.cpp
+++ test-suite/quantlibtestsuite.cpp
@@ -381,7 +381,7 @@ test_suite* init_unit_test_suite(int, char* []) {
     //test->add(LiborMarketModelTest::suite());
     //test->add(LiborMarketModelProcessTest::suite());
 
-	//test->add(GsrTest::suite());
+	test->add(GsrTest::suite());
 
     test->add(QUANTLIB_TEST_CASE(stopTimer));
 
