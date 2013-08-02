6028876eed38bab1b4ff360928e4e18efb3b7401
klausspanderen <klaus@spanderen.de>
2013-08-02 17:08:04 +0200

    changed formatting

--- QuantLib-SWIG/Java/examples/UnaryFunctions.java
+++ QuantLib-SWIG/Java/examples/UnaryFunctions.java
@@ -7,18 +7,18 @@ import org.quantlib.Brent;
 public class UnaryFunctions {
 
     public static void main(String[] args) throws java.lang.InterruptedException {
-	long beginTime = System.currentTimeMillis();
+    long beginTime = System.currentTimeMillis();
 
-	System.out.println("Integration result " +
-	    new GaussKronrodAdaptive(1e-8).calculate(
-		new UnaryFunctionDelegate() {
-		    public double value(double x) { return Math.sin(x); }
-		}, 0.0, Math.PI));
+    System.out.println("Integration result " +
+        new GaussKronrodAdaptive(1e-8).calculate(
+            new UnaryFunctionDelegate() {
+		        public double value(double x) { return Math.sin(x); }
+		    }, 0.0, Math.PI));
 
-	System.out.println("Brent Solver result " +
-	    new Brent().solve(
-	        new UnaryFunctionDelegate() {
-		    public double value(double x) { return Math.cos(x)-x; }
-		}, 1e-8, 0.5, 0.0, Math.PI));
+    System.out.println("Brent Solver result " +
+        new Brent().solve(
+            new UnaryFunctionDelegate() {
+                public double value(double x) { return Math.cos(x)-x; }
+        }, 1e-8, 0.5, 0.0, Math.PI));
     }
 }
