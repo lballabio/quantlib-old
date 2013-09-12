d09b6b5af6c7a60a5bdf0d28cf2d0cc2a576c605
Peter Caspers <pcaspers1973@gmail.com>
2013-09-12 22:48:58 +0200

    move zabr class, add zabr smile section

---
+++ QuantLib/ql/termstructures/volatility/zabrsmilesection.cpp
@@ -0,0 +1,106 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2013 Peter Caspers
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#include <ql/termstructures/volatility/zabrsmilesection.hpp>
+#include <ql/pricingengines/blackformula.hpp>
+
+namespace QuantLib {
+
+    ZabrSmileSection::ZabrSmileSection(Time timeToExpiry, Rate forward,
+                                       const std::vector<Real> &zabrParams,
+                                       const Evaluation evaluation)
+        : SmileSection(timeToExpiry), evaluation_(evaluation),
+          forward_(forward), params_(zabrParams) {
+
+        init();
+    }
+
+    ZabrSmileSection::ZabrSmileSection(const Date &d, Rate forward,
+                                       const std::vector<Real> &zabrParams,
+                                       const DayCounter &dc,
+                                       const Evaluation evaluation)
+        : SmileSection(d, dc), evaluation_(evaluation), forward_(forward),
+          params_(zabrParams) {
+
+        init();
+    }
+
+    void ZabrSmileSection::init() {
+
+        QL_REQUIRE(params_.size() == 5,
+                   "zabr expects 5 parameters (alpha,beta,nu,rho,gamma) but ("
+                       << params_.size() << ") given");
+
+        model_ = boost::shared_ptr<ZabrModel>(
+            new ZabrModel(exerciseTime(), forward_, params_[0], params_[1],
+                          params_[2], params_[3], params_[4]));
+    }
+
+    Real ZabrSmileSection::optionPrice(Rate strike, Option::Type type,
+                                       Real discount) const {
+
+        switch (evaluation_) {
+
+        case ShortMaturityLognormal:
+            return SmileSection::optionPrice(strike, type, discount);
+
+        case ShortMaturityNormal:
+            return bachelierBlackFormula(
+                type, strike, model_->forward(),
+                model_->normalVolatility(strike) * std::sqrt(exerciseTime()),
+                discount);
+
+        default:
+            QL_FAIL("Unknown evaluation (" << evaluation_ << ")");
+        }
+    }
+
+    Real ZabrSmileSection::volatilityImpl(Rate strike) const {
+
+        switch (evaluation_) {
+
+        case ShortMaturityLognormal: {
+            strike = std::max(0.00001, strike);
+            return model_->lognormalVolatility(strike);
+        }
+
+        case ShortMaturityNormal: {
+            Real impliedVol = 0.0;
+            try {
+                Option::Type type;
+                if (strike >= model_->forward()) // use otm option to imply vol
+                    type = Option::Call;
+                else
+                    type = Option::Put;
+                impliedVol =
+                    blackFormulaImpliedStdDev(
+                        type, strike, model_->forward(),
+                        optionPrice(strike, type, 1.0), 1.0) /
+                    std::sqrt(exerciseTime());
+            }
+            catch (...) {
+            }
+            return impliedVol;
+        }
+
+        default:
+            QL_FAIL("Unknown evaluation (" << evaluation_ << ")");
+        }
+    }
+}
