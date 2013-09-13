beff7460de350e14a8c5b3df9870797a7a85ab89
Peter Caspers <pcaspers1973@gmail.com>
2013-09-13 22:06:40 +0200

    extend example

--- QuantLib/Examples/InterestRateSmiles/InterestRateSmiles.cpp
+++ QuantLib/Examples/InterestRateSmiles/InterestRateSmiles.cpp
@@ -12,6 +12,8 @@ int main(int, char * []) {
     std::ofstream out;
     out.open("smiles.dat");
 
+    // this is an example from the paper, forward is a guess (since not mentioned there)
+
     Real forward = 0.0325;
     Real expiryTime = 10.0;
 
@@ -30,42 +32,52 @@ int main(int, char * []) {
 
     boost::shared_ptr<SabrSmileSection> sabr(
         new SabrSmileSection(expiryTime, forward, sabrParams));
+
     boost::shared_ptr<ZabrSmileSection> zabrln(
         new ZabrSmileSection(expiryTime, forward, zabrParams,
                              ZabrSmileSection::ShortMaturityLognormal));
+
     boost::shared_ptr<ZabrSmileSection> zabrnm(
         new ZabrSmileSection(expiryTime, forward, zabrParams,
                              ZabrSmileSection::ShortMaturityNormal));
 
+    boost::shared_ptr<ZabrSmileSection> zabrlv(new ZabrSmileSection(
+        expiryTime, forward, zabrParams, ZabrSmileSection::LocalVolatility));
+
     boost::shared_ptr<ZabrModel> zabrmodel = zabrln->model();
 
-    Real strike = 0.0010; // we start at 10bp ...
+    Real strike = -0.20;// 0.0010; // we start at 10bp ...
 
-    while (strike <= 1.00) { // ... and go to 100% ...
+    while (strike <= 0.20) { // ... and go to 100% ...
 
         // debug: localvol function for sabr hardcoded
-        Real ytmp = 1.0/(sabrParams[0]*(1.0-sabrParams[1])) * (pow(forward,1.0-sabrParams[1])-pow(strike,1.0-sabrParams[1]));
+        Real ytmp = strike >= 0.0 ?
+            1.0/(sabrParams[0]*(1.0-sabrParams[1])) * (pow(forward,1.0-sabrParams[1])-pow(strike,1.0-sabrParams[1])) :
+            1.0/(sabrParams[0]*(1.0-sabrParams[1])) * (
+                                                       pow(-strike,1.0-sabrParams[1]) + pow(forward,1.0-sabrParams[1]));
         Real Jtmp = sqrt(1.0+sabrParams[2]*sabrParams[2]*ytmp*ytmp-2.0*sabrParams[3]*sabrParams[2]*ytmp);
-        Real localVol = Jtmp * pow(strike,sabrParams[1]) * sabrParams[0];
+        Real localVol = Jtmp * pow(std::fabs(strike),sabrParams[1]) * sabrParams[0];
         // end debug
 
         out << strike << " "                           // 1  SABR hagan
-            << sabr->volatility(strike) << " "         // 2
-            << sabr->optionPrice(strike) << " "        // 3
-            << sabr->digitalOptionPrice(strike) << " " // 4
-            << sabr->density(strike) << " "            // 5
-            << zabrln->volatility(strike)
-            << " "                                       // 6  ZABR short maturity lognormal
-            << zabrln->optionPrice(strike) << " "        // 7
-            << zabrln->digitalOptionPrice(strike) << " " // 8
-            << zabrln->density(strike) << " "            // 9
-            << zabrnm->volatility(strike)
-            << " "                                       // 10  ZABR short maturity normal
-            << zabrnm->optionPrice(strike) << " "        // 11
-            << zabrnm->digitalOptionPrice(strike) << " " // 12
-            << zabrnm->density(strike) << " "            // 13
-            << zabrmodel->localVolatility(strike) << " " // 14  Equivalent deterministic volatility
-            << localVol << " "                           // 15  same as 14, but hardcoded for SABR (debug)
+            << "0.0 " //sabr->volatility(strike) << " "         // 2
+            << "0.0 " //sabr->optionPrice(strike) << " "        // 3
+            << "0.0 " //sabr->digitalOptionPrice(strike) << " " // 4
+            << "0.0 " //sabr->density(strike) << " "            // 5
+            << "0.0 " //zabrln->volatility(strike) << " "       // 6  ZABR short maturity lognormal
+            << "0.0 " //zabrln->optionPrice(strike) << " "        // 7
+            << "0.0 " //zabrln->digitalOptionPrice(strike) << " " // 8
+            << "0.0 " //zabrln->density(strike) << " "            // 9
+            << "0.0 " //zabrnm->volatility(strike) << " "         // 10  ZABR short maturity normal
+            << "0.0 " //zabrnm->optionPrice(strike) << " "        // 11
+            << "0.0 " //zabrnm->digitalOptionPrice(strike) << " " // 12
+            << "0.0 " //zabrnm->density(strike) << " "            // 13
+            << zabrmodel->localVolatility(strike) << " " // 14  *** Equivalent deterministic volatility
+            << localVol << " "                           // 15  *** same as 14, but hardcoded for SABR (debug)
+            << "0.0 " //zabrlv->volatility(strike) << " "         // 16  ZABR local vol 
+            << zabrlv->optionPrice(strike) << " "        // 17
+            << zabrlv->digitalOptionPrice(strike) << " " // 18
+            << zabrlv->density(strike) << " "            // 19
             << std::endl;
 
         strike += 0.0010; // ... in steps of 10bp
