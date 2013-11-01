b938ae18c8de62e7495a9b3d7304cba7ac03fba2
Peter Caspers <pcaspers1973@gmail.com>
2013-11-01 20:58:38 +0100

    extend example

--- QuantLib/Examples/CmsSwaption/CmsSwaption.cpp
+++ QuantLib/Examples/CmsSwaption/CmsSwaption.cpp
@@ -1,128 +1,167 @@
 #include <ql/quantlib.hpp>
 using namespace QuantLib;
 
-int main(int, char* []) {
-
+int main(int, char * []) {
 
     try {
 
-      Date refDate(13,November,2013);
-      Date settlDate = TARGET().advance(refDate,2*Days);
-      Settings::instance().evaluationDate()=refDate;
+        Date refDate(13, November, 2013);
+        Date settlDate = TARGET().advance(refDate, 2 * Days);
+        Settings::instance().evaluationDate() = refDate;
 
-      Handle<Quote> rateLevel(new SimpleQuote(0.03));
-      Handle<YieldTermStructure> yts(new FlatForward(refDate,rateLevel,
-						     Actual365Fixed()));
+        Handle<Quote> rateLevel(new SimpleQuote(0.03));
+        Handle<YieldTermStructure> yts(
+            new FlatForward(refDate, rateLevel, Actual365Fixed()));
 
-      Handle<Quote> volatilityLevel(new SimpleQuote(0.30));
+        boost::shared_ptr<IborIndex> iborIndex(new Euribor(6 * Months, yts));
+        boost::shared_ptr<SwapIndex> swapIndex(
+            new EuriborSwapIsdaFixA(10 * Years, yts));
 
-      Handle<Quote>  reversionLevel(new SimpleQuote(0.02));
+        iborIndex->addFixing(refDate, 0.0200);
+        swapIndex->addFixing(refDate, 0.0315);
 
-      boost::shared_ptr<IborIndex> iborIndex(new Euribor(6*Months,yts));
-      boost::shared_ptr<SwapIndex> swapIndex(new EuriborSwapIsdaFixA(10*Years,yts));   
-      
-      iborIndex->addFixing(refDate,0.0200);
-      swapIndex->addFixing(refDate,0.0315);
+        Handle<Quote> volatilityLevel(new SimpleQuote(0.30));
+        // Handle<SwaptionVolatilityStructure> swaptionVol(
+        //     new ConstantSwaptionVolatility(refDate, TARGET(), Following,
+        //                                    volatilityLevel, Actual365Fixed()));
 
-      // Handle<SwaptionVolatilityStructure> swaptionVol(new ConstantSwaptionVolatility(refDate,TARGET(),
-      //  						  Following,   
-      //  						  Handle<Quote>(new SimpleQuote(0.13)),
-      //  						  Actual365Fixed()));
+        Handle<SwaptionVolatilityStructure> swaptionVol(
+            new SingleSabrSwaptionVolatility(refDate, TARGET(), Following, 0.15,
+                                             0.80, -0.30, 0.20,
+                                             Actual365Fixed(), swapIndex));
 
-      Handle<SwaptionVolatilityStructure> swaptionVol(new SingleSabrSwaptionVolatility(refDate,TARGET(),
-      									       Following,
-                                               0.10,0.80,-0.20,0.080,
-                                               Actual365Fixed(),
-      									       swapIndex));
+        // Real strike = 0.0001;
+        // while(strike < 1.0) {
+        //     std::cout << strike << " " << swaptionVol->volatility(10.0,10.0,strike) << std::endl;
+        //     strike += 0.0010;
+        // }
+        // return 0;
 
-      Date termDate = TARGET().advance(settlDate,10*Years);
-      
-      Schedule sched1(settlDate,termDate,1*Years,TARGET(),ModifiedFollowing,ModifiedFollowing,
-                      DateGeneration::Forward,false);
-      Schedule sched2(settlDate,termDate,6*Months,TARGET(),ModifiedFollowing,ModifiedFollowing,
-			  DateGeneration::Forward,false);
+        Date termDate = TARGET().advance(settlDate, 10 * Years);
 
-      Real nominal = 100000.0;
+        Schedule sched1(settlDate, termDate, 1 * Years, TARGET(),
+                        ModifiedFollowing, ModifiedFollowing,
+                        DateGeneration::Forward, false);
+        Schedule sched2(settlDate, termDate, 6 * Months, TARGET(),
+                        ModifiedFollowing, ModifiedFollowing,
+                        DateGeneration::Forward, false);
 
-      boost::shared_ptr<FloatFloatSwap> cmsswap(new FloatFloatSwap(VanillaSwap::Payer,
-                                                                   nominal,nominal,
-                                                                   sched1,swapIndex,Thirty360(),
-                                                                   sched2,iborIndex,Actual360()));
+        Real nominal = 100000.0;
 
-      boost::shared_ptr<NumericHaganPricer> haganPricer(new NumericHaganPricer(swaptionVol,
-                                                                               GFunctionFactory::Standard,
-                                                                               reversionLevel));
-      setCouponPricer(cmsswap->leg(0), haganPricer);
-      
-      std::vector<Date> exerciseDates;
-      std::vector<Date> sigmaSteps;
-      std::vector<Real> sigma;
-      
-      sigma.push_back(0.01);
-      for(Size i=1;i<sched1.size()-1;i++) {
-          exerciseDates.push_back(swapIndex->fixingDate(sched1[i]));
-          sigmaSteps.push_back(exerciseDates.back());
-          sigma.push_back(0.01);
-      }
+        boost::shared_ptr<FloatFloatSwap> cmsswap(new FloatFloatSwap(
+            VanillaSwap::Payer, nominal, nominal, sched1, swapIndex,
+            Thirty360(), sched2, iborIndex, Actual360(),
+            false,false,1.0,0.0,Null<Real>(),Null<Real>(),1.0,0.00267294));
 
-      boost::shared_ptr<FloatFloatSwap> underlying(new FloatFloatSwap(VanillaSwap::Receiver,
-                                                                   nominal,nominal,
-                                                                   sched1,swapIndex,Thirty360(),
-                                                                   sched2,iborIndex,Actual360()));
+        std::vector<Date> exerciseDates;
+        std::vector<Date> sigmaSteps;
+        std::vector<Real> sigma;
 
-      boost::shared_ptr<Exercise> exercise(new BermudanExercise(exerciseDates));
-      boost::shared_ptr<FloatFloatSwaption> callRight(new FloatFloatSwaption(underlying,exercise));
+        sigma.push_back(0.01);
+        for (Size i = 1; i < sched1.size() - 1; i++) {
+            exerciseDates.push_back(swapIndex->fixingDate(sched1[i]));
+            sigmaSteps.push_back(exerciseDates.back());
+            sigma.push_back(0.01);
+        }
 
-      std::vector<Date> cmsFixingDates(exerciseDates);
-      std::vector<Period> cmsTenors(exerciseDates.size(),10*Years);
+        // double v[] = {0.01,0.0115446,0.0111677,0.0113092,0.0112404,0.0111856,0.0111218,0.0110547,0.0112791,0.00945792};
+        // sigma = std::vector<Real>(v,v+10);
 
-      boost::shared_ptr<MarkovFunctional> mf(new MarkovFunctional(
-          yts, reversionLevel->value(), sigmaSteps, sigma, swaptionVol,
-          cmsFixingDates, cmsTenors, swapIndex,
-          MarkovFunctional::ModelSettings().withYGridPoints(16)
-                                           .withYStdDevs(5.0)
-                                           .withGaussHermitePoints(16)
-          .withUpperRateBound(2.0)
-          .withAdjustments(MarkovFunctional::ModelSettings::SabrSmile)));
+        // boost::shared_ptr<FloatFloatSwap> underlying(new FloatFloatSwap(
+        //     VanillaSwap::Receiver, nominal, nominal, sched1, swapIndex,
+        //     Thirty360(), sched2, iborIndex, Actual360(),
+        //     false,false,1.0,0.0,Null<Real>(),Null<Real>(),1.0,0.00267294));
 
-      boost::shared_ptr<Gaussian1dFloatFloatSwaptionEngine> floatEngine(new Gaussian1dFloatFloatSwaptionEngine(mf));
+        boost::shared_ptr<Exercise> exercise(
+            new BermudanExercise(exerciseDates));
+        boost::shared_ptr<FloatFloatSwaption> callRight(
+            new FloatFloatSwaption(cmsswap, exercise));
 
-      callRight->setPricingEngine(floatEngine);
+        std::vector<Date> cmsFixingDates(exerciseDates);
+        std::vector<Period> cmsTenors(exerciseDates.size(), 10 * Years);
 
-      boost::shared_ptr<SwapIndex> swapBase(new EuriborSwapIsdaFixA(30*Years,yts));
-      std::vector<boost::shared_ptr<CalibrationHelper> > basket = 
-          callRight->calibrationBasket(swapBase,*swaptionVol,BasketGeneratingEngine::Naive);
+        Handle<Quote> reversionLevel(new SimpleQuote(0.02));
 
-      boost::shared_ptr<Gaussian1dSwaptionEngine> stdEngine(new Gaussian1dSwaptionEngine(mf));
-      for(Size i=0;i<basket.size();i++) basket[i]->setPricingEngine(stdEngine);
-      
-      LevenbergMarquardt opt;
-      EndCriteria ec(2000,500,1E-8,1E-8,1E-8);
-      mf->calibrate(basket,opt,ec);
+        boost::shared_ptr<NumericHaganPricer> haganPricer(
+            new NumericHaganPricer(swaptionVol,
+                                   GFunctionFactory::NonParallelShifts,
+                                   reversionLevel));
+        setCouponPricer(cmsswap->leg(0), haganPricer);
 
-      std::cout << "model vol \t swaption market \t swaption model " << std::endl;
-      for(Size i=0;i<basket.size();i++) {
-          std::cout << mf->volatilities()[i] << "\t" << basket[i]->marketValue() << "\t" << basket[i]->modelValue() << std::endl;
-      }
-      std::cout << mf->volatilities().back() << std::endl;
+        // markov model (change the model->calibrate call below)
+        //boost::math::ntl::RR::SetPrecision(113);
+        boost::shared_ptr<MarkovFunctional> model(new MarkovFunctional(
+            yts, reversionLevel->value(), sigmaSteps, sigma, swaptionVol,
+            cmsFixingDates, cmsTenors, swapIndex));
 
-      Real analyticSwapNpv = CashFlows::npv(cmsswap->leg(1), **yts, false) - CashFlows::npv(cmsswap->leg(0), **yts, false);
-      Real callRightNpv = callRight->NPV();
-      Real firstCouponNpv = cmsswap->leg(0)[0]->amount() * yts->discount(cmsswap->leg(0)[0]->date()) - cmsswap->leg(1)[0]->amount() * yts->discount(cmsswap->leg(1)[0]->date());
-      Real underlyingNpv = callRight->result<Real>("underlyingValue") + firstCouponNpv;
+        // hull white model (change the model->calibrate call below)
+        // std::vector<Date> sigmaSteps2(sigmaSteps.begin(), sigmaSteps.end() - 1);
+        // std::vector<Real> sigma2(sigma.begin(), sigma.end() - 1);
+        // boost::shared_ptr<Gsr> model(new Gsr(yts,sigmaSteps2,sigma2,reversionLevel->value()));
+        
+        boost::shared_ptr<Gaussian1dFloatFloatSwaptionEngine> floatEngine(
+            new Gaussian1dFloatFloatSwaptionEngine(model));
 
-      std::cout << "Swap Npv (Hagan)   : " << analyticSwapNpv << std::endl;
-      std::cout << "Call Right Npv (MF): " << callRightNpv << std::endl;
-      std::cout << "Underlying Npv (MF): " << underlyingNpv << std::endl;
-      
-      std::cout << "Model trace : " << std::endl << mf->modelOutputs() << std::endl;
-    } catch (std::exception& e) {
+        callRight->setPricingEngine(floatEngine);
+
+        boost::shared_ptr<SwapIndex> swapBase(
+            new EuriborSwapIsdaFixA(30 * Years, yts));
+        std::vector<boost::shared_ptr<CalibrationHelper> > basket =
+            callRight->calibrationBasket(swapBase, *swaptionVol,
+                                         BasketGeneratingEngine::Naive
+                                         //BasketGeneratingEngine::MaturityStrikeByDeltaGamma
+                                         );
+
+        std::cout << "option date & maturity date & nominal & strike \\\\" << std::endl;
+        for(Size i=0;i<basket.size();i++) {
+            boost::shared_ptr<SwaptionHelper> h = boost::dynamic_pointer_cast<SwaptionHelper>(basket[i]);
+            std::cout << exerciseDates[i] << " & " << h->underlyingSwap()->fixedSchedule().dates().back() << " & " <<
+                      h->underlyingSwap()->nominal() << " & " << h->underlyingSwap()->fixedRate() << std::endl;
+        }
+
+        boost::shared_ptr<Gaussian1dSwaptionEngine> stdEngine(
+            new Gaussian1dSwaptionEngine(model));
+        for (Size i = 0; i < basket.size(); i++)
+            basket[i]->setPricingEngine(stdEngine);
+
+        LevenbergMarquardt opt;
+        EndCriteria ec(2000, 500, 1E-8, 1E-8, 1E-8);
+        model->calibrate(basket, opt, ec); // for markov
+        // model->calibrate(basket, opt, ec, Constraint(), std::vector<Real>(), model->FixedReversions()); // for gsr
+
+        std::cout << "model vol & swaption market & swaption model "
+                  << std::endl;
+        for (Size i = 0; i < basket.size(); i++) {
+            std::cout << model->volatility()[i] << " & "
+                      << basket[i]->marketValue() << " & "
+                      << basket[i]->modelValue() << " \\\\" << std::endl;
+        }
+        std::cout << model->volatility().back() << std::endl;
+
+        Real analyticSwapNpv = CashFlows::npv(cmsswap->leg(1), **yts, false) -
+                               CashFlows::npv(cmsswap->leg(0), **yts, false);
+        Real callRightNpv = callRight->NPV();
+        Real firstCouponNpv = -cmsswap->leg(0)[0]->amount() *
+                                  yts->discount(cmsswap->leg(0)[0]->date()) +
+                              cmsswap->leg(1)[0]->amount() *
+                                  yts->discount(cmsswap->leg(1)[0]->date());
+        Real underlyingNpv =
+            callRight->result<Real>("underlyingValue") + firstCouponNpv;
+
+        std::cout << "Swap Npv (Hagan)     & " << analyticSwapNpv << "\\\\" << std::endl;
+        std::cout << "Call Right Npv (MF)  & " << callRightNpv << "\\\\" << std::endl;
+        std::cout << "Underlying Npv (MF)  & " << underlyingNpv << "\\\\" << std::endl;
+
+        std::cout << "fair margin swap & " << -analyticSwapNpv/CashFlows::bps(cmsswap->leg(1),**yts,false) << std::endl;
+
+        std::cout << "Model trace : " << std::endl << model->modelOutputs() << std::endl;
+    }
+    catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
         return 1;
-    } catch (...) {
+    }
+    catch (...) {
         std::cerr << "unknown error" << std::endl;
         return 1;
     }
-
 }
-
