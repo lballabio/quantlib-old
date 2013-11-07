ead136dce6447165be894c33d6911db90b2b0cc2
Ferdinando M. Ametrano <ferdinando@ametrano.net>
2013-11-07 17:08:47 +0100

    fixed zip + fixed workbook

--- QuantLibXL/make_zip.py
+++ QuantLibXL/make_zip.py
@@ -34,13 +34,14 @@ zfile = zipfile.ZipFile(zipFilePath, "w")
 
 # Zip up some specific files from the QuantLibXL directory.
 zfile.write("Docs/QuantLibXL-docs-1.3.0.chm", ROOT_DIR + "Docs/QuantLibXL-docs-1.3.0.chm", zipfile.ZIP_DEFLATED)
-zfile.write("xll/QuantLibXL-vc90-mt-s-1_3_0.xll", ROOT_DIR + "xll/QuantLibXL-vc90-mt-s-1_3_0.xll", zipfile.ZIP_DEFLATED)
+zfile.write("xll/QuantLibXLDynamic-vc90-mt-1_3_0.xll", ROOT_DIR + "xll/QuantLibXLDynamic-vc90-mt-1_3_0.xll", zipfile.ZIP_DEFLATED)
 zfile.write("zip/README.txt", ROOT_DIR + "README.txt", zipfile.ZIP_DEFLATED)
 # Recursively zip some subdirectories of the QuantLibXL directory.
 os.path.walk("Data", visit, (zfile, ".gitignore", None))
 os.path.walk("framework", visit, (zfile, "ReadMe.txt", None))
 os.path.walk("Workbooks", visit, (zfile, None, None))
 # Zip up some files from other projects in the repo.
+zfile.write("../ObjectHandler/xll/ObjectHandler-xll-vc90-mt-1_3_0.xll", ROOT_DIR + "xll/ObjectHandler-xll-vc90-mt-1_3_0.xll", zipfile.ZIP_DEFLATED)
 os.path.walk("../QuantLibAddin/gensrc/metadata", visit, (zfile, None, "../QuantLibAddin/gensrc/"))
 zfile.write("../XL-Launcher/bin/Addin/Launcher.xla", ROOT_DIR + "Launcher.xla", zipfile.ZIP_DEFLATED)
 zfile.write("../XL-Launcher/bin/Addin/session_file.xml.zipfile", ROOT_DIR + "session_file.xml", zipfile.ZIP_DEFLATED)
