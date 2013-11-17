d7fd29c66e2d0b78aa1abed8210cfa0d3f71b9b4
Peter Caspers <pcaspers1973@gmail.com>
2013-11-17 13:29:40 +0100

    modify example

--- QuantLib/Examples/SimulatedAnnealing/SimulatedAnnealing.cpp
+++ QuantLib/Examples/SimulatedAnnealing/SimulatedAnnealing.cpp
@@ -9,15 +9,17 @@ int main(int, char* []) {
 
     MersenneTwisterUniformRng mt(42);
 
-    //SimulatedAnnealing<MersenneTwisterUniformRng> sa(1.0,5.0,0.01,10,mt);
-    SimulatedAnnealing<MersenneTwisterUniformRng> sa(1.0,0.0,10000,2.0,mt);
+    SimulatedAnnealing<> sa(1.0,10.0,0.10,10);
+    //SimulatedAnnealing<> sa(1.0,1000.0,50000,2.0);
     LevenbergMarquardt lm;
 
     class cfct : public CostFunction {
     public:
         Real value(const Array &x) const {
             //return -cos(x[0])*cos(x[1])*exp(-std::pow(x[0]-M_PI,2.0)-std::pow(x[1]-M_PI,2.0));
-            return (2.0+std::sin(x[0])*std::sin(x[1]))*(x[0]*x[0]+x[1]*x[1]);
+            Real y = (2.0+std::sin(x[0])*std::sin(x[1]))*(x[0]*x[0]+x[1]*x[1]);
+            //std::cout << "f(" << x[0] << "," << x[1] << ")=" << y << std::endl;
+            return y;
         }
         Disposable<Array> values(const Array &x) const {
             Array res(2);
@@ -27,7 +29,7 @@ int main(int, char* []) {
         }
     } aCfct;
 
-    EndCriteria ec(100000,50,1E-10,1E-10,1E-10);
+    EndCriteria ec(1000000,50,1E-10,1E-10,1E-10);
     NoConstraint constraint;
     Array initial(2);
     initial[0] = 10.0;//30.0;
