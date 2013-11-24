2d2c4714358b27c3234be32d751aa13f5e4db523
Eric Ehlers <eric.ehlers@nazcatech.be>
2013-11-24 13:36:54 +0100

    autogenerate excel addin

--- QuantLibAddin/swig/Main/mainQuantLibAddinHw.cpp
+++ QuantLibAddin/swig/Main/mainQuantLibAddinHw.cpp
@@ -6,8 +6,8 @@ int main() {
     try {
 	    std::cout << "hi" << std::endl;
     	std::cout << QuantLibAddin::qlClose(1, 2) << std::endl;
-        QuantLibAddin::qlSimpleQuote("foo", 1);
-        std::cout << QuantLibAddin::qlSimpleQuoteValue("foo") << std::endl;
+        QuantLibAddin::qlSimpleQuote("quote", 1);
+        std::cout << QuantLibAddin::qlSimpleQuoteValue("quote") << std::endl;
     	std::cout << "bye" << std::endl;
     } catch (const std::exception &e) {
     	std::cout << "error " << e.what() << std::endl;
