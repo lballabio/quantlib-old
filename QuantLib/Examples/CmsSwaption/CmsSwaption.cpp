4d8658f703cb6ec9acb76c81f6ae90388b589e2b
Peter Caspers <pcaspers1973@gmail.com>
2013-12-08 19:35:07 +0100

    extend example

--- QuantLib/Examples/CmsSwaption/CmsSwaption.cpp
+++ QuantLib/Examples/CmsSwaption/CmsSwaption.cpp
@@ -1,7 +1,10 @@
 #include <ql/quantlib.hpp>
+
+#include <boost/make_shared.hpp>
+
 using namespace QuantLib;
 
-int main(int, char * []) {
+int example01() {
 
     try {
 
@@ -90,7 +93,7 @@ int main(int, char * []) {
         std::vector<Date> cmsFixingDates(exerciseDates);
         std::vector<Period> cmsTenors(exerciseDates.size(), 10 * Years);
 
-        Handle<Quote> reversionLevel(new SimpleQuote(0.02));
+        Handle<Quote> reversionLevel(new SimpleQuote(0.02)); 
 
         boost::shared_ptr<NumericHaganPricer> haganPricer(
             new NumericHaganPricer(swaptionVol,
@@ -175,6 +178,9 @@ int main(int, char * []) {
         std::cout << "fair margin swap & " << -analyticSwapNpv/CashFlows::bps(cmsswap->leg(1),**yts,false) << std::endl;
 
         //std::cout << "Model trace : " << std::endl << model->modelOutputs() << std::endl;
+
+        return 0;
+
     }
     catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
@@ -184,4 +190,155 @@ int main(int, char * []) {
         std::cerr << "unknown error" << std::endl;
         return 1;
     }
+
+}
+
+
+int example02() {
+
+    try {
+
+        Date refDate(13, November, 2013);
+        Date settlDate = TARGET().advance(refDate, 2 * Days);
+        Settings::instance().evaluationDate() = refDate;
+
+        Handle<Quote> rateLevel(new SimpleQuote(0.03));
+        Handle<YieldTermStructure> yts(
+            new FlatForward(refDate, rateLevel, Actual365Fixed()));
+
+        boost::shared_ptr<IborIndex> iborIndex(new Euribor(6 * Months, yts));
+        boost::shared_ptr<SwapIndex> swapIndex(
+            new EuriborSwapIsdaFixA(10 * Years, yts));
+
+        iborIndex->addFixing(refDate, 0.0200);
+        swapIndex->addFixing(refDate, 0.0315);
+
+        // Handle<Quote> volatilityLevel(new SimpleQuote(0.20)); // vol here !
+        // Handle<SwaptionVolatilityStructure> swaptionVol(
+        //     new ConstantSwaptionVolatility(refDate, TARGET(), Following,
+        //                                    volatilityLevel, Actual365Fixed()));
+
+        Handle<SwaptionVolatilityStructure> swaptionVol(
+            new SingleSabrSwaptionVolatility(refDate, TARGET(), Following, 0.15,
+                                             0.80, -0.30, 0.20,
+                                             Actual365Fixed(), swapIndex));
+
+        // Real strike = 0.0001;
+        // while(strike < 1.0) {
+        //     std::cout << strike << " " << swaptionVol->volatility(10.0,10.0,strike) << std::endl;
+        //     strike += 0.0010;
+        // }
+        // return 0;
+
+        Date termDate = TARGET().advance(settlDate, 10 * Years);
+
+        Schedule sched1(settlDate, termDate, 1 * Years, TARGET(),
+                        ModifiedFollowing, ModifiedFollowing,
+                        DateGeneration::Forward, false);
+        Schedule sched2(settlDate, termDate, 6 * Months, TARGET(),
+                        ModifiedFollowing, ModifiedFollowing,
+                        DateGeneration::Forward, false);
+
+        Real nominal = 100000.0;
+
+        boost::shared_ptr<FloatFloatSwap> cmsswap(new FloatFloatSwap( // CMS Swap
+            VanillaSwap::Payer, nominal, nominal, sched1, swapIndex,
+            Thirty360(), sched2, iborIndex, Actual360(),
+            false,false,1.0,0.00,Null<Real>(),Null<Real>(),1.0,0.0));
+        
+        Handle<Quote> reversionLevel(new SimpleQuote(0.03)); // reversion here !
+
+        boost::shared_ptr<NumericHaganPricer> haganPricerN(
+            new NumericHaganPricer(swaptionVol,
+                                   GFunctionFactory::NonParallelShifts,
+                                   reversionLevel));
+        boost::shared_ptr<AnalyticHaganPricer> haganPricerA(
+            new AnalyticHaganPricer(swaptionVol,
+                                   GFunctionFactory::NonParallelShifts,
+                                   reversionLevel));
+
+        //auto integrator = boost::make_shared<GaussLobattoIntegral>(GaussLobattoIntegral(1000,1E-4));
+
+        boost::shared_ptr<LinearTsrPricer> tsrPricer(new LinearTsrPricer(swaptionVol,
+                                                                   reversionLevel,
+                                                                   Handle<YieldTermStructure>(),
+                                                                   LinearTsrPricer::Settings()
+                                                                   .withRateBound(0.0,10.0)
+                                                                   //.withVegaRatio(0.01)
+                                                                   ));
+
+        boost::shared_ptr<CmsReplicationPricer> replPricer(new CmsReplicationPricer(swaptionVol,
+                                                                                    reversionLevel));
+
+        Real strike = 0.0001;
+        while (strike <= 0.10) {
+
+            auto cap = boost::make_shared<CappedFlooredCmsCoupon>(
+                CappedFlooredCmsCoupon(Date(13, November, 2023), 100000.0,
+                                       Date(13, November, 2022),
+                                       Date(13, November, 2023), 2, swapIndex,
+                                       1.0, 0.0, strike, Null<Rate>(), Date(),
+                                       Date(), DayCounter(), false, true));
+            auto floor = boost::make_shared<CappedFlooredCmsCoupon>(
+                CappedFlooredCmsCoupon(Date(13, November, 2023), 100000.0,
+                                       Date(13, November, 2022),
+                                       Date(13, November, 2023), 2, swapIndex,
+                                       1.0, 0.0, Null<Real>(), strike, Date(),
+                                       Date(), DayCounter(), false, true));
+            auto swaplet = boost::make_shared<CappedFlooredCmsCoupon>(
+                CappedFlooredCmsCoupon(
+                    Date(13, November, 2023), 100000.0,
+                    Date(13, November, 2022), Date(13, November, 2023), 2,
+                    swapIndex, 1.0, 0.0, Null<Rate>(), Null<Rate>(), Date(),
+                    Date(), DayCounter(), false, false));
+
+            cap->setPricer(haganPricerA);
+            floor->setPricer(haganPricerA);
+            swaplet->setPricer(haganPricerA);
+            Real cap1 = cap->adjustedFixing();
+            Real floor1 = floor->adjustedFixing();
+            Real swaplet1 = swaplet->adjustedFixing();
+
+            cap->setPricer(haganPricerN);
+            floor->setPricer(haganPricerN);
+            swaplet->setPricer(haganPricerN);
+            Real cap2 = cap->adjustedFixing();
+            Real floor2 = floor->adjustedFixing();
+            Real swaplet2 = swaplet->adjustedFixing();
+
+            cap->setPricer(tsrPricer);
+            floor->setPricer(tsrPricer);
+            swaplet->setPricer(tsrPricer);
+            Real cap3 = cap->adjustedFixing();
+            Real floor3 = floor->adjustedFixing();
+            Real swaplet3 = swaplet->adjustedFixing();
+
+            std::cout << strike << " " << cap1 << "/" << cap2 << "/" << cap3 << " " << floor1
+                      << "/" << floor2 << "/" << floor3 << " " << swaplet1 << "/" << swaplet2 << "/" << swaplet3
+                      << " " << (cap1 - floor1 - (swaplet1 - strike)) << "/"
+                      << (cap2 - floor2 - (swaplet2 - strike)) << "/" << (cap3-floor3-(swaplet3-strike)) << std::endl;
+
+            strike += 0.0010;
+        }
+
+        return 0;
+
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "unknown error" << std::endl;
+        return 1;
+    }
+
+
+
+}
+
+
+int main(int, char * []) {
+    
+    //return example01();
+    return example02();
 }
