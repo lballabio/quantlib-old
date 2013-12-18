d9eef969f03766c10988a282bb2c2eda2a8abc92
Eric Ehlers <eric.ehlers@nazcatech.be>
2013-12-17 16:05:31 -0800

    support for multiple currencies

---
+++ XL-Launcher/bin/Addin/qlxl.GBP.bat
@@ -0,0 +1,4 @@
+
+set XL_LAUNCHER_PATH=C:\Users\erik\Documents\repos\quantlib_nando\XL-Launcher\bin\Addin\session_file.GBP.xml
+START "EXCEL" "C:\Program Files (x86)\Microsoft Office\Office14\EXCEL.EXE" /e /r "C:\Users\erik\Documents\repos\quantlib_nando\XL-Launcher\bin\Addin\Launcher.xla"
+
---
+++ XL-Launcher/bin/Addin/qlxl.bat
@@ -0,0 +1,3 @@
+
+START "C:\Program Files (x86)\Microsoft Office\Office14\EXCEL.EXE" "C:\Users\erik\Documents\repos\quantlib_nando\XL-Launcher\bin\Addin\Launcher.xla"
+
