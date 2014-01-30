b8f7e8cef4de9fa9dcf204977487f96b0fcbe0ea
Eric Ehlers <eric.ehlers@nazcatech.be>
2014-01-29 17:33:00 -0800

    remove hard coded bat files

--- XL-Launcher/bin/Addin/qlxl.HKD.bat
+++
@@ -1,4 +0,0 @@
-
-SET XL_LAUNCHER_PATH=C:\Users\erik\Documents\repos\quantlib_nando\XL-Launcher\bin\Addin\session_file.HKD.xml
-START "EXCEL" "C:\Program Files (x86)\Microsoft Office\Office14\EXCEL.EXE" /e /r "C:\Users\erik\Documents\repos\quantlib_nando\XL-Launcher\bin\Addin\Launcher.xla"
-
