6520c4dd0bfa20a7e7a5fb870431486dc9485202
Luigi Ballabio <luigi.ballabio@gmail.com>
2014-02-01 23:51:53 +0100

    Drop support for Visual C++ 2003 (VC++7).
    The compiler is now more than 10 years old and no longer supported
    by Microsoft.  Keeping the support is not worth the time and effort
    required.  Anybody who is still stuck with this compiler and needs
    the support can fork the repository and maintain the changes.

--- dev_tools/sync_csharp_projects.sh
+++ dev_tools/sync_csharp_projects.sh
@@ -2,7 +2,7 @@
 
 # execute this script from the root of an uncompressed QuantLib-SWIG tarball
 
-# 1) get reference lists of distributed files (done with find; this is
+# get reference lists of distributed files (done with find; this is
 # why this script should be run from an uncompressed tarball created
 # with 'make dist', not from a working copy.)
 
@@ -10,19 +10,13 @@ find CSharp/csharp -name '*.cs' \
 | awk -F'/' '{ print $3 }' \
 | sort > csharp.ref.files
 
-# 2) extract file names from VC7 project.
-
-grep -o -E 'RelPath *= *".*"' CSharp/csharp/NQuantLib_vc7.csproj \
-| awk -F'"' '{ print $2 }' \
-| sort > csharp.vc7.files
-
-# 3) Same for VC8...
+# extract file names from VC8 project.
 
 grep -o -E 'Compile *Include *= *".*"' CSharp/csharp/NQuantLib_vc8.csproj \
 | awk -F'"' '{ print $2 }' \
 | sort > csharp.vc8.files
 
-# 4) ...VC9...
+# 4) Same for VC9...
 
 grep -o -E 'Compile *Include *= *".*"' CSharp/csharp/NQuantLib_vc9.csproj \
 | awk -F'"' '{ print $2 }' \
@@ -36,12 +30,7 @@ grep -o -E 'Compile *Include *= *".*"' CSharp/csharp/NQuantLib_vc10.csproj \
 
 # 6) write out differences...
 
-echo 'Visual Studio 7:' > sync.report
-diff -b csharp.vc7.files csharp.ref.files >> sync.report
-
-echo '' >> sync.report
-echo '' >> sync.report
-echo 'Visual Studio 8:' >> sync.report
+echo 'Visual Studio 8:' > sync.report
 diff -b csharp.vc8.files csharp.ref.files >> sync.report
 
 echo '' >> sync.report
