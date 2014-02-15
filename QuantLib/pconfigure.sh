264ebb824ddad55f902bede5d3e148a5a5abc4dc
Peter Caspers <pcaspers1973@gmail.com>
2014-02-15 13:04:54 +0100

    add configuration shortcut

---
+++ QuantLib/pconfigure.sh
@@ -0,0 +1,1 @@
+./configure --with-boost-include=/usr/local/boost_1_55_0 --with-boost-lib=/usr/local/boost_1_55_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O3 -g0 -Wall -std=c++11" LIBS="-lntl -lgmp -lm"
