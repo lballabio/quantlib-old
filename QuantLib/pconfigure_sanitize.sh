79bf9fee2d738dfcabc2dceaf204f0795d2d41ca
Peter Caspers <pcaspers1973@gmail.com>
2014-02-21 17:22:51 +0100

    add boost libraries

--- QuantLib/pconfigure_sanitize.sh
+++ QuantLib/pconfigure_sanitize.sh
@@ -1,1 +1,1 @@
-./configure --with-boost-include=/usr/local/boost_1_55_0 --with-boost-lib=/usr/local/boost_1_55_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O3 -g0 -Wall -std=c++11 -fsanitize=address,undefined,integer" LIBS="-lntl -lgmp -lm"
+./configure --with-boost-include=/usr/local/boost_1_55_0 --with-boost-lib=/usr/local/boost_1_55_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O2 -g -Wall -std=c++11 -fsanitize=address,undefined,integer" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
