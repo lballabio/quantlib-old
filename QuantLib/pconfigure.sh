1b817f698a2a666dcdf92bc6a76cdbb09b33b821
Peter Caspers <pcaspers1973@gmail.com>
2014-04-19 16:12:09 +0200

    enable debugging information

--- QuantLib/pconfigure.sh
+++ QuantLib/pconfigure.sh
@@ -1,1 +1,1 @@
-./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O3 -g0 -Wall -std=c++11" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
+./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O3 -g -Wall -std=c++11" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
