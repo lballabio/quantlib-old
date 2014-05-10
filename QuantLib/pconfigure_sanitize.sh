f4518139063080f96148d0bd0ea76f3b20602b9a
Peter Caspers <pcaspers1973@gmail.com>
2014-05-10 21:29:28 +0200

    add leak detection

--- QuantLib/pconfigure_sanitize.sh
+++ QuantLib/pconfigure_sanitize.sh
@@ -1,1 +1,2 @@
-./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O2 -g -Wall -std=c++11 -fsanitize=address,undefined,integer" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
+# use ASAN_OPTIONS="detect_leaks=1" to activate memory leak detection
+./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib CC="clang" CXX="clang++" CXXFLAGS="-O2 -g -Wall -std=c++11 -fsanitize=address,undefined,integer"
