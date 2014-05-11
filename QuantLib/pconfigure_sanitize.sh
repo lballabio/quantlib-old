d13437567c12164b5d7c8c15a8761e0ff53132a1
Peter Caspers <pcaspers1973@gmail.com>
2014-05-11 19:49:23 +0200

    remove CC

--- QuantLib/pconfigure_sanitize.sh
+++ QuantLib/pconfigure_sanitize.sh
@@ -1,2 +1,2 @@
 # use ASAN_OPTIONS="detect_leaks=1" to activate memory leak detection
-./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib CC="clang" CXX="clang++" CXXFLAGS="-O2 -g -Wall -std=c++11 -fsanitize=address,undefined,integer"
+./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib CXX="clang++" CXXFLAGS="-O2 -g -Wall -std=c++11 -fsanitize=address,undefined,integer,unsigned-integer-overflow,vptr" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
