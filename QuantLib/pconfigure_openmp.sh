6b3da13e259e50c8e0d19feec84b5fe4518583b1
Peter Caspers <pcaspers1973@gmail.com>
2014-07-06 13:56:57 +0200

    update configure shortcuts

--- QuantLib/pconfigure_openmp.sh
+++ QuantLib/pconfigure_openmp.sh
@@ -1,2 +1,2 @@
-# configure with open mp support (using gcc)
+# configure gcc, with open mp support
 ./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib --enable-openmp CXXFLAGS="-m64 -O3 -g -Wall -std=c++11" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
