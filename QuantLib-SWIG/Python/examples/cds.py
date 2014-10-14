a73036fddf1e7e2869d9d1fcb5801683e0ab30bf
Luigi Ballabio <luigi.ballabio@gmail.com>
2014-10-14 17:51:35 +0200

    Clean up tabs, variable names etc.

--- QuantLib-SWIG/Python/examples/cds.py
+++ QuantLib-SWIG/Python/examples/cds.py
@@ -1,3 +1,4 @@
+# -*- mode: python; tab-width: 4;
 
 # Copyright (C) 2014 Thema Consulting
 #
@@ -14,70 +15,72 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the license for more details.
 
-import QuantLib as QL
+from QuantLib import *
 
-calendar = QL.TARGET()
+calendar = TARGET()
 
 # set evaluation date
-todaysDate = QL.Date(15,QL.May,2007);
+todaysDate = Date(15,May,2007);
 todaysDate = calendar.adjust(todaysDate)
-QL.Settings.instance().evaluationDate = todaysDate
+Settings.instance().evaluationDate = todaysDate
 
-# define curve
-curve = QL.FlatForward(todaysDate, 0.01, QL.Actual365Fixed())
-tscurve = QL.YieldTermStructureHandle(curve)
+risk_free_rate = YieldTermStructureHandle(
+        FlatForward(todaysDate, 0.01, Actual365Fixed()))
 
 # CDS parameters
 recovery_rate = 0.5
-quoted_spreads = ( 0.0150, 0.0150, 0.0150, 0.0150 )
-tenors = [ QL.Period(3, QL.Months), QL.Period("6M"), QL.Period("1Y"), QL.Period("2Y")]
-maturities = [ calendar.adjust(todaysDate + x, QL.Following) for x in tenors]
+quoted_spreads = [ 0.0150, 0.0150, 0.0150, 0.0150 ]
+tenors = [ Period(3, Months), Period(6, Months),
+           Period(1, Years), Period(2, Years) ]
+maturities = [ calendar.adjust(todaysDate + x, Following) for x in tenors]
 
-instruments = [ QL.SpreadCdsHelper(QL.QuoteHandle(QL.SimpleQuote(quoted_spreads[x])), 
-							tenors[x], 
-							0, 
-							calendar, 
-							QL.Quarterly, 
-							QL.Following, 
-							QL.DateGeneration.TwentiethIMM, 
-							QL.Actual365Fixed(), 
-							recovery_rate, 
-							tscurve) for x in range(len(tenors))]
+instruments = [
+    SpreadCdsHelper(QuoteHandle(SimpleQuote(s)),
+                    tenor,
+                    0,
+                    calendar,
+                    Quarterly,
+                    Following,
+                    DateGeneration.TwentiethIMM,
+                    Actual365Fixed(),
+                    recovery_rate,
+                    risk_free_rate)
+    for s,tenor in zip(quoted_spreads, tenors) ]
 
-hazardcurve = QL.PiecewiseFlatHazardRate(todaysDate, instruments, QL.Actual365Fixed())
+hazard_curve = PiecewiseFlatHazardRate(todaysDate, instruments,
+                                       Actual365Fixed())
 print "Calibrated hazard rate values: "
-for x in hazardcurve.nodes():
-	print "hazard rate on %s is %.7f" % x
+for x in hazard_curve.nodes():
+    print "hazard rate on %s is %.7f" % x
 
 print "Some survival probability values: "
 print "1Y survival probability: %.4g, \n\t\texpected %.4g" % (
-				hazardcurve.survivalProbability(todaysDate + QL.Period("1Y")),
-				0.9704)
+    hazard_curve.survivalProbability(todaysDate + Period("1Y")),
+    0.9704)
 print "2Y survival probability: %.4g, \n\t\texpected %.4g" % (
-				hazardcurve.survivalProbability(todaysDate + QL.Period("2Y")),
-				0.9418)
+    hazard_curve.survivalProbability(todaysDate + Period("2Y")),
+    0.9418)
 
 # reprice instruments
 nominal = 1000000.0
-probability = QL.DefaultProbabilityTermStructureHandle(hazardcurve)
+probability = DefaultProbabilityTermStructureHandle(hazard_curve)
 
 # create a cds for every maturity
-allcds = []
-for x in range(len(tenors)):
-	schedule = QL.Schedule(todaysDate, maturities[x], QL.Period(QL.Quarterly),
-						   calendar, 
-						   QL.Following, QL.Unadjusted,
-						   QL.DateGeneration.TwentiethIMM,
-						   False)
-	cds = QL.CreditDefaultSwap(QL.Protection.Seller, nominal, quoted_spreads[x], schedule, QL.Following, QL.Actual365Fixed())
-	engine = QL.MidPointCdsEngine(probability, recovery_rate, tscurve)
-	cds.setPricingEngine(engine)
-	allcds.append(cds)
+all_cds = []
+for maturity, s in zip(maturities, quoted_spreads):
+    schedule = Schedule(todaysDate, maturity, Period(Quarterly),
+                        calendar, Following, Unadjusted,
+                        DateGeneration.TwentiethIMM, False)
+    cds = CreditDefaultSwap(Protection.Seller, nominal, s,
+                            schedule, Following, Actual365Fixed())
+    engine = MidPointCdsEngine(probability, recovery_rate, risk_free_rate)
+    cds.setPricingEngine(engine)
+    all_cds.append(cds)
 
 print "Repricing of quoted CDSs employed for calibration: "
-for x in range(len(tenors)):
-	print "%s fair spread: %.7g" % (tenors[x], allcds[x].fairSpread())
-	print "   NPV: %g" % allcds[x].NPV()
-	print "   default leg: %.7g" % allcds[x].defaultLegNPV()
-	print "   coupon leg: %.7g" % allcds[x].couponLegNPV()
-	print
+for i in range(len(tenors)):
+    print "%s fair spread: %.7g" % (tenors[i], all_cds[i].fairSpread())
+    print "   NPV: %g" % all_cds[i].NPV()
+    print "   default leg: %.7g" % all_cds[i].defaultLegNPV()
+    print "   coupon leg: %.7g" % all_cds[i].couponLegNPV()
+    print
