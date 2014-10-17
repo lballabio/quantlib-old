fa0aa681b14e251935e2ca8a70b2e3c2b7d54a45
Luigi Ballabio <luigi.ballabio@gmail.com>
2014-10-17 10:09:03 +0200

    Removed extra batch file.

--- QuantLib-SWIG/Python/compila.bat
+++
@@ -1,7 +0,0 @@
-rem aggiungere --debug a build e bdist per compilare la versione debug (serve python compilato in debug!)
-python setup.py wrap
-if not errorlevel 1 python setup.py build --static
-if not errorlevel 1 python setup.py bdist --formats=wininst
-if not errorlevel 1 python setup.py bdist --formats=msi
-
-pause
