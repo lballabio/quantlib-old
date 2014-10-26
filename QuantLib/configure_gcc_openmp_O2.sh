39c9980594543ebfef917f776da1861088e85bf0
Peter Caspers <pcaspers1973@gmail.com>
2014-10-26 18:29:29 +0100

    add two more configure scripts

---
+++ QuantLib/configure_gcc_openmp_O2.sh
@@ -0,0 +1,3 @@
+# configure gcc, with open mp support, optimization O2
+./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib --enable-openmp CXXFLAGS="-m64 -O2 -g -Wall -Wno-unused-local-typedefs -Wno-unknown-pragmas -std=c++11" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
+ 
