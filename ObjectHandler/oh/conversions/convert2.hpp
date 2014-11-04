01b54d2413786496f836548ae6220ff73fa31208
Eric Ehlers <eric.ehlers@nazcatech.be>
2014-11-04 12:35:12 +0100

    update docs

--- ObjectHandler/oh/conversions/convert2.hpp
+++ ObjectHandler/oh/conversions/convert2.hpp
@@ -2,6 +2,7 @@
 
 /*
  Copyright (C) 2008 Plamen Neykov
+ Copyright (C) 2014 Eric Ehlers
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
@@ -26,6 +27,25 @@
 
 namespace ObjectHandler {
 
+    /*! \name convert2
+    The functions in this file are used by addin functions to convert their
+    inputs into types recognized by the underlying library.
+
+    All of these functions are implemented as overrides of template function
+    "convert2".
+
+    The reason that these functions are called "convert2" is because they
+    replace an old function "convert" which has now been deleted.
+
+    These functions operate on an input value of type container_t.
+    \li On C++, container_t is a variant.
+    \li On Excel, container_t is an XLOPER.
+    */
+    //@{
+    //! Convert the input value from type container_t to type value_t.
+    /*! If the attempt fails then wrap the exception with additional info and
+        rethrow.
+    */
     template<class value_t, class container_t>
     value_t convert2(const container_t& c) {
         try {
@@ -37,6 +57,11 @@ namespace ObjectHandler {
         }
     }
 
+    //! Convert the input value from type container_t to type value_t.
+    /*! This override allows the caller to specify the parameter name, which
+        makes the error message clearer in the event of an error.  Otherwise
+        this function behaves the same as the one above.
+    */
     template<class value_t, class container_t>
     value_t convert2(const container_t& c, const std::string &parameterName) {
         try {
@@ -47,6 +72,10 @@ namespace ObjectHandler {
         }
     }
 
+    //! Convert the input value from type container_t to type value_t.
+    /*! This function allows the caller to provide a default value in the event
+        that the input is missing, which on Excel is true for inputs of type \#NA.
+    */
     template<class value_t, class container_t>
     value_t convert2(const container_t& c, const std::string &parameterName, const value_t& defaultValue) {
         try {
@@ -59,6 +88,15 @@ namespace ObjectHandler {
         }
     }
 
+    //! Convert the input value from type container_t to type value_t.
+    /*! This function allows the caller to provide an error value which will be
+        returned in the case of an exception.  This function does not throw.
+
+        An error may occur in two cases:
+
+        \li If the input value has an error state.  On Excel this is true for \#NUM, \#REF, etc.
+        \li If the attempted conversion throws.
+    */
     template<class value_t, class container_t>
     value_t convert2(const container_t& c, const std::string &parameterName, 
             const value_t& defaultValue, const value_t& errorValue) {
@@ -71,7 +109,7 @@ namespace ObjectHandler {
             return errorValue;
         }
     }
-
+    //@}
 }
 
 #endif
