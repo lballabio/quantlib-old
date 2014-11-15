053619b03c880b412fc075ed8f3d420d3740ad08
Peter Caspers <pcaspers1973@gmail.com>
2014-11-15 21:07:39 +0100

    switch to boost 1_57_0

--- QuantLib/configure_clang_sanitize.sh
+++ QuantLib/configure_clang_sanitize.sh
@@ -1,3 +1,3 @@
 # configure clang, with sanitizers (except memory)
 # use ASAN_OPTIONS="detect_leaks=1" to activate memory leak detection
-./configure --with-boost-include=/home/peter/boost_1_55_0 --with-boost-lib=/home/peter/boost_1_55_0/stage/lib CXX="clang++" CXXFLAGS="-O2 -g -Wall -std=c++11 -fsanitize=address,undefined,integer,unsigned-integer-overflow,vptr -Qunused-arguments" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
+./configure --with-boost-include=/home/peter/boost_1_57_0 --with-boost-lib=/home/peter/boost_1_57_0/stage/lib CXX="clang++" CXXFLAGS="-O2 -g -Wall -std=c++11 -fsanitize=address,undefined,integer,unsigned-integer-overflow,vptr -Qunused-arguments" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
