c7f66954b0d62e852fcd9358d246bd647223a2ca
Peter Caspers <pcaspers1973@gmail.com>
2015-02-13 17:51:20 +0100

    disable unused local typedef warning

--- QuantLib/configure_clang_leak.sh
+++ QuantLib/configure_clang_leak.sh
@@ -1,2 +1,2 @@
 # configure clang, with leak sanitizer (no slow down implied)
-./configure --with-boost-include=/home/peter/boost_1_57_0 --with-boost-lib=/home/peter/boost_1_57_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O3 -g -Wall -std=c++11 -fsanitize=leak -Qunused-arguments" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
+./configure --with-boost-include=/home/peter/boost_1_57_0 --with-boost-lib=/home/peter/boost_1_57_0/stage/lib CXX=clang++ CXXFLAGS="-m64 -O3 -g -Wall -std=c++11 -fsanitize=leak -Qunused-arguments -Wno-unused-local-typedef" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
