ba338137b781256e1097eafa605a1832ac07910b
Peter Caspers <pcaspers1973@gmail.com>
2015-03-15 18:59:39 +0100

    add error lines

--- QuantLib/configure_gcc.sh
+++ QuantLib/configure_gcc.sh
@@ -1,2 +1,2 @@
 # configure gcc, no open mp support
-./configure --with-boost-include=/home/peter/boost_1_57_0 --with-boost-lib=/home/peter/boost_1_57_0/stage/lib CXXFLAGS="-m64 -O3 -g -Wall -Wno-unused-local-typedefs -Wno-unknown-pragmas -std=c++11" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
+./configure --with-boost-include=/home/peter/boost_1_57_0 --with-boost-lib=/home/peter/boost_1_57_0/stage/lib --enable-error-lines CXXFLAGS="-m64 -O3 -g -Wall -Wno-unused-local-typedefs -Wno-unknown-pragmas -std=c++11" LIBS="-lntl -lgmp -lm -lboost_timer -lboost_chrono -lboost_system"
