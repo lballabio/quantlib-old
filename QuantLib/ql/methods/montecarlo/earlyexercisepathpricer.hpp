30ed2500e6a76e2ee3f7cea590839d955556a977
Peter Caspers <pcaspers1973@gmail.com>
2015-06-24 20:28:43 +0200

    add inspector for second basis system

--- QuantLib/ql/methods/montecarlo/earlyexercisepathpricer.hpp
+++ QuantLib/ql/methods/montecarlo/earlyexercisepathpricer.hpp
@@ -73,6 +73,10 @@ namespace QuantLib {
             state(const PathType& path, TimeType t) const = 0;
         virtual std::vector<boost::function1<ValueType, StateType> >
             basisSystem() const = 0;
+        virtual std::vector<boost::function1<ValueType, StateType> >
+            basisSystem2() const {
+                QL_FAIL("basis system 2 not provided");
+            }
     };
 }
 
