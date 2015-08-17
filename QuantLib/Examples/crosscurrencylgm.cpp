0770210b7a6bffb3366076fae6e911cd8576cf0a
Peter Caspers <pcaspers1973@gmail.com>
2015-08-17 17:01:08 +0200

    extend example

--- QuantLib/Examples/crosscurrencylgm.cpp
+++ QuantLib/Examples/crosscurrencylgm.cpp
@@ -28,15 +28,15 @@ int main() {
         std::vector<Date> volstepdates;
         std::vector<Real> volsteptimes;
         Array volsteptimes_a(0);
-        std::vector<Real> eurVols(1, 0.0010);
-        std::vector<Real> usdVols(1, 0.0025);
-        std::vector<Real> fxSigmas(1, 0.20);
+        std::vector<Real> eurVols(1, atof(getenv("EURVOL")));
+        std::vector<Real> usdVols(1, atof(getenv("USDVOL")));
+        std::vector<Real> fxSigmas(1, atof(getenv("FXVOL")));
         Array fxSigmas_a(fxSigmas.begin(), fxSigmas.end());
 
         boost::shared_ptr<Lgm1> eurLgm =
-            boost::make_shared<Lgm1>(eurYts, volstepdates, eurVols, 0.0020);
+            boost::make_shared<Lgm1>(eurYts, volstepdates, eurVols, 0.0000);
         boost::shared_ptr<Lgm1> usdLgm =
-            boost::make_shared<Lgm1>(usdYts, volstepdates, usdVols, 0.0080);
+            boost::make_shared<Lgm1>(usdYts, volstepdates, usdVols, 0.0000);
 
         std::vector<boost::shared_ptr<Lgm1> > singleModels;
         singleModels.push_back(eurLgm);
@@ -89,14 +89,141 @@ int main() {
         c[0][2] = 0.0; // FX
         c[1][0] = 0.0;
         c[1][1] = 1.0;
-        c[1][2] = 0.95; // EUR
+        c[1][2] = 0.0; // EUR
         c[2][0] = 0.0;
-        c[2][1] = 0.95;
+        c[2][1] = 0.0;
         c[2][2] = 1.0; // USD
 
         boost::shared_ptr<detail::CcLgmPiecewise> ccParam =
             boost::make_shared<detail::CcLgmPiecewise>(fxParametrizations,
                                                        lgmParametrizations, c);
+
+        ccParam->update();
+
+        // test parametrization
+
+        std::clog.precision(12);
+        // std::clog << "H0(0.0) = " << ccParam->H_i(0,0.0) << std::endl;
+        // std::clog << "H0(1.0) = " << ccParam->H_i(0,1.0) << std::endl;
+        // std::clog << "H0(2.0) = " << ccParam->H_i(0,2.0) << std::endl;
+        // std::clog << "H1(0.0) = " << ccParam->H_i(1,0.0) << std::endl;
+        // std::clog << "H1(1.0) = " << ccParam->H_i(1,1.0) << std::endl;
+        // std::clog << "H1(2.0) = " << ccParam->H_i(1,2.0) << std::endl;
+        // std::clog << "zeta0(0.0) = " << ccParam->zeta_i(0,0.0) << std::endl;
+        // std::clog << "zeta0(1.0) = " << ccParam->zeta_i(0,1.0) << std::endl;
+        // std::clog << "zeta0(2.0) = " << ccParam->zeta_i(0,2.0) << std::endl;
+        // std::clog << "zeta0(3.0) = " << ccParam->zeta_i(0,3.0) << std::endl;
+        // std::clog << "zeta1(0.0) = " << ccParam->zeta_i(1,0.0) << std::endl;
+        // std::clog << "zeta1(1.0) = " << ccParam->zeta_i(1,1.0) << std::endl;
+        // std::clog << "zeta1(2.0) = " << ccParam->zeta_i(1,2.0) << std::endl;
+        // std::clog << "zeta1(3.0) = " << ccParam->zeta_i(1,3.0) << std::endl;
+        // std::clog << "alphaialphaj(0.0) = " << ccParam->alpha_i_alpha_j(0,0,0.0) << std::endl;
+        // std::clog << "alphaialphaj(1.0) = " << ccParam->alpha_i_alpha_j(0,0,1.0) << std::endl;
+        // std::clog << "alphaialphaj(2.0) = " << ccParam->alpha_i_alpha_j(0,0,2.0) << std::endl;
+        // std::clog << "alphaialphaj(0.0) = " << ccParam->alpha_i_alpha_j(1,1,0.0) << std::endl;
+        // std::clog << "alphaialphaj(1.0) = " << ccParam->alpha_i_alpha_j(1,1,1.0) << std::endl;
+        // std::clog << "alphaialphaj(2.0) = " << ccParam->alpha_i_alpha_j(1,1,2.0) << std::endl;
+        // std::clog << "alphaialphaj(0.0) = " << ccParam->alpha_i_alpha_j(0,1,0.0) << std::endl;
+        // std::clog << "alphaialphaj(1.0) = " << ccParam->alpha_i_alpha_j(0,1,1.0) << std::endl;
+        // std::clog << "alphaialphaj(2.0) = " << ccParam->alpha_i_alpha_j(0,1,2.0) << std::endl;
+        // std::clog << "alphaialphaj(0.0) = " << ccParam->alpha_i_alpha_j(1,0,0.0) << std::endl;
+        // std::clog << "alphaialphaj(1.0) = " << ccParam->alpha_i_alpha_j(1,0,1.0) << std::endl;
+        // std::clog << "alphaialphaj(2.0) = " << ccParam->alpha_i_alpha_j(1,0,2.0) << std::endl;
+        // std::clog << "sigmaisigmaj(0.0) = " << ccParam->sigma_i_sigma_j(0,0,0.0) << std::endl;
+        // std::clog << "sigmaisigmaj(1.0) = " << ccParam->sigma_i_sigma_j(0,0,1.0) << std::endl;
+        // std::clog << "sigmaisigmaj(2.0) = " << ccParam->sigma_i_sigma_j(0,0,2.0) << std::endl;
+        // std::clog << "alphaisigmaj(0.0) = " << ccParam->alpha_i_sigma_j(0,0,0.0) << std::endl;
+        // std::clog << "alphaisigmaj(1.0) = " << ccParam->alpha_i_sigma_j(0,0,1.0) << std::endl;
+        // std::clog << "alphaisigmaj(2.0) = " << ccParam->alpha_i_sigma_j(0,0,2.0) << std::endl;
+        // std::clog << "alphaisigmaj(0.0) = " << ccParam->alpha_i_sigma_j(1,0,0.0) << std::endl;
+        // std::clog << "alphaisigmaj(1.0) = " << ccParam->alpha_i_sigma_j(1,0,1.0) << std::endl;
+        // std::clog << "alphaisigmaj(2.0) = " << ccParam->alpha_i_sigma_j(1,0,2.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(0,0,0.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(0,0,1.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(0,0,2.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(1,1,0.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(1,1,1.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(1,1,2.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(1,0,0.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(1,0,1.0) << std::endl;
+        // std::clog << "HiAlphaIAlphaJ(0.0) = " << ccParam->H_i_alpha_i_alpha_j(1,0,2.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(0,0,0.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(0,0,1.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(0,0,2.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(1,1,0.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(1,1,1.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(1,1,2.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(1,0,0.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(1,0,1.0) << std::endl;
+        // std::clog << "HiHjAlphaIAlphaJ(0.0) = " << ccParam->H_i_H_j_alpha_i_alpha_j(1,0,2.0) << std::endl;
+        // std::clog << "HiAlphaISigmaJ(0.0) = " << ccParam->H_i_alpha_i_sigma_j(0,0,0.0) << std::endl;
+        // std::clog << "HiAlphaISigmaJ(0.0) = " << ccParam->H_i_alpha_i_sigma_j(0,0,1.0) << std::endl;
+        // std::clog << "HiAlphaISigmaJ(0.0) = " << ccParam->H_i_alpha_i_sigma_j(0,0,2.0) << std::endl;
+        // std::clog << "HiAlphaISigmaJ(0.0) = " << ccParam->H_i_alpha_i_sigma_j(1,0,0.0) << std::endl;
+        // std::clog << "HiAlphaISigmaJ(0.0) = " << ccParam->H_i_alpha_i_sigma_j(1,0,1.0) << std::endl;
+        // std::clog << "HiAlphaISigmaJ(0.0) = " << ccParam->H_i_alpha_i_sigma_j(1,0,2.0) << std::endl;
+        // std::clog << "int_alphaialphaj(0.0) = " << ccParam->int_alpha_i_alpha_j(0,0,0.0,0.0) << std::endl;
+        // std::clog << "int_alphaialphaj(1.0) = " << ccParam->int_alpha_i_alpha_j(0,0,0.0,1.0) << std::endl;
+        // std::clog << "int_alphaialphaj(2.0) = " << ccParam->int_alpha_i_alpha_j(0,0,0.0,2.0) << std::endl;
+        // std::clog << "int_alphaialphaj(0.0) = " << ccParam->int_alpha_i_alpha_j(1,1,0.0,0.0) << std::endl;
+        // std::clog << "int_alphaialphaj(1.0) = " << ccParam->int_alpha_i_alpha_j(1,1,0.0,1.0) << std::endl;
+        // std::clog << "int_alphaialphaj(2.0) = " << ccParam->int_alpha_i_alpha_j(1,1,0.0,2.0) << std::endl;
+        // std::clog << "int_alphaialphaj(0.0) = " << ccParam->int_alpha_i_alpha_j(0,1,0.0,0.0) << std::endl;
+        // std::clog << "int_alphaialphaj(1.0) = " << ccParam->int_alpha_i_alpha_j(0,1,0.0,1.0) << std::endl;
+        // std::clog << "int_alphaialphaj(2.0) = " << ccParam->int_alpha_i_alpha_j(0,1,0.0,2.0) << std::endl;
+        // std::clog << "int_alphaialphaj(0.0) = " << ccParam->int_alpha_i_alpha_j(1,0,0.0,0.0) << std::endl;
+        // std::clog << "int_alphaialphaj(1.0) = " << ccParam->int_alpha_i_alpha_j(1,0,0.0,1.0) << std::endl;
+        // std::clog << "int_alphaialphaj(2.0) = " << ccParam->int_alpha_i_alpha_j(1,0,0.0,2.0) << std::endl;
+        // std::clog << "int_sigmaisigmaj(0.0) = " << ccParam->int_sigma_i_sigma_j(0,0,0.0,0.0) << std::endl;
+        // std::clog << "int_sigmaisigmaj(1.0) = " << ccParam->int_sigma_i_sigma_j(0,0,0.0,1.0) << std::endl;
+        // std::clog << "int_sigmaisigmaj(2.0) = " << ccParam->int_sigma_i_sigma_j(0,0,0.0,2.0) << std::endl;
+        // std::clog << "int_alphaisigmaj(0.0) = " << ccParam->int_alpha_i_sigma_j(0,0,0.0,0.0) << std::endl;
+        // std::clog << "int_alphaisigmaj(1.0) = " << ccParam->int_alpha_i_sigma_j(0,0,0.0,1.0) << std::endl;
+        // std::clog << "int_alphaisigmaj(2.0) = " << ccParam->int_alpha_i_sigma_j(0,0,0.0,2.0) << std::endl;
+        // std::clog << "int_alphaisigmaj(0.0) = " << ccParam->int_alpha_i_sigma_j(1,0,0.0,0.0) << std::endl;
+        // std::clog << "int_alphaisigmaj(1.0) = " << ccParam->int_alpha_i_sigma_j(1,0,0.0,1.0) << std::endl;
+        // std::clog << "int_alphaisigmaj(2.0) = " << ccParam->int_alpha_i_sigma_j(1,0,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(0.0) = " << ccParam->int_H_i_alpha_i_alpha_j(0,0,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(1.0) = " << ccParam->int_H_i_alpha_i_alpha_j(0,0,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(2.0) = " << ccParam->int_H_i_alpha_i_alpha_j(0,0,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(0.0) = " << ccParam->int_H_i_alpha_i_alpha_j(1,1,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(1.0) = " << ccParam->int_H_i_alpha_i_alpha_j(1,1,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(2.0) = " << ccParam->int_H_i_alpha_i_alpha_j(1,1,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(0.0) = " << ccParam->int_H_i_alpha_i_alpha_j(0,1,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(1.0) = " << ccParam->int_H_i_alpha_i_alpha_j(0,1,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(2.0) = " << ccParam->int_H_i_alpha_i_alpha_j(0,1,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(0.0) = " << ccParam->int_H_i_alpha_i_alpha_j(1,0,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(1.0) = " << ccParam->int_H_i_alpha_i_alpha_j(1,0,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_alphaialphaj(2.0) = " << ccParam->int_H_i_alpha_i_alpha_j(1,0,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(0.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(0,0,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(1.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(0,0,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(2.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(0,0,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(0.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(1,1,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(1.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(1,1,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(2.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(1,1,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(0.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(0,1,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(1.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(0,1,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(2.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(0,1,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(0.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(1,0,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(1.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(1,0,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_H_j_alphaialphaj(2.0) = " << ccParam->int_H_i_H_j_alpha_i_alpha_j(1,0,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_alphaisigmaj(0.0) = " << ccParam->int_H_i_alpha_i_sigma_j(0,0,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_alphaisigmaj(1.0) = " << ccParam->int_H_i_alpha_i_sigma_j(0,0,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_alphaisigmaj(2.0) = " << ccParam->int_H_i_alpha_i_sigma_j(0,0,0.0,2.0) << std::endl;
+        // std::clog << "int_H_i_alphaisigmaj(0.0) = " << ccParam->int_H_i_alpha_i_sigma_j(1,0,0.0,0.0) << std::endl;
+        // std::clog << "int_H_i_alphaisigmaj(1.0) = " << ccParam->int_H_i_alpha_i_sigma_j(1,0,0.0,1.0) << std::endl;
+        // std::clog << "int_H_i_alphaisigmaj(2.0) = " << ccParam->int_H_i_alpha_i_sigma_j(1,0,0.0,2.0) << std::endl;
+
+        // std::clog << "rho alpha-alpha 00" << ccParam->rho_alpha_alpha(0,0) << std::endl;
+        // std::clog << "rho alpha-alpha 01" << ccParam->rho_alpha_alpha(0,1) << std::endl;
+        // std::clog << "rho alpha-alpha 10" << ccParam->rho_alpha_alpha(1,0) << std::endl;
+        // std::clog << "rho alpha-alpha 11" << ccParam->rho_alpha_alpha(1,1) << std::endl;
+        // std::clog << "rho alpha-sigma 00" << ccParam->rho_alpha_sigma(0,0) << std::endl;
+        // std::clog << "rho alpha-sigma 10" << ccParam->rho_alpha_sigma(1,0) << std::endl;
+        // std::clog << "rho sigma-sigma 00" << ccParam->rho_sigma_sigma(0,0) << std::endl;
+
+        // end test parametrization
+
 
         boost::shared_ptr<
             CcLgmProcess<detail::CcLgmPiecewise, detail::LgmFxPiecewiseSigma,
@@ -108,8 +235,8 @@ int main() {
 
         // generate paths
 
-        Size n = 1;
-        Size steps = 10;
+        Size n = atoi(getenv("N")); // N paths
+        Size steps = 5.0 * atof(getenv("STEPS")); // STEPS steps per year
         TimeGrid grid(5.0, steps);
 
         // boost::shared_ptr<StochasticProcess> process =
@@ -124,7 +251,7 @@ int main() {
             paths.push_back(pg.next());
         }
 
-        for (Size i = 0; i < steps; ++i) {
+        for (Size i = 0; i < paths[0].value[0].length(); ++i) {
             std::cout << grid[i] << " ";
             for (Size j = 0; j < n; ++j) {
                 std::cout << std::exp(paths[j].value[0][i]) << " "
