4992459cc1a7caf9af3c5d76dc1bb6f872490561
Peter Caspers <pcaspers1973@gmail.com>
2015-08-22 21:33:45 +0200

    fix compilation errors

--- QuantLib/ql/experimental/models/cclgmanalyticfxoptionengine.hpp
+++ QuantLib/ql/experimental/models/cclgmanalyticfxoptionengine.hpp
@@ -22,36 +22,46 @@
     TODO add optional discounting curve
 */
 
-#ifndef quantlib_cclgm_analytic_fx_option_engine.hpp
-#define quantlib_cclgm_analytic_fx_option_engine .hpp
+#ifndef quantlib_cclgm_analytic_fx_option_engine_hpp
+#define quantlib_cclgm_analytic_fx_option_engine_hpp
+
+#include <ql/experimental/models/cclgm.hpp>
+#include <ql/experimental/models/cclgmpiecewise.hpp>
+#include <ql/experimental/models/lgmfxpiecewisesigma.hpp>
+#include <ql/experimental/models/lgmpiecewisealphaconstantkappa.hpp>
+
+#include <ql/instruments/vanillaoption.hpp>
 
 namespace QuantLib {
 
-template <class Impl, ImplFx, ImplLgm>
+template <class Impl, class ImplFx, class ImplLgm>
 class CcLgmAnalyticFxOptionEngine : public VanillaOption::engine {
   public:
-    typedef model_type CcLgm<Impl, ImplFx, ImplLgm>;
-    CcLgmAnalyticFxOptionEngine(const boost::shared_ptr<model_type> &model,
-                                const Size foreignCurrency);
+    CcLgmAnalyticFxOptionEngine(
+        const boost::shared_ptr<CcLgm<Impl, ImplFx, ImplLgm> > &model,
+        const Size foreignCurrency);
     void calculate() const;
 
   private:
-    boost::shared_ptr<model_type> model_;
+    boost::shared_ptr<CcLgm<Impl, ImplFx, ImplLgm> > model_;
     Size foreignCurrency_;
-}
+};
 
 // shortcut for engine applicable to the CcLgm1 model incarnation
-typedef CcLgmAnalyticFxOptionEngine<CcLgmPiecewise, LgmFxPiecewiseSigma,
-                                    LgmPiecewiseAlphaConstantKappa>
-    CcAnalyticFxOptionEngine1;
+typedef CcLgmAnalyticFxOptionEngine<
+    detail::CcLgmPiecewise, detail::LgmFxPiecewiseSigma,
+    detail::LgmPiecewiseAlphaConstantKappa> CcAnalyticFxOptionEngine1;
 
 // implementation
 
+template <class Impl, class ImplFx, class ImplLgm>
 CcLgmAnalyticFxOptionEngine<Impl, ImplFx, ImplLgm>::CcLgmAnalyticFxOptionEngine(
-    const boost::shared_ptr<mode_type> &model)
-    : model_(model) {}
+    const boost::shared_ptr<CcLgm<Impl, ImplFx, ImplLgm> > &model,
+    const Size foreignCurrency)
+    : model_(model), foreignCurrency_(foreignCurrency) {}
 
-CcLgmAnalyticFxOptionEngine<Impl, ImplFx, ImplLgm>::calculate() const {
+template <class Impl, class ImplFx, class ImplLgm>
+void CcLgmAnalyticFxOptionEngine<Impl, ImplFx, ImplLgm>::calculate() const {
 
     QL_REQUIRE(arguments_.exercise->type() == Exercise::European,
                "only European options are allowed");
@@ -63,39 +73,39 @@ CcLgmAnalyticFxOptionEngine<Impl, ImplFx, ImplLgm>::calculate() const {
     Date expiry = arguments_.exercise->lastDate();
     Time t = model_->termStructure(0)->timeFromReference(expiry);
 
-    Real foreignDiscount = model_->curve(foreignCurrency)->discount(expiry);
+    Real foreignDiscount = model_->curve(foreignCurrency_)->discount(expiry);
     Real domesticDiscount = model_->curve(0)->discount(expiry);
 
     Real fxForward = model_->stateProcess()->initialValues[foreignCurrency_] *
                      foreignDiscount / domesticDiscount;
 
-    detail::CcLgmParametrization<Impl, ImplFx, ImplLm> p =
+    boost::shared_ptr<detail::CcLgmParametrization<Impl, ImplFx, ImplLgm> > p =
         model_->parametrization();
 
     Size &i = foreignCurrency_; // just an alias ...
 
     Real variance =
         // first term
-        p.H_i(0, t) * p.H_i(0, t) * int_alpha_i_alpha_j(0, 0, 0.0, t) -
-        2.0 * p_H_i(0, t) * int_H_i_alpha_i_alpha_j(0, 0, 0.0, t) +
-        p.int_H_i_H_j_alpha_i_alpha_j(0, 0, 0.0, t) +
+        p->H_i(0, t) * p->H_i(0, t) * p->int_alpha_i_alpha_j(0, 0, 0.0, t) -
+        2.0 * p->H_i(0, t) * p->int_H_i_alpha_i_alpha_j(0, 0, 0.0, t) +
+        p->int_H_i_H_j_alpha_i_alpha_j(0, 0, 0.0, t) +
         // second term
-        p.H_i(i + 1, t) * p.H_i(i + 1, t) *
-            int_alpha_i_alpha_j(i + 1, i + 1, 0.0, t) -
-        2.0 * p_H_i(0, t) * int_H_i_alpha_i_alpha_j(0, 0, 0.0, t) +
-        p.int_H_i_H_j_alpha_i_alpha_j(i + 1, i + 1, 0.0, t) -
+        p->H_i(i + 1, t) * p->H_i(i + 1, t) *
+            p->int_alpha_i_alpha_j(i + 1, i + 1, 0.0, t) -
+        2.0 * p->H_i(0, t) * p->int_H_i_alpha_i_alpha_j(0, 0, 0.0, t) +
+        p->int_H_i_H_j_alpha_i_alpha_j(i + 1, i + 1, 0.0, t) -
         // third term
-        2.0 * (p.H_i(0, t) * p.H_i(i + 1, t) *
-                   p.int_alpha_i_alpha_j(0, i + 1, 0.0, t) -
-               p.H_i(0, t) * p.int_H_i_alpha_i_alpha_j(0, i + 1, 0.0, t) -
-               p.H_i(i + 1, t) * p.int_H_i_alpha_i_alpha_j(i + 1, 0, 0.0, t) +
-               p.int_H_i_H_j_alpha_i_alpha_j(0, i + 1, 0.0, t)) +
+        2.0 * (p->H_i(0, t) * p->H_i(i + 1, t) *
+                   p->int_alpha_i_alpha_j(0, i + 1, 0.0, t) -
+               p->H_i(0, t) * p->int_H_i_alpha_i_alpha_j(0, i + 1, 0.0, t) -
+               p->H_i(i + 1, t) * p->int_H_i_alpha_i_alpha_j(i + 1, 0, 0.0, t) +
+               p->int_H_i_H_j_alpha_i_alpha_j(0, i + 1, 0.0, t)) +
         // forth term
-        2.0 * (p.H_i(0, t) * p.int_alpha_i_sigma_j(0, i, 0.0, t) -
-               p.int_H_i_alpha_i_sigma_j(0, i, 0.0, t)) -
+        2.0 * (p->H_i(0, t) * p->int_alpha_i_sigma_j(0, i, 0.0, t) -
+               p->int_H_i_alpha_i_sigma_j(0, i, 0.0, t)) -
         // fifth term
-        2.0 * (p.H_i(i + 1, t) * p.int_alpha_i_sigma_j(i + 1, i, 0.0, t) -
-               p.int_H_i_alpha_i_sigma_j(i + 1, i, 0.0, t));
+        2.0 * (p->H_i(i + 1, t) * p->int_alpha_i_sigma_j(i + 1, i, 0.0, t) -
+               p->int_H_i_alpha_i_sigma_j(i + 1, i, 0.0, t));
 
     BlackCalculator black(payoff, fxForward, std::sqrt(variance),
                           domesticDiscount);
