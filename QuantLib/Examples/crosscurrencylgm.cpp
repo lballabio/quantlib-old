7c628b8066bd0b521365a7eab40205589c9d94c5
Peter Caspers <pcaspers1973@gmail.com>
2015-08-25 18:02:11 +0200

    extend the example a bit

--- QuantLib/Examples/crosscurrencylgm.cpp
+++ QuantLib/Examples/crosscurrencylgm.cpp
@@ -33,10 +33,10 @@ int main() {
         std::vector<Real> fxSigmas(1, atof(getenv("FXVOL")));
         Array fxSigmas_a(fxSigmas.begin(), fxSigmas.end());
 
-        boost::shared_ptr<Lgm1> eurLgm =
-            boost::make_shared<Lgm1>(eurYts, volstepdates, eurVols, 0.0000);
-        boost::shared_ptr<Lgm1> usdLgm =
-            boost::make_shared<Lgm1>(usdYts, volstepdates, usdVols, 0.0000);
+        boost::shared_ptr<Lgm1> eurLgm = boost::make_shared<Lgm1>(
+            eurYts, volstepdates, eurVols, atof(getenv("EURMR")));
+        boost::shared_ptr<Lgm1> usdLgm = boost::make_shared<Lgm1>(
+            usdYts, volstepdates, usdVols, atof(getenv("USDMR")));
 
         std::vector<boost::shared_ptr<Lgm1> > singleModels;
         singleModels.push_back(eurLgm);
@@ -84,17 +84,29 @@ int main() {
 
         Matrix c(3, 3);
         //  FX             EUR         USD
-        // c[0][0] = 1.0; c[0][1] = -0.8; c[0][2] = 0.8; // FX
-        // c[1][0] = -0.8; c[1][1] = 1.0; c[1][2] = -0.5; // EUR
-        // c[2][0] = 0.8; c[2][1] = -0.5; c[2][2] = 1.0; // USD
+        c[0][0] = 1.0;
+        c[0][1] = -0.8;
+        c[0][2] = 0.8; // FX
+        c[1][0] = -0.8;
+        c[1][1] = 1.0;
+        c[1][2] = -0.5; // EUR
+        c[2][0] = 0.8;
+        c[2][1] = -0.5;
+        c[2][2] = 1.0; // USD
         //  FX             EUR         USD
         // c[0][0] = 1.0; c[0][1] = 0.8; c[0][2] = 0.8; // FX
         // c[1][0] = 0.8; c[1][1] = 1.0; c[1][2] = 0.5; // EUR
         // c[2][0] = 0.8; c[2][1] = 0.5; c[2][2] = 1.0; // USD
         //  FX             EUR         USD
-        c[0][0] = 1.0; c[0][1] = 0.0; c[0][2] = 0.0; // FX
-        c[1][0] = 0.0; c[1][1] = 1.0; c[1][2] = 0.0; // EUR
-        c[2][0] = 0.0; c[2][1] = 0.0; c[2][2] = 1.0; // USD
+        // c[0][0] = 1.0;
+        // c[0][1] = 0.0;
+        // c[0][2] = 0.0; // FX
+        // c[1][0] = 0.0;
+        // c[1][1] = 1.0;
+        // c[1][2] = 0.0; // EUR
+        // c[2][0] = 0.0;
+        // c[2][1] = 0.0;
+        // c[2][2] = 1.0; // USD
 
         boost::shared_ptr<detail::CcLgmPiecewise> ccParam =
             boost::make_shared<detail::CcLgmPiecewise>(fxParametrizations,
@@ -339,51 +351,77 @@ int main() {
 
         // generate paths
 
-        Size n = atoi(getenv("N"));               // N paths
-        Size steps = 1.0 * atof(getenv("STEPS")); // STEPS steps per year
-        TimeGrid grid(1.0, steps);
+        Size n = atoi(getenv("N")); // N paths
+        Time T = atof(getenv("T")); // cashflow time
+        Size steps = static_cast<Size>(
+            T * atof(getenv("STEPS")));   // STEPS steps per year
+        Size seed = atoi(getenv("SEED")); // rng seed
+        TimeGrid grid(T, steps);
 
         PseudoRandom::rsg_type sg =
-            PseudoRandom::make_sequence_generator(steps * 3, 14);
+            PseudoRandom::make_sequence_generator(steps * 3, seed);
         MultiPathGenerator<PseudoRandom::rsg_type> pg(process, grid, sg, false);
+
+        PseudoRandom::rsg_type sg2 =
+            PseudoRandom::make_sequence_generator(steps, seed);
+        PathGenerator<PseudoRandom::rsg_type> pg2(usdLgm->stateProcess(), grid,
+                                                  sg2, false);
 
         std::vector<Sample<MultiPath> > paths;
         for (Size j = 0; j < n; ++j) {
             paths.push_back(pg.next());
         }
 
+        std::vector<Sample<Path> > paths2;
+        for (Size j = 0; j < n; ++j) {
+            paths2.push_back(pg2.next());
+        }
+
         // output paths for visual inspection in gnuplot
 
-        for (Size i = 0; i < paths[0].value[0].length(); ++i) {
-            std::cout << grid[i] << " ";
-            for (Size j = 0; j < n; ++j) {
-                std::cout << std::exp(paths[j].value[0][i]) << " "
-                          << paths[j].value[1][i] << " " << paths[j].value[2][i]
-                          << " ";
+        if (atoi(getenv("OUTPUT"))) {
+            // cc model paths
+            for (Size i = 0; i < paths[0].value[0].length(); ++i) {
+                std::cout << grid[i] << " ";
+                for (Size j = 0; j < n; ++j) {
+                    std::cout << std::exp(paths[j].value[0][i]) << " "
+                              << paths[j].value[1][i] << " "
+                              << paths[j].value[2][i] << " "
+                              << paths2[j].value[i] << " ";
+                }
+                std::cout << "\n";
             }
-            std::cout << "\n";
         }
 
         // test: 1 USD in 1y, priced in domestic measure
 
         Size l = paths[0].value[0].length() - 1;
-        IncrementalStatistics stat;
+        IncrementalStatistics stat, stat2;
         for (Size j = 0; j < n; ++j) {
             Real fx = std::exp(paths[j].value[0][l]);
             Real zeur = paths[j].value[1][l];
             Real zusd = paths[j].value[2][l];
-            Real y =
-                (zeur - eurLgm->stateProcess()->expectation(0.0, 0.0, 1.0)) /
-                eurLgm->stateProcess()->stdDeviation(0.0, 0.0, 1.0);
-            stat.add(1.0 * fx / eurLgm->numeraire(1.0,y));
+            Real zusd2 = paths2[j].value[l];
+            Real stddev = eurLgm->stateProcess()->stdDeviation(0.0, 0.0, T);
+            Real stddev2 = usdLgm->stateProcess()->stdDeviation(0.0, 0.0, T);
+            Real y = (zeur - eurLgm->stateProcess()->expectation(0.0, 0.0, T)) /
+                     (!close_enough(stddev, 0.0) ? stddev : 1.0);
+            Real y2 =
+                (zusd2 - usdLgm->stateProcess()->expectation(0.0, 0.0, T)) /
+                (!close_enough(stddev2, 0.0) ? stddev2 : 1.0);
+            stat.add(1.0 * fx / eurLgm->numeraire(T, y));
+            stat2.add(1.0 / usdLgm->numeraire(T, y2));
         }
         std::clog << "1 USD @ 1y  = " << stat.mean() << " EUR +/- "
                   << stat.errorEstimate() << std::endl;
         ;
-        std::clog << "curve price = " << usdYts->discount(1.0) << " spot "
+        std::clog << "curve price = " << usdYts->discount(T) << " spot "
                   << std::exp(fxSpots[0]->value()) << " EUR price "
-                  << usdYts->discount(1.0) * std::exp(fxSpots[0]->value())
+                  << usdYts->discount(T) * std::exp(fxSpots[0]->value())
                   << "\n";
+
+        std::clog << "1 USD @ 1y = " << stat2.mean() << " USD +/-"
+                  << stat2.errorEstimate() << std::endl;
 
         return 0;
 
--- QuantLib/Examples/crosscurrencylgm.gp
+++ QuantLib/Examples/crosscurrencylgm.gp
@@ -2,16 +2,17 @@ unset key
 
 cd "~/"
 
-
 # fx
-plot for [n=0:4] 'mcpaths.dat' u 1:(column(2+3*n)) w l
+plot for [n=0:4] 'mcpaths.dat' u 1:(column(2+4*n)) w l
 # eur
-plot for [n=0:4] 'mcpaths.dat' u 1:(column(3+3*n)) w l
+plot for [n=0:4] 'mcpaths.dat' u 1:(column(3+4*n)) w l
 # usd
-plot for [n=0:4] 'mcpaths.dat' u 1:(column(4+3*n)) w l
+plot for [n=0:4] 'mcpaths.dat' u 1:(column(4+4*n)) w l
+# usd domestic measure (anderer seed !)
+plot for [n=0:4] 'mcpaths.dat' u 1:(column(5+4*n)) w l
 
 # fx process
-plot for [n=0:4] 'mcpaths.dat' u 1:(column(2+3*n)) w l
+plot for [n=0:4] 'mcpaths.dat' u 1:(column(2+4*n)) w l
 
 # eur and usd lgm process
-plot for [n=0:4] 'mcpaths.dat' u 1:(column(3+3*n)) w l lt 1, for [n=0:4] '' u 1:(column(4+3*n)) w l lt 2
+plot for [n=0:4] 'mcpaths.dat' u 1:(column(3+4*n)) w l lt 1, for [n=0:4] '' u 1:(column(4+4*n)) w l lt 2
