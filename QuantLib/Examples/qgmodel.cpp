38e5b831df517b5e85e29434b550f2adcfd818fc
Peter Caspers <pcaspers1973@gmail.com>
2015-09-24 15:28:18 +0200

    extend example

--- QuantLib/Examples/qgmodel.cpp
+++ QuantLib/Examples/qgmodel.cpp
@@ -2,43 +2,126 @@
 #include <ql/time/calendars/target.hpp>
 #include <ql/experimental/models/qg1dlinearmodel.hpp>
 
+#include <ql/indexes/swap/euriborswap.hpp>
+
 #include <iostream>
 
 using namespace QuantLib;
 
 int main() {
 
-    Date refDate(17,Sep,2015);
+    Date refDate(17, Sep, 2015);
     Settings::instance().evaluationDate() = refDate;
 
-    Handle<YieldTermStructure> yts(boost::make_shared<FlatForward>(0,TARGET(),0.03,Actual365Fixed()));
+    Handle<YieldTermStructure> yts(
+        boost::make_shared<FlatForward>(0, TARGET(), 0.03, Actual365Fixed()));
 
-    // no steps
+    // steps
 
     std::vector<Date> stepDates;
 
-    for(Size i=1;i<10;++i) {
-        stepDates.push_back(TARGET().advance(refDate,i*Years));
+    for (Size i = 1; i < 10; ++i) {
+        stepDates.push_back(TARGET().advance(refDate, i * Years));
     }
 
-    std::vector<Real> lambda(10,1.0);
-    std::vector<Real> alpha(10,1.0);
-    std::vector<Real> beta(10,1.0);
-    std::vector<Real> kappa(10,0.02);
+    std::vector<Real> lambda(10, 1.0);
+    std::vector<Real> alpha(10, 1.0);
+    std::vector<Real> beta(10, 1.0);
+    std::vector<Real> kappa(10, 0.02);
 
-    Qg1dLinearModel model(yts,stepDates,lambda, alpha, beta, kappa);
+    Qg1dLinearModel model(yts, stepDates, lambda, alpha, beta, kappa);
 
     // test kappa, h, G
 
-    std::cout.precision(16);
+    // std::cout.precision(16);
+    // Real t = 0.0;
+    // while (t < 10.0) {
+    //     std::cout << t << " " << model.kappa(t) << " " << model.h(t) << " "
+    //               << std::exp(-0.02 * t) << " " << model.G(0.0, t) << " "
+    //               << -(exp(-0.02 * t) - 1.0) / 0.02 << std::endl;
+    //     t += 0.01;
+    // }
 
-    Real t = 0.0;
-    while( t < 10.0 ) {
-        std::cout << t << " " << model.kappa(t) << " " << model.h(t) << " " << std::exp(-0.02*t) << " " << model.G(0.0,t) << " " << -(exp(-0.02*t)-1.0)/0.02 << std::endl;
-        t += 0.01;
+    // test yApprox
+
+    Real y = model.yApprox(10.0);
+    std::cout << "yApprox(10) = " << y << std::endl;
+
+    // test swap rate
+
+    Date startDate = TARGET().advance(refDate, 10 * Years);
+    boost::shared_ptr<SwapIndex> index =
+        boost::make_shared<EuriborSwapIsdaFixA>(10 * Years);
+
+    Real x = -0.02;
+    while (x <= 0.02) {
+        std::cout << x << " "
+                  << model.swapRate(startDate, startDate, index, 10 * Years, x,
+                                    y)
+                  << " "
+                  << -std::log(
+                         model.zerobond(TARGET().advance(startDate, 10 * Years),
+                                        startDate, x, y)) /
+                         10.0
+                  << std::endl;
+        x += 0.0001;
     }
 
+    // test numerical inversion of swap rate (fixed y)
+
+    std::vector<Real> times, taus;
+    Real T0;
+    model.timesAndTaus(startDate, index, 10 * Years, T0, times, taus);
+    std::cout << "times and taus result: T0=" << T0 << std::endl;
+    for (Size i = 0; i < times.size(); ++i) {
+        std::cout << times[i] << " " << taus[i] << std::endl;
+    }
+
+    std::cout << "x( 0.0104 ) = "
+              << model.sInvX(10.0, 10.0, times, taus,
+                             Handle<YieldTermStructure>(), 0.0104)
+              << std::endl;
+    std::cout << "x( 0.0304 ) = "
+              << model.sInvX(10.0, 10.0, times, taus,
+                             Handle<YieldTermStructure>(), 0.0304)
+              << std::endl;
+    std::cout << "x( 0.0504 ) = "
+              << model.sInvX(10.0, 10.0, times, taus,
+                             Handle<YieldTermStructure>(), 0.0504)
+              << std::endl;
+
+    // test swap rate derivative
+
+    std::cout << "dS/dx ( -0.02 ) = "
+              << model.dSwapRateDx(startDate, startDate, index, 10 * Years,
+                                   -0.02, y)
+              << " fd = "
+              << (model.swapRate(startDate, startDate, index, 10 * Years,
+                                 -0.02 + 0.0001, y) -
+                  model.swapRate(startDate, startDate, index, 10 * Years, -0.02,
+                                 y)) /
+                     0.0001
+              << std::endl;
+    std::cout << "dS/dx ( 0.00 ) = "
+              << model.dSwapRateDx(startDate, startDate, index, 10 * Years,
+                                   0.00, y)
+              << " fd = "
+              << (model.swapRate(startDate, startDate, index, 10 * Years,
+                                 0.0 + 0.0001, y) -
+                  model.swapRate(startDate, startDate, index, 10 * Years, 0.0,
+                                 y)) /
+                     0.0001
+              << std::endl;
+    std::cout << "dS/dx ( +0.02 ) = "
+              << model.dSwapRateDx(startDate, startDate, index, 10 * Years,
+                                   0.02, y)
+              << " fd = "
+              << (model.swapRate(startDate, startDate, index, 10 * Years,
+                                 0.02 + 0.0001, y) -
+                  model.swapRate(startDate, startDate, index, 10 * Years, 0.02,
+                                 y)) /
+                     0.0001
+              << std::endl;
 
     return 0;
-
 }
