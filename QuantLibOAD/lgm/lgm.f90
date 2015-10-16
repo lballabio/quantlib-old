0ce79afc8c53ed845cf47fc8d66197707e34ba5a
Peter Caspers <pcaspers1973@gmail.com>
2015-10-16 16:43:22 +0200

    finalize first version of engine
    the linear approximation during rollback is not accurate enough, many
    points are required to match the original's engine result (using cubic
    interpolation), so we have to work on this

--- QuantLibOAD/lgm/lgm.f90
+++ QuantLibOAD/lgm/lgm.f90
@@ -3,9 +3,8 @@
 
 subroutine lgm_swaption_engine(n_times, times, modpar, n_expiries, &
      expiries, callput, n_floats, &
-     float_startidxes, float_mults, float_spreads, float_t1s, float_t2s, float_tps, &
-     fix_startidxes, n_fixs, fix_cpn, fix_tps, res, &
-     integration_points, stddevs)
+     float_startidxes, float_mults, index_acctimes, float_spreads, float_t1s, float_t2s, float_tps, &
+     fix_startidxes, n_fixs, fix_cpn, fix_tps, integration_points, stddevs, res)
 
   implicit none
 
@@ -23,14 +22,14 @@ subroutine lgm_swaption_engine(n_times, times, modpar, n_expiries, &
   ! concatenated model parameter array: H, zeta, discounts
   double precision, dimension(0:3*n_times-1):: modpar
 
-  integer:: n_expiries, expiries, callput
-  integer, dimension(0:n_expiries):: fix_startidxes, float_startidxes
+  integer:: n_expiries, callput
+  integer, dimension(0:n_expiries-1):: expiries, fix_startidxes, float_startidxes
   integer:: n_floats, n_fixs
-  double precision, dimension(0:n_floats-1):: float_mults, float_spreads
+  double precision, dimension(0:n_floats-1):: float_mults, index_acctimes, float_spreads
   integer, dimension(0:n_floats-1):: float_t1s, float_t2s
   integer, dimension(0:n_floats-1):: float_tps
-  double precision:: fix_cpn
   integer, dimension(0:n_fixs-1):: fix_tps
+  double precision, dimension(0:n_fixs-1):: fix_cpn
 
   integer:: integration_points
   double precision:: stddevs
@@ -40,8 +39,8 @@ subroutine lgm_swaption_engine(n_times, times, modpar, n_expiries, &
   ! additional variables
 
   integer:: idx, expiry0idx, expiry1idx, yidx0, yidx1
-  integer:: i, k, l
-  double precision, dimension(0:2*integration_points,2):: npv
+  integer:: i, k, l, m
+  double precision, dimension(0:2*integration_points,0:1):: npv
   double precision, dimension(0:2*integration_points):: val, z
   double precision:: sigma_t0_t1, sigma_0_t0, sigma_0_t1
   double precision:: center, yidx, a, b, coeff1, coeff2, da, x0, x1, price
@@ -52,6 +51,15 @@ subroutine lgm_swaption_engine(n_times, times, modpar, n_expiries, &
 
   ! start of the calculation
 
+  write (*,*) '====== lgm f90 routine ======'
+
+  write (*,*) 'nfix =', n_fixs, ' cpn first=', fix_cpn(0), 'cpn last=', fix_cpn(n_fixs-1)
+
+  do k=0, 2*integration_points,1
+     npv(k,0) = 0.0d0
+     npv(k,1) = 0.0d0
+  end do
+
   swapflag = 0
 
   !$openad INDEPENDENT(activevars)
@@ -60,18 +68,19 @@ subroutine lgm_swaption_engine(n_times, times, modpar, n_expiries, &
   expiry1idx = 0
 
   do k = 0, 2*integration_points, 1
-     z(k) = -stddevs + dble(k)/dble(2*integration_points+1) * 2.0d0 * stddevs
+     z(k) = -stddevs + dble(k)/dble(2*integration_points) * 2.0d0 * stddevs
      write (*,*) 'z(', k, ')=', z(k)
   end do
 
+  write (*,*) 'number of times is ', n_times
 
   ! loop over expiry dates
-  do idx = expiries(n_expiries-1), expiries(0)-1, -1
-
-     if (idx == expiries(0)-1) then
+  do idx = n_expiries-1, -1, -1
+     
+     if (idx == -1) then
         expiry0idx = 0
      else
-        expiry0idx = idx
+        expiry0idx = expiries(idx)
      endif
 
      sigma_0_t0 = sqrt(modpar(n_times+expiry0idx))
@@ -80,83 +89,95 @@ subroutine lgm_swaption_engine(n_times, times, modpar, n_expiries, &
         sigma_t0_t1 = sqrt(modpar(n_times+expiry1idx)-modpar(n_times+expiry0idx))
      end if
 
+     write (*,*), '========================================================'
+     write (*,*), 'idx ', idx, ' expiryindex ', expiry0idx, ' time ', times(expiry0idx)  
+     write (*,*), '========================================================'
+
      ! loop over integration points
-     do k = 0, 2*integration_points, 1
+
+     if(idx == -1) then
+        m = 0
+        center = 0
+     else
+        m = 2*integration_points
+     endif
+
+     do k = 0, m, 1
 
         ! roll back
         if (expiry1idx /= 0) then
 
-           if (expiry0idx == 0) then
-              center = 0.0d0
-           else
+           if (idx /= -1) then
               center = z(k)
            endif
+
            do i=0, 2*integration_points, 1
-              yidx =  ( (center*sigma_0_t0 + &
-                   dble(i-integration_points) * stddevs * sigma_t0_t1) / sigma_0_t1 + stddevs ) &
-                   / (2*stddevs)
+              yidx = ((center*sigma_0_t0+dble(i-integration_points)/dble(integration_points)* &
+                   stddevs*sigma_t0_t1)/sigma_0_t1 + stddevs) / (2.0d0*stddevs) * dble(2*integration_points+1)
               yidx0 = floor(yidx)
               yidx1 = yidx0+1
               weight0 = yidx1 - yidx
               weight1 = yidx - yidx0
-              val(i) = weight0 * npv(max(yidx0,0),1-swapflag) + &
-                   weight1 * npv(min(yidx1,2*integration_points),1-swapflag)
+              val(i) = weight0 * npv(min(max(yidx0,0),2*integration_points),1-swapflag) + &
+                   weight1 * npv(min(max(yidx1,0),2*integration_points),1-swapflag)
            end do
 
            price = 0.0d0
            do i=0, 2*integration_points-1, 1
               a = (val(i+1)-val(i))/(z(i+1)-z(i))
               b = (val(i)*z(i+1)-val(i+1)*z(i)) / (z(i+1)-z(i))
-              coeff1 = a
-              coeff2 = - a * z(i) + b
-              da = M_SQRT2 * coeff1
+              da = M_SQRT2 * a
               x0 = z(i) * M_SQRT1_2
               x1 = z(i+1) * M_SQRT1_2
-              price = price + 0.5d0 * coeff2 * erf(x1) - &
+              price = price + (0.5d0 * b * erf(x1) - &
                    1.0d0 / (4.0d0 * M_SQRTPI) * exp(-x1 * x1) * &
-                   (2.0d0 * da) - &
-                   0.5d0 * coeff2 * erf(x0) - &
+                   (2.0d0 * da)) - &
+                   (0.5d0 * b * erf(x0) - &
                    1.0d0 / (4.0d0 * M_SQRTPI) * exp(-x0 * x0) * &
-                   (2.0d0 * da)
+                   (2.0d0 * da))
+              ! debug
+              ! simple integration
+              ! price = price + (val(i+1)+val(i))/2.0d0 * (z(i+1)-z(i)) * 1.0d0/sqrt(2.0d0*3.1415926535) * &
+              !      exp(- 0.5*(z(i)+z(i+1))/2.0d0*(z(i)+z(i+1))/2.0d0 )
            end do
-
+           write (*,*) 'roll back k', k, ' **** result **** ', price
            npv(k,swapflag) = price
-
         end if
 
         ! payoff generation
 
-        if(expiry0Idx > 0) then
+        if(idx >= 0) then
            floatlegnpv = 0.0d0
-           do l = float_startidxes(k), n_floats, 1
-              forward = modpar(2*n_times+float_t2s(l)) * &
-                   exp(-modpar(float_t2s(l))*z(k)*sigma_0_t0- &
-                   0.5*modpar(float_t2s(l))*modpar(float_t2s(l))*modpar(n_times+float_t2s(l))) / &
-                   (modpar(2*n_times+float_t1s(l))  * &
-                   exp(-modpar(float_t1s(l))*z(k)*sigma_0_t0- &
-                   0.5*modpar(float_t1s(l))*modpar(float_t1s(l))*modpar(n_times+float_t1s(l)))) &
-                   - 1.0d0
+           do l = float_startidxes(idx), n_floats-1, 1
+              forward = (modpar(2*n_times+float_t1s(l)) * &
+                   exp(-modpar(float_t1s(l))*(z(k)*sigma_0_t0+ &
+                   0.5*modpar(n_times+expiries(idx))*modpar(float_t1s(l)))) / &
+                   (modpar(2*n_times+float_t2s(l)) * &
+                   exp(-modpar(float_t2s(l))*(z(k)*sigma_0_t0+ &
+                   0.5*modpar(n_times+expiries(idx))*modpar(float_t2s(l))))) &
+                   - 1.0d0) / index_acctimes(l)
               discount =  modpar(2*n_times+float_tps(l)) * &
-                   exp(-modpar(float_tps(l))*z(k)*sigma_0_t0- &
-                   0.5*modpar(float_tps(l))*modpar(float_tps(l))*modpar(n_times+float_tps(l)))
+                   exp(-modpar(float_tps(l))*(z(k)*sigma_0_t0+ &
+                   0.5*modpar(n_times+expiries(idx))*modpar(float_tps(l))))
               floatlegnpv = floatlegnpv + float_mults(l) * ( float_spreads(l) + forward) * discount
            end do
            fixlegnpv = 0.0d0
-           do l = fix_startidxes(k), n_fixs, 1
-              discount = modpar(2*n_times+float_tps(l)) * &
-                   exp(-modpar(fix_tps(l))*z(k)*sigma_0_t0- &
-                   0.5*modpar(fix_tps(l))*modpar(fix_tps(l))*modpar(n_times+fix_tps(l)))
+           do l = fix_startidxes(idx), n_fixs-1, 1
+              discount =  modpar(2*n_times+fix_tps(l)) * &
+                   exp(-modpar(fix_tps(l))*(z(k)*sigma_0_t0+ &
+                   0.5*modpar(n_times+expiries(idx))*modpar(fix_tps(l))))
               fixlegnpv = fixlegnpv + fix_cpn(l) * discount
            end do
+           exercisevalue = callput * (floatlegnpv - fixlegnpv)
+           ! write (*,*) 'k', k, 'fix', fixlegnpv, 'float', floatlegnpv, 'npv0',npv(k,swapflag), &
+           !      'execrise ', exercisevalue
+           npv(k,swapflag) = max(npv(k,swapflag),exercisevalue)
         end if
-
-        exercisevalue = callput * (floatlegnpv - fixlegnpv)
-
-        npv(k,swapflag) = max(npv(k,swapflag),exercisevalue)
 
      end do ! loop integration points
 
      swapflag = 1-swapflag
+     expiry1idx = expiry0idx
 
   end do ! loop expiry dates
 
