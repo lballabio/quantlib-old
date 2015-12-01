b8c334be60026d3e2729b1fbc3dad0ab7109e8d2
Pepe <japari@free.fr>
2015-12-01 07:30:12 +0100

    doc and culprit

--- QuantLib/ql/pricingengines/swap/cvaswapengine.hpp
+++ QuantLib/ql/pricingengines/swap/cvaswapengine.hpp
@@ -1,3 +1,22 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2015 Jose Aparicio
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
 #ifndef quantlib_cva_swap_engine_hpp
 #define quantlib_cva_swap_engine_hpp
 
@@ -14,6 +33,9 @@ namespace QuantLib {
   /*! Unilateral counterparty adjusted swap pricing engine.
     See sect. II-5 in: Risk Neutral Pricing of Counterparty Risk
     D. Brigo, M. Masetti, 2004
+    or in "A Formula for Interest Rate Swaps Valuation under
+      Counterparty Risk in presence of Netting Agreements"
+    D. Brigo and M. Masetti; May 4, 2005
 
     Using an object for the option engine. It might be more adecute
     to use a template. Using a type avoids resetting arguments and 
